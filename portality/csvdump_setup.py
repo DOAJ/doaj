f77417c096272d2d84e0d1d70a79e80fbca6df9d
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-11-23 18:28:22 +0000

    CSV dummy data script changed to conform to new data model

--- portality/csvdump_setup.py
+++ portality/csvdump_setup.py
@@ -2,15 +2,16 @@ from portality.models import Journal
 
 
 journals = [
-    {'title':'Title 1', 'publisher': 'Publisher 1', 'issn': '12345678'},
-    {'title':'Title 2', 'publisher': 'Publisher 2', 'issn': '01234567', 'eissn': '76543210', 'license': 'cc-by'},
-    {'title':'Title 3', 'publisher': 'Publisher 3', 'apc': False},
-    {'title':'Title 4', 'publisher': 'Publisher 3', 'language': 'en', 'content_in_doaj': True},
-    {'title':'Title 5', 'publisher': 'Publisher 2', 'issn': '23456789', 'content_in_doaj': False, 'language': 'bg', 'apc':True},
+    {'title':'Title 1', 'publisher': 'Publisher 1', 'identifier':[{'type':'pissn', 'id': '12345678'}]},
+    {'title':'Title 2', 'publisher': 'Publisher 2', 'identifier':[{'type':'pissn', 'id': '01234567'}, {'type':'eissn', 'id': '76543210'}], 'license': [{'type': 'cc-by','open_access': True, 'url': 'http://opendefinition.org/licenses/cc-by/'}]},
+    {'title':'Title 3', 'publisher': 'Publisher 3', 'author_pays': False},
+    {'title':'Title 4', 'publisher': 'Publisher 3', 'language': 'en', 'active': True},
+    {'title':'Title 5', 'publisher': 'Publisher 2', 'identifier':[{'type':'pissn', 'id': '23456789'}], 'active': False, 'language': 'bg', 'author_pays':True},
 ]
 
 for j in journals:
-    jm = Journal(**j)
+    jm = Journal(**{'bibjson':j})
+
     jm.save()
 
 Journal.refresh()
