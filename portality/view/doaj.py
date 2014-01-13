5a8edf3d5f0d8e486ca4fc5e107f8f1b54aae16f
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-01-13 11:36:51 +0000

    add a Thanks for your support page

--- portality/view/doaj.py
+++ portality/view/doaj.py
@@ -345,6 +345,10 @@ def suggest():
 def support_doaj():
     return render_template("doaj/supportDoaj.html")
 
+@blueprint.route("/support_thanks")
+def support_doaj():
+    return render_template("doaj/support_thanks.html")
+
 @blueprint.route("/googlebdb21861de30fe30.html")
 def google_webmaster_tools():
     return 'google-site-verification: googlebdb21861de30fe30.html'
