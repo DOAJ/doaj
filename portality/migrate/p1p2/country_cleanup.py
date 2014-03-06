6be8caa8ad806806e920d0135c1fb0f35b700046
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-03-06 14:56:16 +0000

    fix country cleanup migrate script

--- portality/migrate/p1p2/country_cleanup.py
+++ portality/migrate/p1p2/country_cleanup.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from portality import models
 from portality import xwalk
 
-def main(argv=sys.argv)
+def main(argv=sys.argv):
     start = datetime.now()
     
     journal_iterator = models.Journal.all_in_doaj()
