acea48043dff8b412df0f2ad58c7856daddf4d79
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-04-14 09:53:45 +0100

    suggestion form: add autocomplete to owner + minor JS cleanup

--- portality/static/doaj/js/suggest.js
+++ portality/static/doaj/js/suggest.js
@@ -51,58 +51,6 @@ jQuery(document).ready(function($) {
             })
         })
     }
-
-    /*
-    $("#submit_status").click(function(event) {
-        event.preventDefault()
-        
-        var newstatus = $("select[name=application_status]").val()
-        var suggestion_id = $("input[name=suggestion_id]").val()
-        var original = $("input[name=current_status]").val()
-        var obj = {"status" : newstatus}
-        
-        $.ajax({
-            type: "POST",
-            url: "/admin/suggestion/" + suggestion_id,
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify(obj),
-            success: function() {
-                // alert("status updated")
-                // update the hidden field
-                $("input[name=current_status]").val(newstatus)
-                $("#submit_status").attr("disabled", "disabled")
-                $("#submit_status").attr("class", "btn")
-            },
-            error: function() {
-                alert("ERROR: unable to update status.  If the problem persists, please contact your sysadmin.")
-                // reset the pull-down
-                $("select[name=application_status] option").filter(function() {return $(this).val() === original}).prop("selected", true)
-                $("#submit_status").attr("disabled", "disabled")
-                $("#submit_status").attr("class", "btn")
-            },
-            complete: function(req, status) {
-                // alert(status)
-            }
-        })
-
-    });
-
-    
-    $("select[name=application_status]").change(function() {
-        var original = $("input[name=current_status]").val()
-        var newstatus = $("select[name=application_status]").val()
-        
-        if (newstatus !== original) {
-            $("#submit_status").removeAttr("disabled")
-            $("#submit_status").attr("class", "btn btn-info")
-        } else {
-            $("#submit_status").attr("disabled", "disabled")
-            $("#submit_status").attr("class", "btn")
-        }
-    });
-    */
-    
     
     toggle_optional_field('waiver_policy', ['#waiver_policy_url']);
     toggle_optional_field('download_statistics', ['#download_statistics_url']);
@@ -130,6 +78,7 @@ jQuery(document).ready(function($) {
     autocomplete('#publisher', 'bibjson.publisher');
     autocomplete('#society_institution', 'bibjson.institution');
     autocomplete('#platform', 'bibjson.provider');
+    autocomplete('#owner', 'id', 'account');
 
     exclusive_checkbox('digital_archiving_policy', 'No policy in place');
     exclusive_checkbox('article_identifiers', 'None');
