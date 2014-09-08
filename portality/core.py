4519f5e40be58b4cc1a8b2857e461e58e920737f
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-09-08 13:46:06 +0100

    improve error logging and send emails to dedicated address

--- portality/core.py
+++ portality/core.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask import request, redirect, url_for, flash
 
 from portality import settings
+from portality.error_handler import setup_error_logging
 from flask.ext.login import LoginManager, current_user
 login_manager = LoginManager()
 
@@ -76,37 +77,6 @@ def initialise_index(app):
             r = requests.put(im, json.dumps(mapping))
             print key, r.status_code
 
-def setup_error_logging(app):
-    if not app.debug:
-        # Custom logging WILL BE IGNORED by Flask if app.debug == True -
-        # even if you remove the condition above this comment.
-        import logging, sys
-        ADMINS = app.config.get('ADMINS', [])
-        if not ADMINS:
-            e = app.config.get('ADMIN_EMAIL')
-            if e:
-                if isinstance(e, basestring):
-                    ADMINS = [e]
-                elif isinstance(e, list):
-                    ADMINS = e
-        if ADMINS and not app.config.get('SUPPRESS_ERROR_EMAILS'):
-            from logging.handlers import SMTPHandler
-            import platform
-            hostname = platform.uname()[1]
-            mail_handler = SMTPHandler('mailtrap.io',
-                                       'server-error@' + hostname,
-                                       ADMINS,
-                                       'DOAJ Flask Error',
-                                       credentials=('doaj-errors-265cc22d4983a31c', 'd8788b4007fd9cc6')
-                           )
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-
-        # send errors to stderr, supervisord will capture them in the app's
-        # error log
-        send_errors_to_supervisor = logging.StreamHandler(sys.stderr)
-        send_errors_to_supervisor.setLevel(logging.ERROR)
-        app.logger.addHandler(send_errors_to_supervisor)
 
 def setup_jinja(app):
     '''Add jinja extensions and other init-time config as needed.'''
