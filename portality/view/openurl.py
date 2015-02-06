bfeeeb6721aa7f78874c24c6adf3f88fc5ea0b9a
Steven Eardley <me@steveneardley.co.uk>
2015-02-06 16:38:16 +0000

    echo the incoming query

--- portality/view/openurl.py
+++ portality/view/openurl.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 blueprint = Blueprint('openurl', __name__)
+
+@blueprint.route("/openurl", methods=["GET", "POST"])
+def get_incoming_query():
+    url_query = request.url.split(request.base_url).pop()
+    return url_query
 
