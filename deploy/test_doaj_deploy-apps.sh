d89a791034f2081894bb1e1e1add89f7db2638a4
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-03-10 19:17:25 +0000

    new DOAJ deploy scripts

--- deploy/test_doaj_deploy-apps.sh
+++ deploy/test_doaj_deploy-apps.sh
@@ -3,9 +3,6 @@ sudo apt-get update -q -y
 sudo apt-get -q -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
 cd $DIR/..
 . ../../bin/activate
-git checkout develop
-git pull
-git submodule init
-git submodule update
 pip install -r requirements.txt
-sudo supervisorctl restart doaj-test
+sudo supervisorctl reread doaj-test
+sudo supervisorctl update doaj-test
---
+++ deploy/test_doaj_deploy-gateway.sh
@@ -0,0 +1,16 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR/..
+git submodule update --recursive --init
+git submodule update --recursive
+
+# prep sym links for app servers
+ln -sf $DIR/supervisor/doaj-test.conf /home/cloo/repl/apps/supervisor/conf.d/doaj-test.conf
+ln -sf $DIR/nginx/doaj-apps /home/cloo/repl/apps/nginx/sites-available/doaj-apps
+ln -sf /home/cloo/repl/apps/nginx/sites-available/doaj-apps /home/cloo/repl/apps/nginx/sites-enabled/doaj-apps
+
+# prep sym links for gateway
+ln -sf /home/cloo/repl/apps/doaj/src/doaj/deploy/nginx/doaj-gate /home/cloo/repl/gateway/nginx/sites-available/doaj-gate
+
+# replicate across app servers
+/home/cloo/repl/replicate.sh
+/home/cloo/repl/command.sh apps /home/cloo/repl/apps/doaj/src/doaj/deploy/test_doaj_deploy-apps.sh
