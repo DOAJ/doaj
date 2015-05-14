caf751e79e9d1d26cb8a841b5f68467d836909ba
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-05-14 17:05:54 +0100

    increase gunicorn workers

--- deploy/doaj_gunicorn_config.py
+++ deploy/doaj_gunicorn_config.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
 bind = "127.0.0.1:5050"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 8 + 1
 proc_name = 'doaj'
 max_requests = 1000
