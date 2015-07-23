d9d80cd5ed533328dc22e99989803fddf7d98981
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-07-23 17:49:47 +0100

    finish CRUD foundation by making everything a class method

--- portality/api/v1/crud/common.py
+++ portality/api/v1/crud/common.py
@@ -1,5 +1,7 @@
 from portality.api.v1.crud.journals import JournalsCrudApi
+from portality.api.v1.crud.articles import ArticlesCrudApi
+from portality.api.v1.crud.applications import ApplicationsCrudApi
 
 
-class CrudApi(JournalsCrudApi):
+class CrudApi(JournalsCrudApi, ArticlesCrudApi, ApplicationsCrudApi):
     pass
--- portality/api/v1/crud/journals.py
+++ portality/api/v1/crud/journals.py
@@ -2,7 +2,8 @@ from portality.api.v1.common import Api
 
 
 class JournalsCrudApi(Api):
-    def __retrieve_journal(self, jid):
+    @classmethod
+    def __retrieve_journal(cls, jid):
         pass
         # fetch the journal from Elasticsearch, make a copy of journal.data
         # remove keys that are never to be returned through the API, regardless
@@ -12,12 +13,14 @@ class JournalsCrudApi(Api):
         # remove these keys: bibjson.active
         # remove everything except bibjson, id, created_date, last_updated
 
-    def retrieve_public_journal(self, jid):
-        j = self.__retrieve_journal(jid)
+    @classmethod
+    def retrieve_public_journal(cls, jid):
+        j = cls.__retrieve_journal(jid)
         # return j
 
-    def retrieve_auth_journal(self, jid, owner):
-        j = self.__retrieve_journal(jid)
+    @classmethod
+    def retrieve_auth_journal(cls, jid, owner):
+        j = cls.__retrieve_journal(jid)
         # check if owner arg above is the same as the journal's owner (see portality.models.Journal for owner property)
         # authenticated owners of a journal are allowed to see journals where .is_in_doaj() == False
         # if yes
