36e336365803a5f8a40533154397928a5d1c080f
Emanuil Tolev <emanuil.tolev@gmail.com>
2016-02-11 16:39:15 +0000

    add harvester environment

--- deploy/deploy-apps.sh
+++ deploy/deploy-apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_SCRIPT=`basename "$0"`
-[ $# -ne 1 ] && echo "Call this script as $THIS_SCRIPT <environment: [production, staging, test]>" && exit 1
+[ $# -ne 1 ] && echo "Call this script as $THIS_SCRIPT <environment: [production, staging, test, harvester]>" && exit 1
 
 ENV=$1
 
