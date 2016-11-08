1a46f1624055cb282af40f355f9739cc09d2c7a3
Richard Jones <richard@cottagelabs.com>
2016-11-08 14:38:22 +0000

    expand on async runner and streamline task interface

--- portality/models/background.py
+++ portality/models/background.py
@@ -10,19 +10,62 @@ class BackgroundJob(dataobj.DataObj, dao.DomainObject):
             kwargs = kwargs["_source"]
 
         self._add_struct(BACKGROUND_STRUCT)
+        if "status" not in kwargs:
+            kwargs["status"] = u"queued"
+
         super(BackgroundJob, self).__init__(raw=kwargs)
 
     @property
     def user(self):
         return self._get_single("user")
 
+    @user.setter
+    def user(self, val):
+        self._set_with_struct("user", val)
+
+    @property
+    def action(self):
+        return self._get_single("action")
+
+    @action.setter
+    def action(self, val):
+        self._set_with_struct("action", val)
+
     @property
     def audit(self):
         return self._get_list("audit")
 
+    @property
+    def params(self):
+        return self._get_single("params")
+
+    @params.setter
+    def params(self, obj):
+        self._set_single("params", obj)     # note we don't bother setting with struct, as there is none
+
+    @property
+    def reference(self):
+        return self._get_single("reference")
+
+    @reference.setter
+    def reference(self, obj):
+        self._set_single("reference", obj)     # note we don't bother setting with struct, as there is none
+
+    def start(self):
+        self._set_with_struct("status", u"processing")
+
+    def success(self):
+        self._set_with_struct("status", u"complete")
+
+    def fail(self):
+        self._set_with_struct("status", u"error")
+
+    def is_failed(self):
+        return self._get_single("status") == u"error"
+
     def add_audit_message(self, msg, timestamp=None):
         if timestamp is None:
-            timestamp = dates.now()
+            timestamp = dates.now_with_microseconds()
         obj = {"message" : msg, "timestamp": timestamp}
         self._add_to_list_with_struct("audit", obj)
 
@@ -48,7 +91,7 @@ BACKGROUND_STRUCT = {
         "audit" : {
             "fields" : {
                 "message" : {"coerce" : "unicode"},
-                "timestamp" : {"coerce" : "utcdatetime"}
+                "timestamp" : {"coerce" : "utcdatetimemicros"}
             }
         }
     }
