9884bd789d01c11377af7cb8f843aa0f97303913
Richard Jones <richard@cottagelabs.com>
2016-12-02 09:16:35 +0000

    throw an error if a schedule is called for an action which has not been configured

--- portality/tasks/redis_huey.py
+++ portality/tasks/redis_huey.py
@@ -5,7 +5,10 @@ main_queue = RedisHuey('doaj', host=app.config['HUEY_REDIS_HOST'], port=app.conf
 
 def schedule(action):
     cfg = app.config.get("HUEY_SCHEDULE", {})
-    action_cfg = cfg.get(action, {})
+    action_cfg = cfg.get(action)
+    if action_cfg is None:
+        raise RuntimeError(u"No configuration for scheduled action '{x}'.  Define this in HUEY_SCHEDULE first then try again.".format(x=action))
+
     month = action_cfg.get("month", "*")
     day = action_cfg.get("day", "*")
     hour = action_cfg.get("hour", "*")
