aa32d1a6b9f19b4c47a96d99c6e7e624446a7e88
Richard Jones <richard@cottagelabs.com>
2016-12-07 08:22:45 +0000

    set huey to always_eager in tests

--- doajtest/bootstrap.py
+++ doajtest/bootstrap.py
@@ -1,5 +1,7 @@
 import time
 from portality import core, dao
+from portality.tasks.redis_huey import main_queue
+
 
 import logging
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -9,6 +11,8 @@ def prepare_for_test():
 
     # Ensure all features are enabled so we don't fail feature-specific tests
     core.app.config['FEATURES'] = core.app.config['VALID_FEATURES']
+
+    main_queue.always_eager = True
 
     # if a test on a previous run has totally failed and tearDown has not run, then make sure the index is gone first
     dao.DomainObject.destroy_index()
