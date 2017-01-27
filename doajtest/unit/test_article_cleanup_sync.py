f73ab7a65cb7e506be798313123ea4eb63d867ca
Steven Eardley <steve@eardley.xyz>
2017-01-27 16:10:24 +0000

    Additional tests for article cleanup script edge cases

--- doajtest/unit/test_article_cleanup_sync.py
+++ doajtest/unit/test_article_cleanup_sync.py
@@ -6,6 +6,7 @@ from portality.tasks import article_cleanup_sync
 
 from datetime import datetime
 import time
+import rstr
 
 
 class TestArticleCleanupSync(DoajTestCase):
@@ -139,6 +140,92 @@ class TestArticleCleanupSync(DoajTestCase):
 
         # this one should have been deleted
         assert a3u is None
+
+    def test_04_odd_sync_situations(self):
+        # If articles contain ISSNS for >1 distinct journals, we should choose the 'best'
+
+        [jk_s, l_s, m_s] = fixtures.JournalFixtureFactory.make_many_journal_sources(3, in_doaj=False)
+
+        # a journal from which we will sync metadata
+        jk_s['admin']['in_doaj'] = True
+        j = models.Journal(**jk_s)
+        j.save()
+
+        # another journal that looks suspiciously like the first, more recently updated
+        copied_journal_title = "This is a duplicate journal"
+        jk_s['bibjson']['title'] = copied_journal_title
+        k = models.Journal(**jk_s)
+        k.set_last_manual_update()
+        k.save()
+
+        # a third journal, different to the first two and it's not in the DOAJ.
+        bad_journal_title = "This is an undesirable journal because it is not in DOAJ"
+        l_s['bibjson']['title'] = bad_journal_title
+        l = models.Journal(**l_s)
+        l.save()
+
+        # yet another journal, distinct but in the DOAJ
+        good_journal_title = "This is a good journal because it is in DOAJ"
+        m_s['bibjson']['title'] = good_journal_title
+        m_s['admin']['in_doaj'] = True
+        m = models.Journal(**m_s)
+        m.save()
+
+        # the identifiers to allow us to connect articles to the journal
+        jk_eissn = j.bibjson().get_identifiers(j.bibjson().E_ISSN)[0]
+        jk_pissn = j.bibjson().get_identifiers(j.bibjson().P_ISSN)[0]
+        l_eissn =l.bibjson().get_identifiers(l.bibjson().E_ISSN)[0]
+        l_pissn = l.bibjson().get_identifiers(l.bibjson().P_ISSN)[0]
+        m_pissn = m.bibjson().get_identifiers(m.bibjson().P_ISSN)[0]
+
+        # an article source which is already synchronised with both journals j and k
+        source = fixtures.ArticleFixtureFactory.make_article_source(eissn=jk_eissn, pissn=jk_pissn, with_journal_info=False,
+                                                                     with_id=False)
+        a1 = models.Article(**source)
+        a1.add_journal_metadata(j)
+        a1.save()
+
+        # an article without metadata, but whose ISSNs match both j and k
+        source2 = fixtures.ArticleFixtureFactory.make_article_source(eissn=jk_eissn, pissn=jk_pissn, with_journal_info=False,
+                                                                     with_id=False)
+        a2 = models.Article(**source2)
+        a2.save()
+
+        # an article that matches two different journal ISSNs
+        source3 = fixtures.ArticleFixtureFactory.make_article_source(eissn=l_eissn, pissn=m_pissn,
+                                                                     with_journal_info=False,
+                                                                     with_id=False)
+        a3 = models.Article(**source3)
+        a3.save()
+
+        # an article without metadata, but whose ISSNs match both j and k
+        source4 = fixtures.ArticleFixtureFactory.make_article_source(eissn=l_eissn, pissn=l_pissn,
+                                                                     with_journal_info=False,
+                                                                     with_id=False)
+        a4 = models.Article(**source4)
+        a4.save(blocking=True)
+
+        # run the sync/cleanup job
+        job = article_cleanup_sync.ArticleCleanupSyncBackgroundTask.prepare("testuser")
+        article_cleanup_sync.ArticleCleanupSyncBackgroundTask.submit(job)
+
+        time.sleep(2)
+
+        # retrieve any updated records
+        a1u = models.Article.pull(a1.id)
+        a2u = models.Article.pull(a2.id)
+        a3u = models.Article.pull(a3.id)
+        a4u = models.Article.pull(a4.id)
+
+        # a1 and a2 should have synced with journal k, the more recent one
+        assert a1u.bibjson().journal_title == copied_journal_title
+        assert a2u.bibjson().journal_title == copied_journal_title
+
+        # a3 should have synced with the journal that was in_doaj
+        assert a3u.bibjson().journal_title == good_journal_title
+
+        # a4 should have the journal info for journal l
+        assert a4u.bibjson().journal_title == bad_journal_title
 
     def test_05_best_journal(self):
         j1 = models.Journal()
