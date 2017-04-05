be551145e75f0f321f46bef6aa3aa634c13f81c5
Steven Eardley <steve@eardley.xyz>
2017-04-05 19:10:44 +0100

    Added script to run notes project

--- portality/scripts/update_mapping_reindex.py
+++ portality/scripts/update_mapping_reindex.py
@@ -2,11 +2,16 @@
 
 import esprit
 from portality.lib import es_data_mapping
+from portality.core import app
 
 if __name__ == "__main__":
     if app.config.get("SCRIPTS_READ_ONLY_MODE", False):
         print "System is in READ-ONLY mode, script cannot run"
         exit()
 
-    import argparse
-    parser = argparse.ArgumentParser()
+    new_mappings = es_data_mapping.get_mappings(app)
+
+    old = esprit.raw.Connection("localhost", 'doaj')
+    new = esprit.raw.Connection("localhost", 'doaj_notes')
+
+    esprit.tasks.reindex(old, new, "doaj_alias", new_mappings.keys(), new_mappings, new_version="1.7.5")
