867169cb7b7c959f17dba67c20c347b67aea0e95
Steven Eardley <steve@eardley.xyz>
2017-05-10 01:38:15 +0100

    Made the language codes unicode in datasets, which fixes some warnings and errors relating to writing journalcsv and starting the app.

--- portality/tasks/journal_csv.py
+++ portality/tasks/journal_csv.py
@@ -33,7 +33,7 @@ class JournalCSVBackgroundTask(BackgroundTask):
         out = os.path.join(csvdir, attachment_name)
 
         # write the csv file
-        with codecs.open(out, 'wb', encoding='utf8') as csvfile:
+        with codecs.open(out, 'wb', encoding='utf-8') as csvfile:
             make_journals_csv(csvfile)
 
         # update the ES record to point to the new file
