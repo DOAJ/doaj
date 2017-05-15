9197a23282c9d78bb1da2092b8e923170464c134
Steven Eardley <steve@eardley.xyz>
2017-05-15 16:52:51 +0100

    Added a dry_run to the duplicate article removal script.
    (cherry picked from commit 865c058)

--- portality/scripts/remove_duplicate_articles.py
+++ portality/scripts/remove_duplicate_articles.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-g", "--ghost", help="specify if you want the articles being deleted not to be snapshot", action="store_true")
+    parser.add_argument("-d", "--dry-run", help="run this script without actually deleting the articles.", action="store_true")
     parser.add_argument("-y", "--yes", help="are you running this script with output redirection e.g. DOAJENV=production python portality/scripts/remove_duplicate_articles.py >> ~/dedupe_`date +\%%F_\%%H\%%M\%%S`.log 2>&1 ? You really should.", action="store_true")
 
     args = parser.parse_args()
@@ -38,13 +39,15 @@ if __name__ == "__main__":
     snapshot = not args.ghost
     snapshot_report = "with snapshotting" if snapshot else "without snapshotting"
 
+    dry_run = args.dry_run
+
     start = datetime.now()
     count = 0
     print 'Starting {0} {snapshot} in 5 seconds, Ctrl+C to exit.'.format(start.isoformat(), snapshot=snapshot_report)
     time.sleep(5)
 
     originals = []  # make sure we only delete FORWARD. So, if articles
-    # A, B and C are duplicates, we stumble upon A first, and delete B and C.
+    # A, B and C are duplicates, we stumble upon A first, and delete B and C.cat
     # We must then ensure we do not delete A if we chance upon B or C
     # (because they are still in Python memory even though they are not in ES).
     article_iterator = models.Article.iterall(page_size=5000)
@@ -54,10 +57,11 @@ if __name__ == "__main__":
         if duplicates:
             print '{0}:'.format(a.id),  # original: ...
             for d in duplicates:
-                if not d.id in originals:
-                    if snapshot:
-                        d.snapshot()
-                    d.delete()
+                if d.id not in originals:
+                    if not dry_run:
+                        if snapshot:
+                            d.snapshot()
+                        d.delete()
                     count += 1
                     print '{0},'.format(d.id)  # ... duplicate_deleted1, duplicate_deleted2, etc.
                     time.sleep(1)
@@ -65,4 +69,7 @@ if __name__ == "__main__":
 
     end = datetime.now()
     length = end - start
-    print "Ended {0}. {1} articles deleted. Took {2}.".format(end.isoformat(), count, str(length))
+    if dry_run:
+        print "Ended {0}. {1} articles would be deleted. Took {2}.".format(end.isoformat(), count, str(length))
+    else:
+        print "Ended {0}. {1} articles deleted. Took {2}.".format(end.isoformat(), count, str(length))
