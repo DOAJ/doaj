5b8955c577d1f971a4deb181f85c0e0466400b6b
Richard Jones <richard@cottagelabs.com>
2017-05-23 13:27:23 +0100

    nearly complete implementation of new submit handler

--- portality/static/doaj/js/available_facetviews/admin.journalarticle.facetview.js
+++ portality/static/doaj/js/available_facetviews/admin.journalarticle.facetview.js
@@ -1,7 +1,7 @@
 var doaj = {
     currentFVOptions: false,
     active : false,
-    
+
     newMultiFormBox : function(params) {
         params = params === undefined ? {} : params;
         return new doaj.MultiFormBox(params);
@@ -12,6 +12,8 @@ var doaj = {
         this.widths = params.widths !== undefined ? params.widths : {};
         this.bindings = params.bindings !== undefined ? params.bindings : {};
         this.validators = params.validators !== undefined ? params.validators : {};
+        this.submitCfg = params.submit !== undefined ? params.submit : {};
+        this.urls = params.urls !== undefined ? params.urls : {};
 
         this.context = false;
         this.initialContent = {};
@@ -39,7 +41,148 @@ var doaj = {
             var selectHandler = doaj.eventClosure(this, "selectChanged");
             selectBox.unbind("change.multiformbox").bind("change.multiformbox", selectHandler);
 
+            var submit = this.context.find(".multiformbox-submit");
+            var submitHandler = doaj.eventClosure(this, "submit");
+            submit.unbind("click.multiformbox").bind("click.multiformbox", submitHandler);
+
             this.validate();
+        };
+
+        this.reset = function() {
+            this.transitionPart1({
+                target: ""
+            });
+        };
+
+        this.submit = function(element) {
+            this._submitting();
+
+            var cfg = this.submitCfg[this.currentField];
+            if (!cfg) {
+                cfg = {}
+            }
+
+            var sure = false;
+            if (cfg.sure) {
+                sure = confirm(cfg.sure);
+            } else {
+                sure = true;
+            }
+
+            if (!sure) {
+                this._enableSubmit();
+                return;
+            }
+
+            var successHandler = doaj.objClosure(this, "confirmSubmit");
+            var errorHandler = doaj.objClosure(this, "submitError");
+
+            this._doSubmit({
+                cfg: cfg,
+                dryRun: true,
+                success: successHandler,
+                error: errorHandler
+            });
+        };
+
+        this.confirmSubmit = function(data) {
+            var sure = confirm('This operation will affect ' + this._affectedMessage(data) + '.');
+
+            if (!sure) {
+                this._enableSubmit();
+                return;
+            }
+
+            var cfg = this.submitCfg[this.currentField];
+            if (!cfg) {
+                cfg = {}
+            }
+
+            var successHandler = doaj.objClosure(this, "submissionComplete");
+            var errorHandler = doaj.objClosure(this, "submitError");
+
+            this._doSubmit({
+                cfg: cfg,
+                dryRun: false,
+                success: successHandler,
+                error: errorHandler
+            });
+        };
+
+        this.submissionComplete = function(data) {
+            var msg = "Your bulk edit request has been submitted and queued for execution.<br>";
+            msg += this._affectedMessage(data) + " have been queued for edit.<br>";
+            msg += 'You can see your request <a href="' + this._bulkJobUrl(data) + '" target="_blank">here</a> in the background jobs interface (opens new tab).<br>';
+            msg += "You will get an email when your request has been processed; this could take anything from a few minutes to a few hours.<br>";
+            msg += '<a href="#" id="bulk-action-feedback-dismiss">dismiss</a>';
+
+            $(".bulk-action-feedback").html(msg).show();
+            $("#bulk-action-feedback-dismiss").unbind("click").bind("click", function(event) {
+                event.preventDefault();
+                $(".bulk-action-feedback").html("").hide();
+            });
+
+            this.reset();
+        };
+
+        this._bulkJobUrl = function(data) {
+            var url = "/admin/background_jobs?source=%7B%22query%22%3A%7B%22query_string%22%3A%7B%22query%22%3A%22" + data.job_id + "%22%2C%22default_operator%22%3A%22AND%22%7D%7D%2C%22sort%22%3A%5B%7B%22created_date%22%3A%7B%22order%22%3A%22desc%22%7D%7D%5D%2C%22from%22%3A0%2C%22size%22%3A25%7D";
+            return url;
+        };
+
+        this._doSubmit = function(params) {
+            var cfg = params.cfg;
+            var dryRun = params.dryRun;
+            var success = params.success;
+            var error = params.error;
+
+            var query = elasticSearchQuery({
+                options: doaj.currentFVOptions,
+                include_facets: false,
+                include_fields: false
+            });
+
+            var data = {};
+            if (cfg.data) {
+                data = cfg.data(this.context);
+            }
+            data["selection_query"] = query;
+            data["dry_run"] = dryRun;
+
+            var url = this.urls[this.currentField];
+            if (typeof(url) === "function") {
+                url = url();
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: JSON.stringify(data),
+                contentType : 'application/json',
+                success : success,
+                error: error
+            });
+        };
+
+        this._affectedMessage = function(data) {
+            var mfrg = "";
+            if (data.affected) {
+                if ("journals" in data.affected && "articles" in data.affected) {
+                    mfrg += data.affected.journals + " journals and " + data.affected.articles + " articles";
+                } else if ("journals" in data.affected) {
+                    mfrg = data.affected.journals + " journals";
+                } else if ("articles" in data.affected) {
+                    mfrg = data.affected.articles + " articles";
+                } else {
+                    mfrg = "an unknown number of records";
+                }
+            }
+            return mfrg;
+        };
+
+        this.submitError = function(jqXHR, textStatus, errorThrown) {
+            alert('There was an error with your request.');
+            this._enableSubmit();
         };
 
         this.selectChanged = function(element) {
@@ -82,6 +225,10 @@ var doaj = {
 
         this._disableSubmit = function() {
             this.context.find(".multiformbox-submit").attr('disabled', 'disabled').html("Submit");
+        };
+
+        this._submitting = function() {
+            this.context.find(".multiformbox-submit").attr('disabled', 'disabled').html("<img src='/static/doaj/images/white-transparent-loader.gif'>&nbsp;Submitting...");
         };
 
         this._showError = function(error_id) {
@@ -234,6 +381,14 @@ jQuery(document).ready(function($) {
         return {valid: true};
     }
 
+    function typeSelected() {
+        var type = doaj.currentFVOptions.active_filters._type;
+        if (type && type.length > 0) {
+            return type[0];
+        }
+        return null;
+    }
+
     var mfb = doaj.newMultiFormBox({
         selector: "#admin-bulk-box",
         widths: {
@@ -295,6 +450,63 @@ jQuery(document).ready(function($) {
                 }
                 return {valid: true};
             }
+        },
+        submit : {
+            delete : {
+                sure : 'Are you sure?  This operation cannot be undone!'
+            },
+            note : {
+                data: function(context) {
+                    return {
+                        note: $('#note', context).val()
+                    };
+                }
+            },
+            editor_group : {
+                data : function(context) {
+                    return {
+                        editor_group: $('#editor_group', context).val()
+                    };
+                }
+            },
+            edit_metadata : {
+                data : function(context) {
+                    var seal = $('#doaj_seal', context).val();
+                    if (seal === "True") {
+                        seal = true;
+                    } else if (seal === "False") {
+                        seal = false;
+                    }
+                    var data = {
+                        metadata : {
+                            publisher: $('#publisher', context).select2("val"),
+                            platform: $('#platform', context).select2("val"),
+                            country: $('#country', context).select2("val"),
+                            owner: $('#owner', context).select2("val"),
+                            contact_name: $('#contact_name', context).val(),
+                            contact_email: $('#contact_email', context).val(),
+                            doaj_seal: seal
+                        }
+                    };
+                    return data;
+                }
+            }
+        },
+        urls : {
+            withdraw: "/admin/journals/bulk/withdraw",
+            reinstate: "/admin/journals/bulk/reinstate",
+            delete : function() {
+                var type = typeSelected();
+                if (type === "journal") {
+                    return "/admin/journals/bulk/delete"
+                } else if (type === "article") {
+                    return "/admin/articles/bulk/delete"
+                }
+                return null;
+            },
+            note : "/admin/journals/bulk/edit_metadata",
+            editor_group : "/admin/journals/bulk/edit_metadata",
+            edit_metadata : "/admin/journals/bulk/edit_metadata"
         }
     });
     doaj.active = mfb;
