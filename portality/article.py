c4885a2f8d0782307641e31098023e945de289bb
Steven Eardley <steve@eardley.xyz>
2017-11-30 11:08:26 +0000

    When we are checking for duplicates, only check if we have a genuine DOI.

--- portality/article.py
+++ portality/article.py
@@ -156,10 +156,12 @@ class XWalk(object):
         if len(dois) > 0:
             # there should only be the one
             doi = dois[0]
-            articles = models.Article.duplicates(issns=issns, doi=doi)
-            possible_articles['doi'] = [a for a in articles if a.id != article.id]
-            if possible_articles['doi']:
-                found = True
+            # Only perform de-duplication on genuine DOIs - todo: do we need a full regex check?
+            if isinstance(doi, str) and doi.startswith('10.'):
+                articles = models.Article.duplicates(issns=issns, doi=doi)
+                possible_articles['doi'] = [a for a in articles if a.id != article.id]
+                if possible_articles['doi']:
+                    found = True
 
         # Second test is to look by fulltext url
         urls = b.get_urls(b.FULLTEXT)
