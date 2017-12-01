86a6c4aef1098e87b7764a94d5208b8543130891
Richard Jones <richard@cottagelabs.com>
2017-12-01 15:47:35 +0000

    validate status being set in the script

--- portality/scripts/change_application_status.py
+++ portality/scripts/change_application_status.py
@@ -2,8 +2,13 @@ from datetime import datetime
 from portality.core import app
 import csv
 from portality.models import Suggestion
+from portality.formcontext.choices import Choices
 
 def change_status(ids, new_status):
+    statuses = [x[0] for x in Choices.application_status("admin") if x[0] != ""]
+    if new_status not in statuses:
+        raise Exception("Must use an allowed status: " + ", ".join(statuses))
+
     for id in ids:
         a = Suggestion.pull(id)
         a.set_application_status(new_status)
