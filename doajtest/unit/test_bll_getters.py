7c735127e496c2fa558e2e4ba59e54a9a5c532d0
Richard Jones <richard@cottagelabs.com>
2017-12-22 13:51:21 +0000

    extend bll journal getter tests to include object locks

--- doajtest/unit/test_bll_getters.py
+++ doajtest/unit/test_bll_getters.py
@@ -1,28 +1,36 @@
 from doajtest.helpers import DoajTestCase
 from parameterized import parameterized, param
-from doajtest.fixtures import JournalFixtureFactory, ApplicationFixtureFactory
-import uuid
+from doajtest.fixtures import JournalFixtureFactory, ApplicationFixtureFactory, AccountFixtureFactory
+import uuid, time
 
 from copy import deepcopy
 
-from portality.models import Journal, Suggestion
+from portality.models import Journal, Suggestion, Account
 
 from portality.bll import DOAJ
 from portality.bll import exceptions
+from portality import lock
 
 
 def load_journal_cases():
+    account = Account(**AccountFixtureFactory.make_publisher_source())
+    account.set_id(account.makeid())
+
     journal = Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
-    journal.makeid()
+    journal.set_id(journal.makeid())
 
     wrong_id = uuid.uuid4()
 
     return [
-        param("no_journal_no_id", None, None, raises=exceptions.ArgumentException),
-        param("no_journal", None, journal.id),
-        param("journal_wrong_id", journal, wrong_id),
-        param("journal_right_id", journal, journal.id)
+        param("j_id_acc_lock", journal, journal.id, account, True, raises=lock.Locked),
+        param("j_id_acc_nolock", journal, journal.id, account, False),
+        param("j_id_noacc_nolock", journal, journal.id, None, False),
+        param("j_noid_noacc_nolock", journal, None, None, False, raises=exceptions.ArgumentException),
+        param("j_wid_noacc_nolock", journal, wrong_id, None, False),
+        param("noj_id_noacc_nolock", None, journal.id, None, False),
+        param("noj_noid_noacc_nolock", None, None, None, False, raises=exceptions.ArgumentException)
     ]
+
 
 def load_application_cases():
     application = Suggestion(**ApplicationFixtureFactory.make_application_source())
@@ -40,7 +48,11 @@ def load_application_cases():
 class TestBLLGetters(DoajTestCase):
 
     @parameterized.expand(load_journal_cases)
-    def test_01_get_journal(self, name, journal, journal_id, raises=None):
+    def test_01_get_journal(self, name, journal, journal_id, account, lock_journal, raises=None):
+
+        if lock_journal:
+            lock.lock("journal", journal.id, "someoneelse", blocking=True)
+
         doaj = DOAJ()
 
         if journal is not None:
@@ -48,13 +60,27 @@ class TestBLLGetters(DoajTestCase):
 
         if raises is not None:
             with self.assertRaises(raises):
-                retrieved = doaj.journal(journal_id)
+                if account is None:
+                    retrieved, _ = doaj.journal(journal_id)
+                else:
+                    retrieved, jlock = doaj.journal(journal_id, lock_journal=True, lock_account=account)
         else:
-            retrieved = doaj.journal(journal_id)
-            if retrieved is not None:
-                assert retrieved.data == journal.data
+            if account is None:
+                retrieved, _ = doaj.journal(journal_id)
+                if retrieved is not None:
+                    assert retrieved.data == journal.data
+                else:
+                    assert retrieved is None
             else:
-                assert retrieved is None
+                retrieved, jlock = doaj.journal(journal_id, lock_journal=True, lock_account=account)
+                if retrieved is not None:
+                    assert retrieved.data == journal.data
+                else:
+                    assert retrieved is None
+
+                time.sleep(2)
+
+                assert lock.has_lock("journal", journal_id, account.id)
 
     @parameterized.expand(load_application_cases)
     def test_02_get_application(self, name, application, application_id, raises=None):
