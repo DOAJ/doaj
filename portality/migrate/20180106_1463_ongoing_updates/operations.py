a6db59d1417280b12cae43fa490a5e82714ea003
Richard Jones <richard@cottagelabs.com>
2018-01-08 09:28:25 +0000

    modify migration so that current_journal is moved to related_journal when status is 'accepted' or 'rejected'

--- portality/migrate/20180106_1463_ongoing_updates/operations.py
+++ portality/migrate/20180106_1463_ongoing_updates/operations.py
@@ -1,6 +1,9 @@
 def clear_current_journal(application):
     if application.application_status in ["rejected", "accepted"]:
-        application.remove_current_journal()
+        cj = application.current_journal
+        if cj is not None:
+            application.remove_current_journal()
+            application.set_related_journal(cj)
     return application
 
 def relabel_status(application):
