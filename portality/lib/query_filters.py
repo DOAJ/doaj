379764e648cbbd9d57d6587bc91d616fb4dfa934
Steven Eardley <steve@eardley.xyz>
2018-05-23 16:43:05 +0100

    Added query filter for author emails
    https://github.com/DOAJ/doajPM/issues/1667

--- portality/lib/query_filters.py
+++ portality/lib/query_filters.py
@@ -5,25 +5,30 @@ from portality import models
 # query filters
 ###############
 
+
 def only_in_doaj(q):
     q.clear_match_all()
     q.add_must({"term" : {"admin.in_doaj" : True}})
     return q
+
 
 def owner(q):
     q.clear_match_all()
     q.add_must({"term" : {"admin.owner.exact" : current_user.id}})
     return q
 
+
 def update_request(q):
     q.clear_match_all()
     q.add_must({"range" : {"created_date" : {"gte" : app.config.get("UPDATE_REQUEST_SHOW_OLDEST")}}})
     return q
 
+
 def associate(q):
     q.clear_match_all()
     q.add_must({"term" : {"admin.editor.exact" : current_user.id}})
     return q
+
 
 def editor(q):
     gnames = []
@@ -33,6 +38,7 @@ def editor(q):
     q.clear_match_all()
     q.add_must({"terms" : {"admin.editor_group.exact" : gnames}})
     return q
+
 
 # results filters
 #################
@@ -51,6 +57,21 @@ def public_result_filter(results):
                         del hit["_source"]["admin"][k]
 
     return results
+
+
+def prune_author_emails(results):
+    if "hits" not in results:
+        return results
+    if "hits" not in results["hits"]:
+        return results
+
+    for hit in results["hits"]["hits"]:
+        if "_source" in hit:
+            if "bibjson" in hit["_source"]:
+                if "author" in hit["_source"]["bibjson"]:
+                    for a in hit["_source"]["bibjson"]["author"]:
+                        del a["email"]
+
 
 def publisher_result_filter(results):
     if "hits" not in results:
