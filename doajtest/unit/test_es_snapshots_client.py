641e17e7ad657b6a8aefd0cec303d62a69a949b3
Steven Eardley <steve@eardley.xyz>
2018-05-27 20:01:48 +0100

    Added some tests for the latest snapshot check, and the ordering of snapshot retrieval. https://github.com/DOAJ/doajPM/issues/1656

--- doajtest/unit/test_es_snapshots_client.py
+++ doajtest/unit/test_es_snapshots_client.py
@@ -2,31 +2,63 @@ from doajtest.helpers import DoajTestCase
 from portality.core import app
 from portality.lib import es_snapshots
 from doajtest.fixtures.snapshots import SNAPSHOTS_LIST
+from copy import deepcopy
+from datetime import datetime, timedelta
+from nose.tools import assert_raises
 import responses
-import freezegun
+from freezegun import freeze_time
 
 
 class TestSnapshotClient(DoajTestCase):
 
     def setUp(self):
+        self.snapshot_url = app.config.get('ELASTIC_SEARCH_HOST', 'http://localhost:9200') + '/_snapshot/' + app.config.get('ELASTIC_SEARCH_SNAPSHOT_REPOSITORY', 'doaj_s3')
         super(TestSnapshotClient, self).setUp()
 
     def tearDown(self):
+        responses.reset()
         super(TestSnapshotClient, self).tearDown()
 
     @responses.activate
     def test_01_list_snapshots(self):
-        snapshot_url = app.config.get('ELASTIC_SEARCH_HOST', 'http://localhost:9200') + '/_snapshot/' + app.config.get('ELASTIC_SEARCH_SNAPSHOT_REPOSITORY', 'doaj_s3')
-        responses.add(responses.GET, snapshot_url, json=SNAPSHOTS_LIST, status=200)
+        # Set up a mock response for the snapshots request
+        responses.add(responses.GET, self.snapshot_url, json=SNAPSHOTS_LIST, status=200)
+
+        # Ensure the client can list the snapshots correctly
         client = es_snapshots.ESSnapshotsClient()
-        s = client.list_snapshots()
-        assert s == SNAPSHOTS_LIST
+        snaps = client.list_snapshots()
+        assert snaps == [es_snapshots.ESSnapshot(s) for s in SNAPSHOTS_LIST['snapshots']]
 
+    @responses.activate
     def test_02_snapshots_are_sorted(self):
-        pass
+        # Set up a mock response for the snapshots request, this time in the wrong order
+        REVERSED_SNAPS = deepcopy(SNAPSHOTS_LIST)
+        REVERSED_SNAPS['snapshots'].reverse()
+        responses.add(responses.GET, self.snapshot_url, json=REVERSED_SNAPS, status=200)
 
+        # Ensure the client can list the snapshots in the correct order
+        client = es_snapshots.ESSnapshotsClient()
+        snaps = client.list_snapshots()
+        assert snaps != [es_snapshots.ESSnapshot(s) for s in REVERSED_SNAPS['snapshots']]
+        assert snaps == [es_snapshots.ESSnapshot(s) for s in SNAPSHOTS_LIST['snapshots']]
+
+    @responses.activate
     def test_03_todays_exists(self):
-        pass
+        # Set up a mock response for the snapshots request
+        responses.add(responses.GET, self.snapshot_url, json=SNAPSHOTS_LIST, status=200)
+
+        # Set the time to be that which will succeed according to the fixtures
+        latest_fixture_date = datetime.utcfromtimestamp(SNAPSHOTS_LIST['snapshots'][-1]['start_time_in_millis'] / 1000)
+        with freeze_time(latest_fixture_date):
+            # Ensure the client correctly identifies the last snapshot exists per the date set
+            client = es_snapshots.ESSnapshotsClient()
+            client.check_today_snapshot()
+
+        # And a day later it raises an exception, because that snapshot is missing
+        with assert_raises(es_snapshots.TodaySnapshotMissingException):
+            with freeze_time(latest_fixture_date + timedelta(days=1)):
+                client = es_snapshots.ESSnapshotsClient()
+                client.check_today_snapshot()
 
     def test_04_prune_snapshots(self):
         pass
--- portality/lib/es_snapshots.py
+++ portality/lib/es_snapshots.py
@@ -17,6 +17,15 @@ class ESSnapshot(object):
         self.name = snapshot_json['snapshot']
         self.datetime = datetime.utcfromtimestamp(snapshot_json['start_time_in_millis'] / 1000)
 
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
     def delete(self):
         pass
 
@@ -27,15 +36,16 @@ class ESSnapshotsClient(object):
         self.snapshots = []
 
     def list_snapshots(self):
-        snapshots_url = app.config.get('ELASTIC_SEARCH_HOST', 'http://localhost:9200') + '/_snapshot/' + app.config.get('ELASTIC_SEARCH_SNAPSHOT_REPOSITORY', 'doaj_s3')
-        resp = requests.get(snapshots_url)
-        print resp.text
-        return resp.json()
+        # If we don't have the snapshots, ask ES for them
+        if not self.snapshots:
+            snapshots_url = app.config.get('ELASTIC_SEARCH_HOST', 'http://localhost:9200') + '/_snapshot/' + app.config.get('ELASTIC_SEARCH_SNAPSHOT_REPOSITORY', 'doaj_s3')
+            resp = requests.get(snapshots_url)
 
-        # if self.snapshots:
-        #     return self.snapshots
-        # self.snapshots = [ordered ESSnapshot]
-        # return self.snapshots
+            if 'snapshots' in resp.json():
+                snap_objs = [ESSnapshot(s) for s in resp.json()['snapshots']]
+                self.snapshots = sorted(snap_objs, key=lambda x: x.datetime)
+
+        return self.snapshots
 
     def check_today_snapshot(self):
         snapshots = self.list_snapshots()
