81ca87a87eb344a61e6b41b98ca0bd268b3aea34
Steven Eardley <steve@eardley.xyz>
2018-12-19 19:20:57 +0000

    New deploy script and added production.cfg to repo with publicly readable configuration options.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+# Apt dependencies for the DOAJ app
+sudo apt-get update
+sudo apt-get install -q -y libxml2-dev libxslt-dev python-dev lib32z1-dev awscli
+
+# Run from the doaj folder that's already checked out
+
+# activate the virtualenv that we expect to be at /home/cloo/doaj
+. /home/cloo/doaj/activate
+cd /home/cloo/doaj/src/doaj
+
+# Install DOAJ submodules and requirements
+git submodule update --init --recursive
+pip install -r requirements.txt
+
+# Get the app configuration secrets from AWS
+aws --profile doaj-app secretsmanager get-secret-value --secret-id doaj/app-credentials | cut -f4 | base64 -D > app.cfg
+
+# Restart the app and web server with SIGHUP, the hangup signal to reload the configs.
+kill -HUP $(sudo supervisorctl pid doaj) || supervisorctl start doaj
+sudo nginx -t && sudo nginx -s reload
