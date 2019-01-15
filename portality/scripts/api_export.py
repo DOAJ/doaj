cb9bb7def8e8e8ffdbbf089d85e0ede6d63786e7
Richard Jones <richard@cottagelabs.com>
2019-01-15 16:56:17 +0000

    reworking the export script to optimise local disk usage and fix a few minor issues

--- portality/scripts/api_export.py
+++ portality/scripts/api_export.py
@@ -8,34 +8,22 @@ from portality.api.v1 import Api400Error
 from urlparse import parse_qs, urlparse
 from datetime import datetime
 
-def _save_file(typ, day_at_start, results, record_count):
+def _save_file(storage, container, typ, day_at_start, results, file_num, tarball):
     # Create a dir for today and save all files in there
-    file_count = 1
-    records_per_file = app.config.get('DISCOVERY_RECORDS_PER_FILE', 100000)
-    if (record_count % records_per_file) == 0:
-        file_count = (record_count / records_per_file) + 1
-
     data = json.dumps(results, cls=ModelJsonEncoder)
 
-    filename = os.path.join(day_at_start, "{typ}_{file_count}.json".format(typ=typ, file_count=file_count))
-    output_file = tmpStore.path(container, filename, create_container=True, must_exist=False)
+    filename = os.path.join("doaj_data_" + day_at_start, "{typ}_{file_num}.json".format(typ=typ, file_num=file_num))
+    output_file = storage.path(container, filename, create_container=True, must_exist=False)
     dn = os.path.dirname(output_file)
     if not os.path.exists(dn):
         os.makedirs(dn)
     print("Saving file {filename}".format(filename=filename))
-    with codecs.open(output_file, "ab", "utf-8") as out_file:
-        out_file.write(data + '\n')
+    with codecs.open(output_file, "wb", "utf-8") as out_file:
+        out_file.write(data)
 
-
-def _get_last_page(results):
-    url = results.data.get('last', '')
-    last_page = None
-    if url:
-        last_page = parse_qs(urlparse(url).query).get('page', [])
-    if last_page:
-        return int(last_page[0])
-    return None
-
+    print("Adding file {filename} to compressed tar".format(filename=filename))
+    tarball.add(output_file, arcname=filename)
+    storage.delete(container, filename)
 
 def _get_dir_size(path):
     total_size = 0
@@ -56,36 +44,36 @@ def _prune_container(day_at_start):
         return
     for container_file in container_files:
         if container_file != file_for_today:
-            mainStore.delete(target_name=fn)
+            mainStore.delete(target_name=container_file)
 
 
-def _copy_on_complete(path):
-    name = os.path.basename(path)
-    raw_size = _get_dir_size(path)
-    print("Compressing temporary file {0} ({1} bytes)".format(path, raw_size))
-    zipped_name = name + ".gz"
-    zip_dir = os.path.dirname(path)
-    zipped_path = os.path.join(zip_dir, zipped_name)
-    with tarfile.open(zipped_path, "w:gz") as tar_fo:
-        tar_fo.add(path)
+def _copy_on_complete(zipped_path):
+    #name = os.path.basename(path)
+    #raw_size = _get_dir_size(path)
+    #print("Compressing temporary directory {0} ({1} bytes)".format(path, raw_size))
+    #zipped_name = name + ".gz"
+    #zip_dir = os.path.dirname(path)
+    #zipped_path = os.path.join(zip_dir, zipped_name)
+    #with tarfile.open(zipped_path, "w:gz") as tar_fo:
+    #    tar_fo.add(path)
     zipped_size = os.path.getsize(zipped_path)
     print("Storing from temporary file {0} ({1} bytes)".format(zipped_name, zipped_size))
     mainStore.store(container, zipped_name, source_path=zipped_path)
-    tmpStore.delete(container, name)
+    # tmpStore.delete(container, name)
     tmpStore.delete(container, zipped_name)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--type", choices=['article','journal', 'all'], help="type of data to export. ")
+    parser.add_argument("type", choices=['article','journal', 'all'], help="type of data to export. ")
     parser.add_argument("-c", "--clean", action="store_false", help="Clean any pre-existing output before continuing")
     parser.add_argument("-p", "--prune", action="store_true", help="Delete previous backups if anyafter running current backup")
     args = parser.parse_args()
 
     tmpStore = StoreFactory.tmp()
     mainStore = StoreFactory.get("api_data")
-    container = app.config.get("STORE_ANON_DATA_CONTAINER")
+    container = app.config.get("STORE_API_DATA_CONTAINER")
 
     if args.clean:
         mainStore.delete(container)
@@ -96,28 +84,48 @@ if __name__ == '__main__':
     # Do the search and save it
     query = '*'
     page_size = app.config.get("DISCOVERY_BULK_PAGE_SIZE", 1000)
+    records_per_file = app.config.get('DISCOVERY_RECORDS_PER_FILE', 100000)
 
     if args.type == 'all':
         types = ['article', 'journal']
     else:
         types = [args.type]
 
+    out_dir = tmpStore.path(container, "doaj_data_" + day_at_start, create_container=True, must_exist=False)
+    out_name = os.path.basename(out_dir)
+    zipped_name = out_name + ".tar.gz"
+    zip_dir = os.path.dirname(out_dir)
+    zipped_path = os.path.join(zip_dir, zipped_name)
+    tarball = tarfile.open(zipped_path, "w:gz")
+
+
     # Scroll for article and/or journal
     for typ in types:
+        batch = []
+        file_num = 1
         print("\n" + dates.now() + ": Starting download of " + typ)
-        count = 0
-        for result in DiscoveryApi.scroll(typ, None, query, page_size)
-            count += 1
-            _save_file(typ, day_at_start, result, count)
 
+        for result in DiscoveryApi.scroll(typ, None, query, page_size):
+            batch.append(result)
+
+            if len(batch) >= records_per_file:
+                _save_file(tmpStore, container, typ, day_at_start, batch, file_num, tarball)
+                batch = []
+                file_num += 1
+
+        if len(batch) > 0:
+            _save_file(tmpStore, container, typ, day_at_start, batch, file_num, tarball)
+
+    tarball.close()
     print(dates.now() + ": done\n")
 
     # Copy the source directory to main store
-    out_dir = tmpStore.path(container, day_at_start)
+    # out_dir = tmpStore.path(container, day_at_start)
     try:
-        _copy_on_complete(out_dir)
+        _copy_on_complete(zipped_path)
     except Exception as e:
         # TODO: Name exceptions to catch
+        tmpStore.delete(container)
         raise StoreException("Error copying data on complete\n" + e.message)
 
     if args.prune:
