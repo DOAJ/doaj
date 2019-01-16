01efe9ef490ee4e1ce5073ec4be069e7d08d3b54
Steven Eardley <steve@eardley.xyz>
2019-01-16 11:44:34 +0000

    Edit the deployment script to take into account test server env

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Environment is a required arg to this script
+[ $# -ne 1 ] && echo "Call this script as `basename "$0"` <environment: [production, test]>" && exit 1
+ENV=$1
+
 # apt dependencies for the DOAJ app
 sudo apt-get update
 sudo apt-get install -q -y libxml2-dev libxslt-dev python-dev lib32z1-dev awscli
@@ -15,7 +19,14 @@ git submodule update --init --recursive
 pip install -r requirements.txt
 
 # Get the app configuration secrets from AWS  - NOTE: on a mac, base64 needs -D rather than -d
-aws --profile doaj-app secretsmanager get-secret-value --secret-id doaj/app-credentials | cut -f4 | base64 -d > app.cfg
+if [ "$ENV" = 'production' ]
+then
+    aws --profile doaj-app secretsmanager get-secret-value --secret-id doaj/app-credentials | cut -f4 | base64 -d > app.cfg
+elif [ "$ENV" = 'test' ]
+then
+    aws --profile doaj-test secretsmanager get-secret-value --secret-id doaj/test-credentials | cut -f4 | base64 -d > app.cfg
+fi
+
 
 # Restart all supervisor tasks, which will cover the app, and huey on the background server. Then reload nginx.
 sudo supervisorctl reread
