1194a4f846c5fb4d05154176814ab5a2f8ffea4f
Richard Jones <richard@cottagelabs.com>
2019-03-30 05:48:43 +0000

    add general support for reporting to csv and implement for journals missing q14 and q18 from the application form

--- portality/lib/report_to_csv.py
+++ portality/lib/report_to_csv.py
@@ -1,2 +1,23 @@
-def report_to_csv(record_feed=None, query=None, type=None, headers=None, convert_record=None, field_to_header=None):
-    pass
+import esprit, codecs
+
+from portality.core import app
+from portality.clcsv import UnicodeWriter
+
+
+def query_result_generator(query, type, page_size=1000, keepalive="1m"):
+    conn = esprit.raw.make_connection(None, app.config["ELASTIC_SEARCH_HOST"], None, app.config["ELASTIC_SEARCH_DB"])
+    for result in esprit.tasks.scroll(conn, type, q=query, page_size=page_size, keepalive=keepalive):
+        yield result
+
+
+def report_to_csv(result_generator, headers, output_map, output_file):
+    with codecs.open(output_file, "wb", "utf-8") as f:
+        writer = UnicodeWriter(f)
+        writer.writerow(headers)
+
+        for result in result_generator:
+            record = output_map(result)
+            row = []
+            for h in headers:
+                row.append(record[h])
+            writer.writerow(row)
