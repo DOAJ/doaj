14281ff10f5b05aa336c27842f2ee26a29d98ceb
Richard Jones <richard@cottagelabs.com>
2019-05-07 09:26:00 +0100

    partial fix up of the administrative site search.  This is a complicated one because it's a search interface and a bulk edit form which are woven together.  Current state is that search is working, and we're attempting to get the layout of the forms to work (fixing that should sort out much of the other form stuff for bs3 too), then after that we need to confirm that the bulk edit is actually still working

--- portality/static/doaj/js/facetview_results_render_callbacks.js
+++ portality/static/doaj/js/facetview_results_render_callbacks.js
@@ -1,37 +1,6 @@
 // use a closure to allow attaching the mapping of values directly to
 // the function, a bit cleaner
-fv_author_pays = (function(resultobj) {
-    var that = function(resultobj) {
-        var field = "";
-        if (resultobj.bibjson && resultobj.bibjson.author_pays) {
-            if(that.mapping[resultobj['bibjson']['author_pays']]) {
-                var result = '<span class=' + that.mapping[resultobj['bibjson']['author_pays']]['class'] + '>';
-                result += that.mapping[resultobj['bibjson']['author_pays']]['text'];
-                result += '</span>';
-                field += result;
-            } else {
-                field += resultobj['bibjson']['author_pays'];
-            }
-            if (resultobj.bibjson && resultobj.bibjson.author_pays_url) {
-                var url = resultobj.bibjson.author_pays_url;
-                field += " (see <a href='" + url + "'>" + url + "</a>)"
-            }
-            if (field === "") {
-                return false
-            }
-            return field
-        }
-        return false;
-    };
-    return that;
-})();
 
-fv_author_pays.mapping = {
-    "Y": {"text": "Has charges", "class": "red"},
-    "N": {"text": "No charges", "class": "green"},
-    "CON": {"text": "Conditional charges", "class": "blue"},
-    "NY": {"text": "No info available", "class": ""}
-};
 
 fv_application_status = (function(resultobj) {
     var that = function(resultobj) {
@@ -61,19 +30,7 @@ fv_created_date = (function (resultobj) {
     return that;
 })();
 
-fv_created_date_with_time = (function (resultobj) {
-    var that = function(resultobj) {
-        return iso_datetime2date_and_time(resultobj['created_date']);
-    };
-    return that;
-})();
 
-fv_last_updated = (function (resultobj) {
-    var that = function(resultobj) {
-        return iso_datetime2date_and_time(resultobj['last_updated']);
-    };
-    return that;
-})();
 
 fv_last_manual_update = (function (resultobj) {
     var that = function(resultobj) {
@@ -99,157 +56,6 @@ fv_suggested_on = (function (resultobj) {
     return that;
 })();
 
-
-fv_abstract = (function (resultobj) {
-    var that = function(resultobj) {
-        if (resultobj['bibjson']['abstract']) {
-            var result = '<a class="abstract_action" href="" rel="';
-            result += resultobj['id'];
-            result += '">(show/hide)</a> <span class="abstract_text" rel="';
-            result += resultobj['id'];
-            result += '">' + '<br>';
-            result += escapeHtml(resultobj['bibjson']['abstract']);
-            result += '</span>';
-            return result;
-        }
-        return false;
-    };
-    return that;
-})();
-
-
-fv_journal_license = (function (resultobj) {
-    var that = function(resultobj) {
-        var title = undefined;
-        if (resultobj.bibjson && resultobj.bibjson.journal && resultobj.bibjson.journal.license) {
-            var lics = resultobj["bibjson"]["journal"]["license"];
-            if (lics.length > 0) {
-                title = lics[0].title
-            }
-        }
-        else if (resultobj.bibjson && resultobj.bibjson.license) {
-            var lics = resultobj["bibjson"]["license"];
-            if (lics.length > 0) {
-                title = lics[0].title
-            }
-        }
-        
-        if (title) {
-            if (CC_MAP[title]) {
-                var urls = CC_MAP[title];
-                // i know i know, i'm not using styles.  the attrs still work and are easier.
-                return "<a href='" + urls[1] + "' title='" + title + "' target='blank'><img src='" + urls[0] + "' width='80' height='15' valign='middle' alt='" + title + "'></a>"
-            } else {
-                return title
-            }
-        }
-        
-        return false;
-    };
-    return that;
-})();
-
-CC_MAP = {
-    "CC BY" : ["/static/doaj/images/cc/by.png", "https://creativecommons.org/licenses/by/4.0/"],
-    "CC BY-NC" : ["/static/doaj/images/cc/by-nc.png", "https://creativecommons.org/licenses/by-nc/4.0/"],
-    "CC BY-NC-ND" : ["/static/doaj/images/cc/by-nc-nd.png", "https://creativecommons.org/licenses/by-nc-nd/4.0/"],
-    "CC BY-NC-SA" : ["/static/doaj/images/cc/by-nc-sa.png", "https://creativecommons.org/licenses/by-nc-sa/4.0/"],
-    "CC BY-ND" : ["/static/doaj/images/cc/by-nd.png", "https://creativecommons.org/licenses/by-nd/4.0/"],
-    "CC BY-SA" : ["/static/doaj/images/cc/by-sa.png", "https://creativecommons.org/licenses/by-sa/4.0/"]
-};
-
-fv_title_field = (function (resultobj) {
-    var that = function(resultobj) {
-        var field = '<span class="title">';
-        var isjournal = false;
-        if (resultobj.bibjson && resultobj.bibjson.journal) {
-            // this is an article
-            field += "<i class='fa fa-file'></i>";
-        }
-        else if (resultobj.suggestion) {
-            // this is a suggestion
-            field += "<i class='fa fa-sign-in' style=\"margin-right: 0.5em;\"></i>";
-        } else {
-            // this is a journal
-            field += "<i class='fa fa-book'></i>";
-            isjournal = true;
-        }
-        if (resultobj.bibjson.title) {
-            if (isjournal) {
-                field += "&nbsp<a href='/toc/" + journal_toc_id(resultobj) + "'>" + escapeHtml(resultobj.bibjson.title) + "</a>";
-            } else {
-                field += "&nbsp" + escapeHtml(resultobj.bibjson.title);
-            }
-            if (resultobj.admin && resultobj.admin.ticked) {
-                field += "&nbsp<img src='/static/doaj/images/tick_short.png' width='16px' height='16px' title='Accepted after March 2014' alt='Tick icon: journal was accepted after March 2014' style='padding-bottom: 3px'>​​";
-            }
-            if (resultobj.admin && resultobj.admin.seal) {
-                field += "&nbsp<img src='/static/doaj/images/seal_short.png' width='16px' height='16px' title='Awarded the DOAJ Seal' alt='Seal icon: awarded the DOAJ Seal' style='padding-bottom: 3px'>​​";
-            }
-            return field + "</span>"
-        } else {
-            return false;
-        }
-    };
-    return that;
-})();
-
-fv_doi_link = (function (resultobj) {
-    var that = function(resultobj) {
-        if (resultobj.bibjson && resultobj.bibjson.identifier) {
-            var ids = resultobj.bibjson.identifier;
-            for (var i = 0; i < ids.length; i++) {
-                if (ids[i].type === "doi") {
-                    var doi = ids[i].id;
-                    var tendot = doi.indexOf("10.");
-                    var url = "https://doi.org/" + doi.substring(tendot);
-                    return "<a href='" + url + "'>" + escapeHtml(doi.substring(tendot)) + "</a>"
-                }
-            }
-        }
-        return false
-    };
-    return that;
-})();
-
-fv_links = (function (resultobj) {
-    var that = function(resultobj) {
-        if (resultobj.bibjson && resultobj.bibjson.link) {
-            var ls = resultobj.bibjson.link;
-            for (var i = 0; i < ls.length; i++) {
-                var t = ls[i].type;
-                var label = '';
-                if (t == 'fulltext') {
-                    label = 'Full text'
-                } else if (t == 'homepage') {
-                    label = 'Home page'
-                } else {
-                    label = t.substring(0, 1).toUpperCase() + t.substring(1)
-                }
-                return "<strong>" + label + "</strong>: <a href='" + ls[i].url + "'>" + escapeHtml(ls[i].url) + "</a>"
-            }
-        }
-        return false;
-    };
-    return that;
-})();
-
-fv_issns = (function (resultobj) {
-    var that = function(resultobj) {
-        if (resultobj.bibjson && resultobj.bibjson.identifier) {
-            var ids = resultobj.bibjson.identifier;
-            var issns = [];
-            for (var i = 0; i < ids.length; i++) {
-                if (ids[i].type === "pissn" || ids[i].type === "eissn") {
-                    issns.push(escapeHtml(ids[i].id))
-                }
-            }
-            return issns.join(", ")
-        }
-        return false
-    };
-    return that;
-})();
 
 fv_edit_suggestion = (function (resultobj) {
     var that = function(resultobj) {
@@ -293,40 +99,7 @@ fv_readonly_journal = (function (resultobj) {
 })();
 
 
-fv_delete_article = (function (resultobj) {
-    var that = function(resultobj) {
-        if (!resultobj.suggestion && resultobj.bibjson.journal) {
-            // if it's not a suggestion or a journal .. (it's an article!)
-            // we really need to expose _type ...
-            var result = '<a class="delete_article_link pull-right" href="';
-            result += "/admin/article/";
-            result += resultobj['id'];
-            result += '" target="_blank"';
-            result += '>Delete this article</a>';
-            return result;
-        }
-        return false;
-    };
-    return that;
-})();
 
-fv_edit_journal = (function (resultobj) {
-    var that = function(resultobj) {
-        if (!resultobj.suggestion && !resultobj.bibjson.journal) {
-            // if it's not a suggestion or an article .. (it's a
-            // journal!)
-            // we really need to expose _type ...
-            var result = '<a class="edit_journal_link pull-right" href="';
-            result += journal_edit_url;
-            result += resultobj['id'];
-            result += '" target="_blank"';
-            result += '>Edit this journal</a>';
-            return result;
-        }
-        return false;
-    };
-    return that;
-})();
 
 fv_make_continuation = (function (resultobj) {
     var that = function(resultobj) {
@@ -355,43 +128,6 @@ fv_make_continuation = (function (resultobj) {
     return that;
 })();
 
-
-fv_in_doaj = (function(resultobj) {
-    var that = function(resultobj) {
-        var field = "";
-        if (resultobj.admin && resultobj.admin.in_doaj !== undefined) {
-            if(that.mapping[resultobj['admin']['in_doaj']]) {
-                var result = '<span class=' + that.mapping[resultobj['admin']['in_doaj']]['class'] + '>';
-                result += that.mapping[resultobj['admin']['in_doaj']]['text'];
-                result += '</span>';
-                field += result;
-            } else {
-                field += resultobj['admin']['in_doaj'];
-            }
-            if (field === "") {
-                return false
-            }
-            return field
-        }
-        return false;
-    };
-    return that;
-})();
-
-fv_in_doaj.mapping = {
-    "false": {"text": "No", "class": "red"},
-    "true": {"text": "Yes", "class": "green"}
-};
-
-fv_country_name = (function (resultobj) {
-    var that = function(resultobj) {
-        if (resultobj.index && resultobj.index.country) {
-            return escapeHtml(resultobj.index.country);
-        }
-        return false
-    };
-    return that;
-})();
 
 fv_owner = (function (resultobj) {
     var that = function(resultobj) {
