083fa402676b8a973f9fc0fa6d11b1b7a38487ae
Richard Jones <richard@cottagelabs.com>
2019-05-16 10:51:08 +0100

    keep huey deployment scripts for development purposes, and update the long running consumer to import the public data dump task queue

--- portality/tasks/consumer_long_running.py
+++ portality/tasks/consumer_long_running.py
@@ -11,3 +11,4 @@ from portality.tasks.redis_huey import long_running
 # these are the ones that bind to the long_running queue
 from portality.tasks.article_cleanup_sync import scheduled_article_cleanup_sync, article_cleanup_sync
 from portality.tasks.prune_es_backups import scheduled_prune_es_backups, prune_es_backups
+from portality.tasks.public_data_dump import scheduled_public_data_dump, public_data_dump
