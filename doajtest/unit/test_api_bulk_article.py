bf01db20dded91cafda12ba079bdd339e11fba6b
Steven Eardley <steve@eardley.xyz>
2019-06-12 14:08:53 +0200

    Test the bulk API root

--- doajtest/unit/test_api_bulk_article.py
+++ doajtest/unit/test_api_bulk_article.py
@@ -2,9 +2,12 @@ from doajtest.helpers import DoajTestCase
 from portality.api.v1 import ArticlesBulkApi, Api401Error, Api400Error
 from portality import models
 from doajtest.fixtures import ArticleFixtureFactory, JournalFixtureFactory
-import time
 from copy import deepcopy
 from portality.dao import ESMappingMissingError
+from flask import url_for
+import json
+import time
+
 
 class TestCrudArticle(DoajTestCase):
 
@@ -48,9 +51,7 @@ class TestCrudArticle(DoajTestCase):
         # add a journal to the account
         journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
         journal.set_owner(account.id)
-        journal.save()
-
-        time.sleep(2)
+        journal.save(blocking=True)
 
         # call create on the object (which will save it to the index)
         ids = ArticlesBulkApi.create(dataset, account)
@@ -60,11 +61,11 @@ class TestCrudArticle(DoajTestCase):
         assert len(list(set(ids))) == 10, len(list(set(ids)))  # are they actually 10 unique IDs?
 
         # let the index catch up
-        time.sleep(2)
+        time.sleep(0.6)
 
         # check that each id was actually created
-        for id in ids:
-            s = models.Article.pull(id)
+        for _id in ids:
+            s = models.Article.pull(_id)
             assert s is not None
 
     def test_02_create_duplicate_articles(self):
@@ -81,15 +82,13 @@ class TestCrudArticle(DoajTestCase):
         # add a journal to the account
         journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
         journal.set_owner(account.id)
-        journal.save()
-
-        time.sleep(2)
+        journal.save(blocking=True)
 
         # call create on the object (which will save it to the index)
         with self.assertRaises(Api400Error):
             ids = ArticlesBulkApi.create(dataset, account)
 
-        time.sleep(2)
+        time.sleep(0.6)
 
         with self.assertRaises(ESMappingMissingError):
             all_articles = models.Article.all()
@@ -114,8 +113,7 @@ class TestCrudArticle(DoajTestCase):
             # add a journal to the account
             journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
             journal.set_owner(account.id)
-            journal.save()
-            time.sleep(1)
+            journal.save(blocking=True)
             dataset = dataset[:5] + [{"some" : {"junk" : "data"}}] + dataset[5:]
             ids = ArticlesBulkApi.create(dataset, account)
 
@@ -138,21 +136,20 @@ class TestCrudArticle(DoajTestCase):
         # add a journal to the account
         journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
         journal.set_owner(account.id)
-        journal.save()
-        time.sleep(1)
+        journal.save(blocking=True)
 
         # call create on the objects (which will save it to the index)
         ids = ArticlesBulkApi.create(dataset, account)
 
         # let the index catch up
-        time.sleep(2)
+        time.sleep(0.6)
 
         # now delete half of them
         dels = ids[:5]
         ArticlesBulkApi.delete(dels, account)
 
         # let the index catch up
-        time.sleep(2)
+        time.sleep(0.6)
 
         for id in dels:
             ap = models.Article.pull(id)
@@ -183,14 +180,13 @@ class TestCrudArticle(DoajTestCase):
         # between account and articles
         journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
         journal.set_owner(article_owner.id)
-        journal.save()
-        time.sleep(1)
+        journal.save(blocking=True)
 
         # call create on the objects (which will save it to the index)
         ids = ArticlesBulkApi.create(dataset, article_owner)
 
         # let the index catch up
-        time.sleep(2)
+        time.sleep(0.6)
 
         # call delete on the object in various context that will fail
 
@@ -212,4 +208,74 @@ class TestCrudArticle(DoajTestCase):
         with self.assertRaises(Api400Error):
             ArticlesBulkApi.delete(ids, article_owner)
 
+    def test_06_test_via_endpoint(self):
+        """ Use a request context to test the API via the route """
+
+        # set up all the bits we need
+        dataset = []
+        for i in range(10):
+            data = ArticleFixtureFactory.make_incoming_api_article(doi="10.123/test/" + str(i),
+                                                                   fulltext="http://example.com/" + str(i))
+            dataset.append(data)
+
+        # create the main account we're going to work as
+        article_owner = models.Account()
+        article_owner.set_id("test")
+        article_owner.set_name("Tester")
+        article_owner.set_email("test@test.com")
+        article_owner.generate_api_key()
+        article_owner.add_role('publisher')
+        article_owner.add_role('api')
+        article_owner.save(blocking=True)
+        
+        # Add another user who doesn't own these articles
+        somebody_else = models.Account()
+        somebody_else.set_id("somebody_else")
+        somebody_else.set_name("Somebody Else")
+        somebody_else.set_email("somebodyelse@test.com")
+        somebody_else.generate_api_key()
+        somebody_else.add_role('publisher')
+        somebody_else.add_role('api')
+        somebody_else.save(blocking=True)
+
+        assert article_owner.api_key != somebody_else.api_key
+
+        # add a journal to the article owner account to create that link between account and articles
+        journal = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
+        journal.set_owner(article_owner.id)
+        journal.save(blocking=True)
+
+        with self.app_test.test_request_context():
+            with self.app_test.test_client() as t_client:
+
+                # Bulk create
+                # The wrong owner can't create articles
+                resp = t_client.post(url_for('api_v1.bulk_article_create', api_key=somebody_else.api_key),
+                                     data=json.dumps(dataset))
+                assert resp.status_code == 400
+
+                # Bur the correct owner can create articles
+                resp = t_client.post(url_for('api_v1.bulk_article_create', api_key=article_owner.api_key),
+                                     data=json.dumps(dataset))
+                assert resp.status_code == 201
+                reply = json.loads(resp.data)
+                assert len(reply) == len(dataset)
+                first_art = reply.pop()
+                assert first_art['status'] == 'created'
+                # Check we actually created new records
+                time.sleep(1)
+                assert len(models.Article.all()) == len(dataset)
+
+                # Bulk delete
+                all_but_one = [new_art['id'] for new_art in reply]
+                resp = t_client.delete(url_for('api_v1.bulk_article_delete', api_key=article_owner.api_key),
+                                       data=json.dumps(all_but_one))
+                assert resp.status_code == 204
+                time.sleep(1)
+                # we should have deleted all but one of the articles.
+                assert len(models.Article.all()) == 1
+                # And our other user isn't allowed to delete the remaining one.
+                resp = t_client.delete(url_for('api_v1.bulk_article_delete', api_key=somebody_else.api_key),
+                                       data=json.dumps([first_art['id']]))
+                assert resp.status_code == 400
 
