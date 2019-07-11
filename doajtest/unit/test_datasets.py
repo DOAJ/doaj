e7337e9a13b2d5f350b87534b712e322bea2056b
Steve Eardley <steve@eardley.xyz>
2019-07-11 17:37:36 +0100

    tests for datasets

---
+++ doajtest/unit/test_datasets.py
@@ -0,0 +1,41 @@
+from portality import datasets
+from doajtest.helpers import DoajTestCase
+
+
+class TestDatasets(DoajTestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_01_countries(self):
+        """ Use country information from our datasets """
+        assert datasets.get_country_code('united kingdom') == 'GB'
+        assert datasets.get_country_name('GB') == u'United Kingdom'
+
+        # If the country is unrecognised, we send it back unchanged.
+        assert datasets.get_country_code('narnia') == 'narnia'
+        assert datasets.get_country_name('narnia') == 'narnia'
+
+        # When we have more than one option, the first alphabetically is returned
+        assert datasets.get_country_code('united') == 'AE'
+        assert datasets.get_country_name('AE') == u'United Arab Emirates'
+
+    def test_02_currencies(self):
+        """ Utilise currency information from the datasets """
+        assert datasets.get_currency_code('yen') == 'JPY'
+        assert datasets.get_currency_name('JPY') == 'JPY - Yen'
+
+        assert datasets.get_currency_code('pound sterling') == 'GBP'
+        assert datasets.get_currency_name('GBP') == u'GBP - Pound Sterling'
+
+        assert datasets.get_currency_code('pound') is None
+        assert datasets.get_currency_code('doubloons') is None
+
+    def test_03_languages(self):
+        """ Use language information from our datasets """
+        assert datasets.language_for('en') == [u'eng', u'', u'en', u'English', u'anglais']
+        assert datasets.name_for_lang('en') == u'English'
+        assert datasets.name_for_lang('eng') == u'English'
+        assert datasets.name_for_lang('anglais') == u'English'
