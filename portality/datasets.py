2fa3d27b82d403c2fe598733f862bbb23405c28e
Steve Eardley <steve@eardley.xyz>
2019-07-12 20:41:55 +0100

    Removed the countries file, and build the currency and country choices from pycountry.

--- portality/datasets.py
+++ portality/datasets.py
@@ -1,52 +1,35 @@
-# -*- coding: UTF-8 -*-
-# the comment above is for the Python interpreter, there are Unicode
-# characters written straight into this source file
-
 import pycountry
-import json
 import sys
-import os
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     from portality.ordereddict import OrderedDict
 else:
     from collections import OrderedDict
 
-# countries
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", 'country-codes.json'), 'rb') as f:
-    countries = json.loads(f.read())
-countries_dict = OrderedDict(sorted(countries.items(), key=lambda x: x[1]['name']))
-countries = countries_dict.items()
-
-country_options = [('','')]
+# Gather the countries with 2-character codes
+country_options = [('', '')]
 country_options_two_char_code_index = []
 
+for co in sorted(pycountry.countries, key=lambda x: x.name):
+    try:
+        country_options.append((co.alpha_2.upper(), co.name))
+        country_options_two_char_code_index.append(co.alpha_2.upper())
+    except AttributeError:
+        continue
+
+# currencies
 CURRENCY_DEFAULT = ''
 currency_options = [(CURRENCY_DEFAULT, CURRENCY_DEFAULT)]
 currency_options_code_index = []
 currency_name_opts = []
 
-for code, country_info in countries:        # FIXME: a bit of a mess - would be better to have an object that just gave us the answers on demand
-    country_options.append((code, country_info['name']))
-    country_options_two_char_code_index.append(code)
-    if 'currency_alphabetic_code' in country_info and 'currency_name' in country_info:
-        if country_info['currency_alphabetic_code'] not in currency_options_code_index:
-            # prevent duplicates in the currency options by checking if
-            # that currency has already been added - multiple countries
-            # use the same currency after all (GBP, EUR..)
-            currency_options.append(
-                (
-                    country_info['currency_alphabetic_code'],
-                    country_info['currency_alphabetic_code'] + ' - ' + country_info['currency_name']
-                )
-            )
-            currency_name_opts.append(
-                (
-                    country_info['currency_alphabetic_code'],
-                    country_info['currency_name']
-                )
-            )
-            currency_options_code_index.append(country_info['currency_alphabetic_code'])
+for cu in sorted(pycountry.currencies, key=lambda x: x.alpha_3):
+    try:
+        currency_options.append((cu.alpha_3, u'{code} - {name}'.format(code=cu.alpha_3, name=cu.name)))
+        currency_name_opts.append((cu.alpha_3, cu.name))
+        currency_options_code_index.append(cu.alpha_3)
+    except AttributeError:
+        continue
 
 currencies_dict = dict(currency_options)
 currency_name_map = dict(currency_name_opts)
@@ -75,8 +58,9 @@ licenses = {
     "CC BY-NC-SA": {'BY': True, 'NC': True, 'ND': False, 'SA': True, 'form_label': 'CC BY-NC-SA'},
 }
 
+# do not change this - the top-level keys in the licenses dict should always be == to the "type" of each license object
 for lic_type, lic_info in licenses.iteritems():
-    lic_info['type'] = lic_type  # do not change this - the top-level keys in the licenses dict should always be == to the "type" of each license object
+    lic_info['type'] = lic_type
     lic_info['title'] = lic_type
 
 license_dict = OrderedDict(sorted(licenses.items(), key=lambda x: x[1]['type']))
@@ -114,7 +98,7 @@ def get_currency_name(code):
     """ get the name of a currency from its code """
     try:
         cur = pycountry.currencies.lookup(code)
-        return '{code} - {name}'.format(code=cur.alpha_3, name=cur.name)
+        return u'{code} - {name}'.format(code=cur.alpha_3, name=cur.name)
     except LookupError:
         return code  # return what was passed in if not found
 
