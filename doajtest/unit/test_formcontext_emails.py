7c7c4b4f10093223aba4f5863137187e3fbb46d1
Aga Domanska <aga@cottagelabs.com>
2019-09-27 13:00:34 +0100

    fix tests

--- doajtest/unit/test_formcontext_emails.py
+++ doajtest/unit/test_formcontext_emails.py
@@ -286,7 +286,7 @@ class TestApplicationReviewEmails(DoajTestCase):
                                             info_stream_contents,
                                             re.DOTALL)
         assert bool(publisher_email_matched)
-        assert len(re.findall(email_count_string, info_stream_contents)) == 3
+        assert len(re.findall(email_count_string, info_stream_contents)) == 2
 
         # Clear the stream for the next part
         self.info_stream.truncate(0)
@@ -472,7 +472,7 @@ class TestApplicationReviewEmails(DoajTestCase):
                                             info_stream_contents,
                                             re.DOTALL)
         assert bool(publisher_email_matched)
-        assert len(re.findall(email_count_string, info_stream_contents)) == 3
+        assert len(re.findall(email_count_string, info_stream_contents)) == 2
 
         # Clear the stream for the next part
         self.info_stream.truncate(0)
@@ -573,8 +573,8 @@ class TestApplicationReviewEmails(DoajTestCase):
         info_stream_contents = self.info_stream.getvalue()
 
         # We expect one email to be sent here:
-        #   * to the publisher, notifying that an editor is viewing their application
-        publisher_template = re.escape('publisher_application_inprogress.txt')
+        #   * to the account owner, notifying that an editor is viewing their application
+        publisher_template = re.escape('publisher_update_request_inprogress.txt')
         publisher_to = re.escape(owner.email)
         publisher_subject = 'your application is under review'
 
@@ -582,7 +582,7 @@ class TestApplicationReviewEmails(DoajTestCase):
                                             info_stream_contents,
                                             re.DOTALL)
         assert bool(publisher_email_matched)
-        assert len(re.findall(email_count_string, info_stream_contents)) == 2
+        assert len(re.findall(email_count_string, info_stream_contents)) == 1
 
         # Clear the stream for the next part
         self.info_stream.truncate(0)
