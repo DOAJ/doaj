9ff1f32deb741496d775e7c7b1003a5bf1fbba90
Richard Jones <richard@cottagelabs.com>
2019-10-11 11:11:48 +0100

    add script for running tests with coverage output

---
+++ coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+###############################################
+## Execute the test coverage analysis
+###############################################
+
+# Specify the directory you want the output data and report to go to
+OUTDIR=../doaj-docs/python3_edges_bs3_xml/coverage
+
+
+COVERAGE_FILE=$OUTDIR/coverage.data
+export COVERAGE_FILE
+
+coverage run --source=portality,esprit,combinatrix,dictdiffer $(which nosetests) doajtest/unit/
+coverage html --include=portality*.py -d $OUTDIR/report
+
