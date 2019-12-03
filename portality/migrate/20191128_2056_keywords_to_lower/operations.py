d08092675f1fc7fefb1fb3fd0fb5318b3a6d556d
Aga Domanska <aga@cottagelabs.com>
2019-12-03 13:56:48 +0000

    naming

--- portality/migrate/20191128_2056_keywords_to_lower/operations.py
+++ portality/migrate/20191128_2056_keywords_to_lower/operations.py
@@ -3,7 +3,7 @@ from portality.core import app
 import esprit
 import re
 
-def rewrite_keywords(journal):
-    bib = journal.bibjson()
+def rewrite_keywords(journal_like):
+    bib = journal_like.bibjson()
     kwords = [k.lower() for k in bib.keywords]
     bib.set_keywords(kwords)
