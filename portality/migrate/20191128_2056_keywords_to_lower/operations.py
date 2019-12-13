0b5d8476c7656d6088fd4cf62bdcbce6bd8dfd4c
Aga Domanska <aga@cottagelabs.com>
2019-12-13 10:13:53 +0000

    remove unused imports

--- portality/migrate/20191128_2056_keywords_to_lower/operations.py
+++ portality/migrate/20191128_2056_keywords_to_lower/operations.py
@@ -1,8 +1,3 @@
-from portality import models
-from portality.core import app
-import esprit
-import re
-
 def rewrite_keywords(journal_like):
     bib = journal_like.bibjson()
     kwords = [k.lower() for k in bib.keywords]
