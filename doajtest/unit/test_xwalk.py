6497810cfbac4fde1b24791361d5910bd3e5ed1f
Aga Domanska <aga@cottagelabs.com>
2020-01-17 11:11:57 +0000

    add crossref xwalk UT

--- doajtest/unit/test_xwalk.py
+++ doajtest/unit/test_xwalk.py
@@ -2,6 +2,7 @@ from doajtest.fixtures.article_doajxml import DoajXmlArticleFixtureFactory
 from doajtest.fixtures.article_crossref import CrossrefArticleFixtureFactory
 from doajtest.helpers import DoajTestCase, diff_dicts
 from portality.crosswalks.article_doaj_xml import DOAJXWalk
+from portality.crosswalks.article_crossref_xml import CrossrefXWalk
 from portality.formcontext import xwalk, forms
 from portality import models
 from werkzeug.datastructures import MultiDict
@@ -124,7 +125,20 @@ class TestXwalk(DoajTestCase):
         assert bibjson.get_one_identifier(bibjson.E_ISSN) == "9876-5432", "expected '9876-5432', received: {}".format(bibjson.get_one_identifier(bibjson.E_ISSN))
         assert bibjson.year == "2013", "expected '2013', received: {}".format(bibjson.year)
         assert bibjson.title == "Imaginaires autochtones contemporains. Introduction", "expected 'Imaginaires autochtones contemporains. Introduction', received: {}".format(bibjson.title)
-        assert bibjson.author == [{'name': 'Papillon, Joëlle'}], "expected [{'name': 'Papillon, Joëlle'}], received: {}".format(bibjson.author)
+        assert bibjson.author == [{'name': 'Papillon, Joëlle'}], "expected [{{'name': 'Papillon, Joëlle'}}]', received: {}".format(bibjson.author)
         assert bibjson.get_single_url("fulltext") == "http://doaj.org/testing/url.pdf", "expected 'http://doaj.org/testing/url.pdf', received: {}".format(bibjson.get_single_url("fulltext"))
 
+    def test_06_crossref_article_xml_xwalk(self):
+        handle = CrossrefArticleFixtureFactory.upload_2_issns_correct()
+        xwalk = CrossrefXWalk()
+        art = xwalk.crosswalk_file(file_handle=handle, add_journal_info=False)
+        article = models.Article(**art[0])
+        bibjson = article.bibjson()
 
+        assert bibjson.journal_title == "2 ISSNs Correct", "expected '2 ISSNs Correct', received: {}".format(bibjson.journal_title)
+        assert bibjson.get_one_identifier(bibjson.P_ISSN) == "1234-5678", "expected '1234-5678', received: {}".format(bibjson.get_one_identifier(bibjson.P_ISSN))
+        assert bibjson.get_one_identifier(bibjson.E_ISSN) == "9876-5432", "expected '9876-5432', received: {}".format(bibjson.get_one_identifier(bibjson.E_ISSN))
+        assert bibjson.year == "2004", "expected '2004', received: {}".format(bibjson.year)
+        assert bibjson.title == "Article 12292005 9:32", "expected 'Article 12292005 9:32', received: {}".format(bibjson.title)
+        assert bibjson.author == [{'name': 'Bob, Surname'}], "expected [{{'name': 'Bob, Surname'}}]', received: {}".format(bibjson.author)
+        assert bibjson.get_single_url("fulltext") == "http://www.crossref.org/", "expected 'http://www.crossref.org/', received: {}".format(bibjson.get_single_url("fulltext"))
