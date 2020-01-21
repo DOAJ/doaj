3fd1f2df0f4a8679ebe708c176cf2069bec67817
Steve Eardley <steve@eardley.xyz>
2020-01-21 19:04:58 +0000

    Revert "created an abominable mix of new and old language stuff to keep equivalency with the old python 2 branch."
    This reverts commit f01048dd

--- portality/datasets.py
+++ portality/datasets.py
@@ -1,6 +1,5 @@
 import pycountry
 from collections import OrderedDict
-from portality import legacy_datasets
 
 
 def _generate_country_options():
@@ -70,22 +69,17 @@ def _generate_license_options():
 # Datasets for export
 ####################################################
 
-#FIXME: this has been butchered to restore the old datasets temporarily
-
 # COUNTRIES
 # country_options is a list of tuples, [(2-char_code, name)] for 2-char codes (ISO 3166).
-# country_options = _generate_country_options()
-country_options = legacy_datasets.country_options
+country_options = _generate_country_options()
 
 # CURRENCIES
 CURRENCY_DEFAULT = ''
 # currency_options is a list of tuples, [(3-char_code, '3-char_code - name')] for all currencies.
-# currency_options = _generate_currency_options()
-currency_options = legacy_datasets.currency_options
+currency_options = _generate_currency_options()
 
 # LANGUAGES
-#language_options = _generate_language_options()
-language_options = legacy_datasets.language_options
+language_options = _generate_language_options()
 
 # LICENSES
 # licenses contains a dict with each license's permissions. license_dict is ordered by the name of the licenses.
@@ -95,62 +89,48 @@ licenses, license_dict, main_license_options = _generate_license_options()
 
 def language_for(rep):
     """ Get the entire language entry for a given representation """
-    return legacy_datasets.language_for(rep)
-'''
     try:
         return pycountry.languages.lookup(rep)
     except LookupError:
         return None
-'''
+
 
 def name_for_lang(rep):
     """ Get the language name from a representation of the language"""
-    return legacy_datasets.name_for_lang(rep)
-'''
     try:
         return pycountry.languages.lookup(rep).name
     except LookupError:
         return rep
-'''
 
 
 def get_country_code(current_country, fail_if_not_found=False):
     """ Get the two-character country code for a given country name """
-    return legacy_datasets.get_country_code(current_country, fail_if_not_found)
-'''    
     try:
         return pycountry.countries.lookup(current_country).alpha_2
     except LookupError:
         return None if fail_if_not_found else current_country
-'''
+
 
 def get_country_name(code):
     """ Get the name of a country from its two-character code """
-    return legacy_datasets.get_country_name(code)
-'''
     try:
         return pycountry.countries.lookup(code).name
     except LookupError:
         return code  # return what was passed in if not found
-'''
+
 
 def get_currency_name(code):
     """ get the name of a currency from its code """
-    return legacy_datasets.get_currency_name(code)
-'''
     try:
         cur = pycountry.currencies.lookup(code)
         return '{code} - {name}'.format(code=cur.alpha_3, name=cur.name)
     except LookupError:
         return code  # return what was passed in if not found
-'''
+
 
 def get_currency_code(name):
     """ Retrieve a currency code by the currency name """
-    return legacy_datasets.get_currency_code(name)
-'''
     try:
         return pycountry.currencies.lookup(name).alpha_3
     except LookupError:
         return None
-'''
