aab7fc8205d1f1f5051d9805383764fdf71cdd21
Aga Domanska <aga@cottagelabs.com>
2020-05-20 11:34:33 +0100

    tab works!

--- portality/view/apply.py
+++ portality/view/apply.py
@@ -12,20 +12,11 @@ blueprint = Blueprint('apply', __name__)
 
 
 @blueprint.route("/", methods=["GET", "POST"])
-@blueprint.route("/01-oa-compliance/", methods=["GET", "POST"])
-@blueprint.route("/02-about/", methods=["GET", "POST"])
-@blueprint.route("/03-copyright-licensing/", methods=["GET", "POST"])
-@blueprint.route("/04-editorial/", methods=["GET", "POST"])
-@blueprint.route("/05-business-model/", methods=["GET", "POST"])
-@blueprint.route("/06-best-practice/", methods=["GET", "POST"])
 @write_required()
 def public_application(draft_id=None):
 
     if not current_user.is_authenticated:
         return redirect(url_for("account.login",  redirected="apply"))
-
-    if request.path == "/apply/":
-        return redirect("/apply/01-oa-compliance/")
 
     draft_application = None
     if draft_id is not None:
