0c354d3489086b14e29f5330204ee159ed8a23a9
Aga Domanska <aga@cottagelabs.com>
2020-07-03 13:03:55 +0100

    working on autocompletion and tags

--- portality/static/js/autocomplete.js
+++ portality/static/js/autocomplete.js
@@ -6,7 +6,7 @@ function autocomplete(selector, doc_field, doc_type, mininput, include_input, al
     var allow_clear = allow_clear === undefined ? true : allow_clear;
 
     var ajax = {
-            url: current_scheme + "//" + current_domain + "/autocomplete/" + doc_type + "/" + doc_field,
+            url: current_scheme + "//" + current_domain + "/autocomplete/" + doc_type + "/" + doc_ffunction autocompleteield,
             dataType: 'json',
             data: function (term, page) {
                 return {
