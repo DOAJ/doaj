09de7b611aa6c9a77ae0c6b6f8286731cfedd398
Richard Jones <richard@cottagelabs.com>
2020-09-17 14:42:49 +0100

    port site-note code over from old system

--- portality/settings.py
+++ portality/settings.py
@@ -986,10 +986,16 @@ ELASTIC_APM = {
 }
 
 ########################################
-## Consent Cookie
+## Consent Cookie and other Top-Level dismissable notes
 
 CONSENT_COOKIE_KEY = "doaj-cookie-consent"
 
+# site notes, which can be configured to run any time with any content
+SITE_NOTE_ACTIVE = False
+SITE_NOTE_KEY = "doaj-site-note"
+SITE_NOTE_SLEEP = 259200    # every 3 days
+SITE_NOTE_COOKIE_VALUE = "You have seen our most recent site wide announcement"
+SITE_NOTE_TEMPLATE = "doaj/site_note.html"
 
 
 #############################################
