0943034c2c5c834b7a8a180d8f091d82e04d9df3
Richard Jones <richard@cottagelabs.com>
2020-09-18 14:49:08 +0100

    reintroduce refine search results header to fix mobile view.  This still has some problems in full view, I will update the related issue

--- portality/static/js/doaj.fieldrender.edges.js
+++ portality/static/js/doaj.fieldrender.edges.js
@@ -54,6 +54,9 @@ $.extend(true, doaj, {
                     <div class="row">\
                         <div class="col-md-3">\
                             <aside class="filters">\
+                                <h2 class="label label--underlined filters__heading" type="button" data-toggle="collapse" data-target="#filters" aria-expanded="false">\
+                                    <span data-feather="sliders" aria-hidden="true"></span> Refine search results <span data-feather="chevron-down" aria-hidden="true"></span>\
+                                </h2>\
                                 <ul class="collapse filters__list" id="filters" aria-expanded="false">\
                                     {{FACETS}}\
                                 </ul>\
