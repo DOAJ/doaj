3b6383f4c3c4bc855f943ff995af6ceaf9598c69
Steve Eardley <steve@eardley.xyz>
2020-12-11 10:59:00 +0000

    Re-ordered CSV and updated question labels

--- portality/crosswalks/journal_questions.py
+++ portality/crosswalks/journal_questions.py
@@ -29,7 +29,7 @@ class Journal2QuestionXwalk(object):
         ("continued_by", "Continued By"),
         ("institution.institution_name", "Society or institution"),
         ("keywords", "Keywords"),
-        ("language", "Full text language"),
+        ("language", "Languages in which the journal accepts manuscripts"),
         ("license_attributes", "License attributes"),
         ("license_display", "Machine-readable CC licensing information embedded or displayed in articles"),
         ("license_display_example_url", "URL to an example page with embedded licensing information"),
@@ -42,16 +42,16 @@ class Journal2QuestionXwalk(object):
         ("editorial_board_url", "URL for the Editorial Board page"),
         ("author_instructions_url", "URL for journal's instructions for authors"),
         ("waiver_url", "Waiver policy information URL"),
-        ("persistent_identifiers", "Permanent article identifiers"),
+        ("persistent_identifiers", "Persistent article identifiers"),
         ("plagiarism_detection", "Journal plagiarism screening policy"),
         ("plagiarism_url", "Plagiarism information URL"),
-        ("publication_time_weeks", "Average number of weeks between submission and publication"),
+        ("publication_time_weeks", "Average number of weeks between article submission and publication"),
         ("publisher.publisher_name", "Publisher"),
-        ("other_charges_url", "Other submission charges information URL"),
+        ("other_charges_url", "Other submission fees information URL"),
         ("title", "Journal title"),
         ("institution.institution_country", "Country of society or institution"),
         ("apc", "APC"),
-        ("has_other_charges", "Has other submission charges"),
+        ("has_other_charges", "Has other fees"),
         ("has_waiver", "Journal waiver policy (for developing country authors etc)"),
         ("orcid_ids", "Article metadata includes ORCIDs"),
         ("open_citations", "Journal complies with I4OC standards for open citations"),
@@ -140,30 +140,56 @@ class Journal2QuestionXwalk(object):
         kvs = []
 
         # create key/value pairs for the questions in order
+        # About
         kvs.append((cls.q("title"), forminfo.get("title")))
         kvs.append((cls.q("journal_url"), forminfo.get("journal_url")))
         kvs.append((cls.q("alternative_title"), forminfo.get("alternative_title")))
         kvs.append((cls.q("pissn"), forminfo.get("pissn")))
         kvs.append((cls.q("eissn"), forminfo.get("eissn")))
+        kvs.append((cls.q("keywords"), ", ".join(forminfo.get("keywords", []))))
+        kvs.append((cls.q("language"), languages(forminfo.get("language", []))))
         kvs.append((cls.q("publisher.publisher_name"), forminfo.get("publisher_name")))
         kvs.append((cls.q("publisher.publisher_country"), datasets.get_country_name(forminfo.get("publisher_country"))))
         kvs.append((cls.q("institution.institution_name"), forminfo.get("institution_name")))
         kvs.append((cls.q("institution.institution_country"), datasets.get_country_name(forminfo.get("institution_country"))))
-        kvs.append((cls.q("continues"), ", ".join(forminfo.get("continues"))))
-        kvs.append((cls.q("continued_by"), ", ".join(forminfo.get("continued_by"))))
+
+        # Copyright & licensing
+        lic = ", ".join(forminfo.get("license", []))
+        kvs.append((cls.q("license"), lic))
+
+        kvs.append((cls.q("license_attributes"), license_checkbox(forminfo.get("license_attributes", []))))
+        kvs.append((cls.q("license_terms_url"), forminfo.get("license_terms_url")))
+        kvs.append((cls.q("license_display"), yes_or_blank(forminfo.get("license_display"))))
+        kvs.append((cls.q("license_display_example_url"), forminfo.get("license_display_example_url")))
+
+        kvs.append((cls.q("copyright_author_retains"), yes_no_or_blank(forminfo.get("copyright_author_retains"))))
+        kvs.append((cls.q("copyright_url"), forminfo.get("copyright_url")))
+
+        # Editorial
+        review_process = other_list("review_process", "review_process_other", "other")
+        kvs.append((cls.q("review_process"), review_process))
+        kvs.append((cls.q("review_url"), forminfo.get("review_url")))
+        kvs.append((cls.q("plagiarism_detection"), yes_no_or_blank(forminfo.get("plagiarism_detection"))))
+        kvs.append((cls.q("plagiarism_url"), forminfo.get("plagiarism_url")))
+        kvs.append((cls.q("aims_scope_url"), forminfo.get("aims_scope_url")))
+        kvs.append((cls.q("editorial_board_url"), forminfo.get("editorial_board_url")))
+        kvs.append((cls.q("author_instructions_url"), forminfo.get("author_instructions_url")))
+        kvs.append((cls.q("publication_time_weeks"), str(forminfo.get("publication_time_weeks"))))
+
+        # Business Model
         kvs.append((cls.q("apc"), yes_no_or_blank(forminfo.get("apc"))))
         kvs.append((cls.q("apc_url"), forminfo.get("apc_url")))
-
         apcs = []
         for apc_charge in forminfo.get("apc_charges", []):
             apcs.append(str(apc_charge.get("apc_max")) + " " + apc_charge.get("apc_currency"))
         kvs.append((cls.q("apc_charges.apc_max"), "; ".join(apcs)))
 
-        kvs.append((cls.q("has_other_charges"), yes_no_or_blank(forminfo.get("has_other_charges"))))
-        kvs.append((cls.q("other_charges_url"), forminfo.get("other_charges_url")))
         kvs.append((cls.q("has_waiver"), yes_no_or_blank(forminfo.get("has_waiver"))))
         kvs.append((cls.q("waiver_url"), forminfo.get("waiver_url")))
+        kvs.append((cls.q("has_other_charges"), yes_no_or_blank(forminfo.get("has_other_charges"))))
+        kvs.append((cls.q("other_charges_url"), forminfo.get("other_charges_url")))
 
+        # Best Practice
         dap = deepcopy(forminfo.get("preservation_service", []))
         if "national_library" in dap: dap.remove("national_library")
         if "other" in dap:
@@ -171,45 +197,24 @@ class Journal2QuestionXwalk(object):
             dap.append(forminfo.get("preservation_service_other"))
         if "none" in dap: dap.remove("none")
         kvs.append((cls.q("preservation_service"), ", ".join(dap)))
-
         kvs.append((cls.q("preservation_service_library"), "; ".join(forminfo.get("preservation_service_library", []))))
         kvs.append((cls.q("preservation_service_url"), forminfo.get("preservation_service_url")))
-
-        article_identifiers = other_list("persistent_identifiers", "persistent_identifiers_other", "other")
-        kvs.append((cls.q("persistent_identifiers"), article_identifiers))
-
-        kvs.append((cls.q("keywords"), ", ".join(forminfo.get("keywords", []))))
-        kvs.append((cls.q("language"), languages(forminfo.get("language", []))))
-        kvs.append((cls.q("editorial_board_url"), forminfo.get("editorial_board_url")))
-
-        review_process = other_list("review_process", "review_process_other", "other")
-        kvs.append((cls.q("review_process"), review_process))
-
-        kvs.append((cls.q("review_url"), forminfo.get("review_url")))
-        kvs.append((cls.q("aims_scope_url"), forminfo.get("aims_scope_url")))
-        kvs.append((cls.q("author_instructions_url"), forminfo.get("author_instructions_url")))
-        kvs.append((cls.q("plagiarism_detection"), yes_no_or_blank(forminfo.get("plagiarism_detection"))))
-        kvs.append((cls.q("plagiarism_url"), forminfo.get("plagiarism_url")))
-        kvs.append((cls.q("publication_time_weeks"), str(forminfo.get("publication_time_weeks"))))
-        kvs.append((cls.q("oa_statement_url"), forminfo.get("oa_statement_url")))
-
-        kvs.append((cls.q("license_display"), yes_or_blank(forminfo.get("license_display"))))
-        kvs.append((cls.q("license_display_example_url"), forminfo.get("license_display_example_url")))
-
-        lic = ", ".join(forminfo.get("license", []))
-        kvs.append((cls.q("license"), lic))
-
-        kvs.append((cls.q("license_attributes"), license_checkbox(forminfo.get("license_attributes", []))))
-        kvs.append((cls.q("license_terms_url"), forminfo.get("license_terms_url")))
-        kvs.append((cls.q("boai"), yes_or_blank(forminfo.get("boai"))))
 
         deposit_policies = other_list("deposit_policy", "deposit_policy_other", "other")
         kvs.append((cls.q("deposit_policy"), deposit_policies))
         kvs.append((cls.q("deposit_policy_url"), forminfo.get("deposit_policy_url")))
-        kvs.append((cls.q("copyright_author_retains"), yes_no_or_blank(forminfo.get("copyright_author_retains"))))
-        kvs.append((cls.q("copyright_url"), forminfo.get("copyright_url")))
+
+        article_identifiers = other_list("persistent_identifiers", "persistent_identifiers_other", "other")
+        kvs.append((cls.q("persistent_identifiers"), article_identifiers))
         kvs.append((cls.q("orcid_ids"), yes_no_or_blank(forminfo.get("orcid_ids"))))
         kvs.append((cls.q("open_citations"), yes_no_or_blank(forminfo.get("open_citations"))))
+
+        # Open Access Compliance (usually first on form)
+        kvs.append((cls.q("boai"), yes_or_blank(forminfo.get("boai"))))
+        kvs.append((cls.q("oa_statement_url"), forminfo.get("oa_statement_url")))
+
+        kvs.append((cls.q("continues"), ", ".join(forminfo.get("continues"))))
+        kvs.append((cls.q("continued_by"), ", ".join(forminfo.get("continued_by"))))
 
         kvs.append((cls.q("subject"), "|".join(forminfo.get("subject"))))
 
