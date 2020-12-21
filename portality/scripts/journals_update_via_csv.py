4905a2d8c579e93eb0d405b74c8ef813564e3d11
Steve Eardley <steve@eardley.xyz>
2020-12-21 22:57:01 +0000

    update by csv script - verify headers of incoming CSV

--- portality/scripts/journals_update_via_csv.py
+++ portality/scripts/journals_update_via_csv.py
@@ -5,6 +5,7 @@ twinned with journals_in_doaj_by_account.py
 
 import csv
 from portality.models import Journal
+from doajtest.fixtures import JournalFixtureFactory
 
 if __name__ == "__main__":
     import argparse
@@ -21,8 +22,20 @@ if __name__ == "__main__":
         with open(args.infile, 'r', encoding='utf-8') as g:
             reader = csv.reader(g)
 
+            # verify header row with current CSV headers, report errors
+            header_row = next(reader)
+            expected_headers = JournalFixtureFactory.csv_headers()
+            if header_row[1:] != expected_headers:
+                print("\nERROR: CSV input file is the wrong format. "
+                      "Expected ID column followed by the JournalCSV columns.\n")
+                for i in range(0, len(expected_headers)):
+                    if expected_headers[i] != header_row[i+1]:
+                        print('At column no {0} expected "{1}", found "{2}"'.format(i+1, expected_headers[i], header_row[i+1]))
+                exit(1)
+
             for row in reader:
-                # verify header row with current CSV headers
+                print(row)
+                # Notify when title has changed?
 
                 success = False
 
@@ -31,7 +44,8 @@ if __name__ == "__main__":
 
                 # Load remaining rows into application form as an update
 
+
                 # Save the journal
-                j.save()
+                #j.save()
                 writer.writerow([j.id, str(success)])
 
