7a1bdc24c3edd042990f30445af33dcce3380d77
Richard Jones <richard@cottagelabs.com>
2021-03-15 15:34:26 +0000

    major clear out of no-longer-used templates

--- portality/view/doaj.py
+++ portality/view/doaj.py
@@ -55,16 +55,8 @@ def dismiss_site_note():
 
 @blueprint.route("/news")
 def news():
-    news = blog.News.latest(app.config.get("NEWS_PAGE_NEWS_ITEMS", 20))
-    return render_template('doaj/news.html', news=news, blog_url=app.config.get("BLOG_URL"))
+    return redirect("https://blog.doaj.org")
 
-
-# @blueprint.route("/widgets")
-# def widgets():
-#     return render_template('doaj/widgets.html',
-#                            env=app.config.get("DOAJENV"),
-#                            widget_filename_suffix='' if app.config.get('DOAJENV') == 'production' else '_' + app.config.get('DOAJENV', '')
-#                            )
 
 @blueprint.route("/ssw_demo")
 def ssw_demo():
