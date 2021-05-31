77ddfea91882b318e3701edefc001e9ed1412b4d
Aga Domanska <aga@cottagelabs.com>
2021-05-31 14:07:30 +0100

    change if order to leave for-now-unused case at the end

--- portality/api/v2/data_objects/common.py
+++ portality/api/v2/data_objects/common.py
@@ -4,11 +4,11 @@ def _check_for_script(data):
             if isinstance(value, dict):
                 if _check_for_script(value):
                     return True
+            elif isinstance(value, str):
+                if "<script>" in value:
+                    return True
             elif isinstance(value, list):
                 for x in value:
                     if _check_for_script(x):
                         return True
-            elif isinstance(value, str):
-                if "<script>" in value:
-                    return True
     return False
