d0e909754cfff11164b0fd1ab9420f4be00022dd
Aga Domanska <aga@cottagelabs.com>
2021-06-30 18:09:27 +0100

    fix script loading issue

--- portality/static/widget/fixed_query.js
+++ portality/static/widget/fixed_query.js
@@ -4,8 +4,31 @@ let parser = document.createElement('a');
 parser.href = script.attributes.src.value;
 var doaj_url = parser.protocol + '//' + parser.host;
 
-(function() {
     let jQuery;
+
+(function(){
+    /******** Load jQuery if not present *********/
+    if (window.jQuery === undefined || window.jQuery.fn.jquery !== "1.9.1") {
+        let script_tag = document.createElement("script");
+        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("src", doaj_url + '/static/vendor/jquery-3.4.1/jquery-3.4.1.min.js');
+        if (script_tag.readyState) {
+            script_tag.onreadystatechange = function () { // For old versions of IE
+                if (this.readyState === "complete" || this.readyState === "loaded") {
+                    main();
+                }
+            };
+        } else { // Other browsers
+            script_tag.onload = main;
+        }
+        // Try to find the head, otherwise default to the documentElement
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+    } else {
+        // The jQuery version on the window is the one we want to use
+        // jQuery = window.jQuery;
+        main();
+    }
+})();
 
     /******** Load scripts *********/
 
@@ -36,42 +59,10 @@ var doaj_url = parser.protocol + '//' + parser.host;
         head.appendChild(scr);
     }
 
-
-    /******** Load jQuery if not present *********/
-    if (window.jQuery === undefined || window.jQuery.fn.jquery !== "1.9.1") {
-        let script_tag = document.createElement("script");
-        script_tag.setAttribute("type","text/javascript");
-        script_tag.setAttribute("src", doaj_url + '/static/vendor/jquery-3.4.1/jquery-3.4.1.min.js');
-        if (script_tag.readyState) {
-            script_tag.onreadystatechange = function () { // For old versions of IE
-                if (this.readyState === "complete" || this.readyState === "loaded") {
-                    scriptLoadHandler();
-                }
-            };
-        } else { // Other browsers
-            script_tag.onload = scriptLoadHandler;
-        }
-        // Try to find the head, otherwise default to the documentElement
-        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
-    } else {
-        // The jQuery version on the window is the one we want to use
-        // jQuery = window.jQuery;
-        main();
-    }
-
-    /******** Called once jQuery has loaded ******/
-    function scriptLoadHandler() {
-        // Restore $ and window.jQuery to their previous values and store the
-        // new jQuery in our local jQuery variable
-        // jQuery = window.jQuery.noConflict(true);
-        // Call our main function
-        main();
-    }
-
     /******** Our main function ********/
     function main() {
         loadCustomScript();
-        $(document).ready(function($) {
+
 
             $('#doaj-fixed-query-widget').append($('<div class="facetview"></div>'));
 
@@ -81,7 +72,5 @@ var doaj_url = parser.protocol + '//' + parser.host;
                 url: doaj_url + "/fqw_hit",
                 data: {embedding_page: window.location.href}
             });
-        });
-    }
 
-})(); // We call our anonymous function immediately
+    }
