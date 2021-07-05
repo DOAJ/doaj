391282dc2428a6947a7b2c82886c3f701646d0ca
Ramakrishna Sakhamuru <mail2rk@gmail.com>
2021-07-05 14:17:06 +0530

    Added some methods to save the zip file uploaded by user into a temp directory

--- doajtest/unit/test_task_preservation.py
+++ doajtest/unit/test_task_preservation.py
@@ -11,7 +11,7 @@ class TestPreservation(DoajTestCase):
     def setUp(self):
         super(TestPreservation, self).setUp()
         self.upload_dir = app.config.get("UPLOAD_DIR", ".")
-        self.preservation = preservation.Preservation()
+        self.preservation = preservation.Preservation("rama")
 
     def tearDown(self):
         super(TestPreservation, self).tearDown()
@@ -20,22 +20,31 @@ class TestPreservation(DoajTestCase):
     def test_local_directory(self):
 
         #Test creation of local directory
-        self.preservation.create_local_directory("rama")
+        self.preservation.create_local_directory()
         assert os.path.isdir(os.path.join(self.upload_dir, self.preservation.dir_name))
 
         #Test deletion of local directory
         self.preservation.delete_local_directory()
         assert not os.path.exists(os.path.join(self.upload_dir, self.preservation.dir_name))
 
-    def test_save_file(self):
+    def test_preservation(self):
 
         resources = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "unit", "resources")
+        temp_dir = os.path.join(self.upload_dir, self.preservation.dir_name)
 
         articles_zip_path = os.path.join(resources,"articles.zip")
+        # Test zip file save
         with open(articles_zip_path, 'rb') as zf:
             zip_file = FileStorage(BytesIO(zf.read()), filename="articles.zip")
-            self.preservation.save_file("rama", zip_file)
+            self.preservation.save_file(zip_file)
 
-            assert os.path.exists(os.path.join(self.upload_dir, self.preservation.dir_name, zip_file.filename))
+            assert os.path.exists(os.path.join(temp_dir, zip_file.filename))
 
+        # Test extraction of zip file
+        self.preservation.extract_zip_file()
+
+        assert os.path.exists(os.path.join(temp_dir, "articles"))
+        assert os.path.isdir(os.path.join(temp_dir, "articles"))
+        assert os.path.isdir(os.path.join(temp_dir, "articles", "article_1"))
+        assert os.path.exists(os.path.join(temp_dir, "articles", "article_1", "identifier.txt"))
 
--- portality/tasks/preservation.py
+++ portality/tasks/preservation.py
@@ -1,58 +1,80 @@
 import os
 import shutil
 from datetime import datetime
+from zipfile import ZipFile
 from portality.core import app
 from portality.lib import dates
 
 class PreservationException(Exception):
     pass
 
+
+class PreservationStorageException(Exception):
+    pass
+
+
 class Preservation:
 
     #Zip file name to download the zip file to temp directory
     ARTICLES_ZIP_NAME = "articles.zip"
+    #Temp directory
+    UPLOAD_DIR = app.config.get("UPLOAD_DIR", ".")
 
-    def __init__(self):
-        self.upload_dir = app.config.get("UPLOAD_DIR", ".")
+    def __init__(self, owner):
         self.__created_time = dates.format(datetime.utcnow(),"%Y-%m-%d-%H-%M-%S")
-        self.__dir_name = None
+        self.__dir_name = owner + "-" + self.__created_time
+        self.__local_dir = os.path.join(Preservation.UPLOAD_DIR, self.__dir_name)
 
     @property
     def dir_name(self):
         return self.__dir_name
 
-    def create_local_directory(self, owner):
+    def create_local_directory(self):
         """
-        Create a unique local directory to download the files
+        Create a local directory to download the files
         :param owner: User who upload files
         :return:
         """
-
-        # Create unique name for the directory
-        self.__dir_name = owner + "-" + self.__created_time
-
-        local_dir = os.path.join(self.upload_dir, self.__dir_name)
-        os.mkdir(local_dir)
+        try:
+            os.mkdir(self.__local_dir)
+        except OSError as exp:
+            raise PreservationStorageException(message="Could not create temp directory", inner=exp)
 
     def delete_local_directory(self):
         """Deletes the directory
         :param dir_name: Name of the directory to delete
         :return:
         """
-        if self.__dir_name:
-            local_dir = os.path.join(self.upload_dir, self.__dir_name)
-            if os.path.exists(local_dir):
-                shutil.rmtree(local_dir)
+        if os.path.exists(self.__local_dir):
+            try:
+                shutil.rmtree(self.__local_dir)
+            except Exception as e:
+                raise PreservationStorageException(message="Could not delete Temp directory", inner=e)
 
-    def save_file(self, owner, file):
+    def save_file(self, file):
         """
         Save the file on to local directory
         :param owner: User who upload files
         :param file: File object
         :return:
         """
-        self.create_local_directory(owner)
-        local_dir = os.path.join(self.upload_dir, self.__dir_name)
-        file_path = os.path.join(local_dir, Preservation.ARTICLES_ZIP_NAME)
-        file.save(file_path)
+        self.create_local_directory()
+        file_path = os.path.join(self.__local_dir, Preservation.ARTICLES_ZIP_NAME)
+        try:
+            file.save(file_path)
+        except Exception as e:
+            raise PreservationStorageException(message="Could not save file in Temp directory", inner=e)
+
+    def extract_zip_file(self):
+        """
+        Extracts zip file in the Temp directory
+        :return:
+        """
+        file_path = os.path.join(self.__local_dir, Preservation.ARTICLES_ZIP_NAME)
+
+        if os.path.exists(file_path):
+            with ZipFile(file_path, 'r') as zFile:
+                zFile.extractall(self.__local_dir)
+        else:
+            raise PreservationException(message="Could not find zip file at Temp directory")
 
