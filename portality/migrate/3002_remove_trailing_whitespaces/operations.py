2191bbd2de630c15f978b71416dfab4553c27b1f
Aga Domanska <aga@cottagelabs.com>
2021-07-09 15:57:17 +0100

    add empty lines where appropriate

--- portality/migrate/3002_remove_trailing_whitespaces/operations.py
+++ portality/migrate/3002_remove_trailing_whitespaces/operations.py
@@ -1,12 +1,14 @@
 from portality.models import Article
 import csv
 
+
 def remove_trailing_whitespaces_rec(d):
     for key, value in d.items():
         if isinstance(value, dict):
             return remove_trailing_whitespaces_rec(value)
         if isinstance(value, str):
             d[key] = value.strip()
+
 
 def remove_trailing_whitespaces(model):
     remove_trailing_whitespaces_rec(model.__dict__)
