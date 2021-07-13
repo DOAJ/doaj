6b95b27b4f2473fd7b4530e5a1ea72a14bcf0624
Aga Domanska <aga@cottagelabs.com>
2021-07-13 14:35:15 +0100

    replace __dict__ with init_with_model: false and fix the method in operations

--- portality/migrate/3002_remove_trailing_whitespaces/operations.py
+++ portality/migrate/3002_remove_trailing_whitespaces/operations.py
@@ -1,15 +1,7 @@
-from portality.models import Article
-import csv
-
-
-def remove_trailing_whitespaces_rec(d):
+def remove_trailing_whitespaces(d):
     for key, value in d.items():
         if isinstance(value, dict):
-            return remove_trailing_whitespaces_rec(value)
+            d[key] = remove_trailing_whitespaces(value)
         if isinstance(value, str):
             d[key] = value.strip()
-
-
-def remove_trailing_whitespaces(model):
-    remove_trailing_whitespaces_rec(model.__dict__)
-    return model
+    return d
