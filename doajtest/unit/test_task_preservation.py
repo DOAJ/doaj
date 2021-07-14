a794452b0d56a48d41119cbeff571cc2eee9b79b
Ramakrishna Sakhamuru <mail2rk@gmail.com>
2021-07-14 14:34:27 +0530

    Added some preservation methods

--- doajtest/unit/test_task_preservation.py
+++ doajtest/unit/test_task_preservation.py
@@ -1,50 +1,95 @@
 import os
 from io import BytesIO
+from unittest.mock import patch
 from doajtest.helpers import DoajTestCase
+from doajtest.mocks.preservation import PreservationMock
 from portality.tasks import preservation
 from portality.core import app
 from werkzeug.datastructures import FileStorage
+from portality.models.article import Article
 
 
 class TestPreservation(DoajTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super(TestPreservation, cls).setUpClass()
+        cls.upload_dir = app.config.get("UPLOAD_DIR", ".")
+        cls.preserve = preservation.Preservation("rama")
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestPreservation, cls).tearDownClass()
+        cls.preserve.delete_local_directory()
+
     def setUp(self):
         super(TestPreservation, self).setUp()
-        self.upload_dir = app.config.get("UPLOAD_DIR", ".")
-        self.preservation = preservation.Preservation("rama")
+
 
     def tearDown(self):
         super(TestPreservation, self).tearDown()
-        self.preservation.delete_local_directory()
+
 
     def test_local_directory(self):
 
         #Test creation of local directory
-        self.preservation.create_local_directory()
-        assert os.path.isdir(os.path.join(self.upload_dir, self.preservation.dir_name))
+        TestPreservation.preserve.create_local_directories()
+        assert os.path.isdir(os.path.join(TestPreservation.upload_dir, TestPreservation.preserve.dir_name))
+        assert os.path.isdir(os.path.join(TestPreservation.upload_dir, TestPreservation.preserve.dir_name,
+                                          TestPreservation.preserve.dir_name))
 
         #Test deletion of local directory
-        self.preservation.delete_local_directory()
-        assert not os.path.exists(os.path.join(self.upload_dir, self.preservation.dir_name))
+        TestPreservation.preserve.delete_local_directory()
+        assert not os.path.exists(os.path.join(TestPreservation.upload_dir, TestPreservation.preserve.dir_name))
 
+    def mock_pull_by_key(key, value):
+        article = Article()
+        article.data = PreservationMock.ARTICLE_DATA
+        return article
+
+    @patch.object(Article, 'pull_by_key', mock_pull_by_key)
     def test_preservation(self):
 
         resources = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "unit", "resources")
-        temp_dir = os.path.join(self.upload_dir, self.preservation.dir_name)
+        temp_dir = os.path.join(TestPreservation.upload_dir, TestPreservation.preserve.dir_name)
 
         articles_zip_path = os.path.join(resources,"articles.zip")
         # Test zip file save
         with open(articles_zip_path, 'rb') as zf:
             zip_file = FileStorage(BytesIO(zf.read()), filename="articles.zip")
-            self.preservation.save_file(zip_file)
+            TestPreservation.preserve.save_file(zip_file)
 
             assert os.path.exists(os.path.join(temp_dir, zip_file.filename))
 
         # Test extraction of zip file
-        self.preservation.extract_zip_file()
+        TestPreservation.preserve.extract_zip_file()
 
         assert os.path.exists(os.path.join(temp_dir, "articles"))
         assert os.path.isdir(os.path.join(temp_dir, "articles"))
         assert os.path.isdir(os.path.join(temp_dir, "articles", "article_1"))
         assert os.path.exists(os.path.join(temp_dir, "articles", "article_1", "identifier.txt"))
 
+        reader = preservation.CSVReader(os.path.join(temp_dir, "articles", "identifiers.csv"))
+        data = reader.articles_info()
+
+        assert "article_1" in data
+        assert "article/10.1186/s40478-018-0619-9" in data["article_1"][0]
+
+        # Test package structure
+        TestPreservation.preserve.create_package_structure()
+        package_dir = os.path.join(TestPreservation.upload_dir,
+                                        TestPreservation.preserve.dir_name, TestPreservation.preserve.dir_name)
+        tag_manifest_file = os.path.join(package_dir, "2051-5960", "00003741594643f4996e2555a01e03c7", "tagmanifest-sha256.txt")
+        manifest_file = os.path.join(package_dir,"2051-5960", "00003741594643f4996e2555a01e03c7", "manifest-sha256.txt")
+        assert os.path.exists(package_dir)
+        assert os.path.exists(tag_manifest_file)
+        assert os.path.exists(manifest_file)
+
+
+
+    def test_get_article_info(self):
+        issn, article_id, metadata_json = TestPreservation.preserve.get_article_info(PreservationMock.ARTICLE_DATA)
+
+        assert issn == "2051-5960"
+        assert article_id == "00003741594643f4996e2555a01e03c7"
+        assert metadata_json["bibjson"]["identifier"][0]["id"] == "10.1186/s40478-018-0619-9"
--- portality/tasks/preservation.py
+++ portality/tasks/preservation.py
@@ -1,9 +1,15 @@
+import bagit
+import csv
+import json
 import os
 import shutil
+from copy import deepcopy
 from datetime import datetime
 from zipfile import ZipFile
 from portality.core import app
 from portality.lib import dates
+from portality.models import Article
+from portality.regex import DOI_COMPILED, HTTP_URL_COMPILED
 
 class PreservationException(Exception):
     pass
@@ -12,31 +18,84 @@ class PreservationException(Exception):
 class PreservationStorageException(Exception):
     pass
 
+class CSVReader:
+
+    # column names for csv file.
+    # Given more identifiers just to handle any mistakes by user like empty identifiers
+    # Max expected identifier are 2 (Full Text URL, DOI) in any order
+    FIELD_DIR = "dir_name"
+    FIELDS = (FIELD_DIR,"id_1","id_2","id_3","id_4")
+
+    def __init__(self, csv_file):
+        self.__csv_file = csv_file
+
+    def articles_info(self):
+        """
+        Reads the csv file and returns dictionary with first column(directory name) as keys
+        and remaining columns as array elements.
+
+        Ex: {'article_1': ['http://link.springer.com/article/10.1186/s40478-018-0619-9',
+            '10.1136/bmjophth-2021-000774'], 'article_2': ['10.1136/bmjophth-2021-000775']}
+
+        :return: Dictionary with articles info
+        """
+        data = {}
+
+        with open(self.__csv_file, 'r') as file:
+            reader = csv.DictReader(file, CSVReader.FIELDS)
+            for row in reader:
+                dir_name = row[CSVReader.FIELD_DIR]
+                # Remove first column so it will not  be part of iteration later
+                row.pop(CSVReader.FIELD_DIR)
+                data[dir_name] = []
+                for key in row.keys():
+                    if row[key]:
+                        data[dir_name].append(row[key])
+        return data
+
 
 class Preservation:
 
-    #Zip file name to download the zip file to temp directory
+    # Zip file name to download the zip file to temp directory
     ARTICLES_ZIP_NAME = "articles.zip"
-    #Temp directory
+    # Identifier file name
+    IDENTIFIER_FILE = "identifier.txt"
+    # CSV file foor identifiers
+    IDENTIFIERS_CSV = "identifiers.csv"
+    # Temp directory
     UPLOAD_DIR = app.config.get("UPLOAD_DIR", ".")
 
     def __init__(self, owner):
         self.__created_time = dates.format(datetime.utcnow(),"%Y-%m-%d-%H-%M-%S")
         self.__dir_name = owner + "-" + self.__created_time
         self.__local_dir = os.path.join(Preservation.UPLOAD_DIR, self.__dir_name)
+        self.__preservation_dir = os.path.join(self.__local_dir, self.__dir_name)
+        self.__csv_articles_dict = None
 
     @property
     def dir_name(self):
         return self.__dir_name
 
-    def create_local_directory(self):
+    def disk_space_available(self):
         """
-        Create a local directory to download the files
+        Check if there is enough disk space to save file
+        :param file_size:
+        :return: True or False
+        """
+        stats = shutil.disk_usage(Preservation.UPLOAD_DIR)
+        # TODO implement storage availability check
+        return True
+
+    def create_local_directories(self):
+        """
+        Create a local directories to download the files and
+        to create preservation package
         :param owner: User who upload files
         :return:
         """
         try:
             os.mkdir(self.__local_dir)
+            os.mkdir(self.__preservation_dir)
         except OSError as exp:
             raise PreservationStorageException(message="Could not create temp directory", inner=exp)
 
@@ -58,12 +117,15 @@ class Preservation:
         :param file: File object
         :return:
         """
-        self.create_local_directory()
-        file_path = os.path.join(self.__local_dir, Preservation.ARTICLES_ZIP_NAME)
-        try:
-            file.save(file_path)
-        except Exception as e:
-            raise PreservationStorageException(message="Could not save file in Temp directory", inner=e)
+        if self.disk_space_available():
+            self.create_local_directories()
+            file_path = os.path.join(self.__local_dir, Preservation.ARTICLES_ZIP_NAME)
+            try:
+                file.save(file_path)
+            except Exception as e:
+                raise PreservationStorageException(message="Could not save file in Temp directory", inner=e)
+        else:
+            raise PreservationStorageException(message="Not enough disk space to save file")
 
     def extract_zip_file(self):
         """
@@ -77,4 +139,150 @@ class Preservation:
                 zFile.extractall(self.__local_dir)
         else:
             raise PreservationException(message="Could not find zip file at Temp directory")
+
+    def create_package_structure(self):
+        """ Create preservation package
+
+        Iterates through the sub directories.
+        Retrieve article info for each article.
+        Creates preservation directories
+
+        :return:
+        """
+        for dir, subdirs, files in os.walk(self.__local_dir):
+
+            app.logger.debug("Directory : " + dir )
+            app.logger.debug("Sub Directories : " + str(subdirs) )
+            app.logger.debug("Files : " + str(files) )
+
+            if Preservation.IDENTIFIERS_CSV in files:
+                # Get articles info from csv file
+                csv_reader = CSVReader(os.path.join(dir, Preservation.IDENTIFIERS_CSV))
+                self.__csv_articles_dict = csv_reader.articles_info()
+            self.__process_article(dir, files)
+
+    def __process_article(self, dir, files):
+
+        identifiers = None
+        dir_name = os.path.basename(dir)
+
+        # check if identifier file exist
+        if Preservation.IDENTIFIER_FILE in files:
+            with open(os.path.join(dir, Preservation.IDENTIFIER_FILE)) as file:
+                identifiers = file.read().splitlines()
+        elif self.__csv_articles_dict:
+            if dir_name in self.__csv_articles_dict:
+                identifiers = self.__csv_articles_dict[dir_name]
+
+        if identifiers:
+            article_data = self.get_article(identifiers)
+
+            if article_data:
+                issn, article_id, metadata_json = self.get_article_info(article_data)
+
+                package = PreservationPackage()
+                package.issn = issn
+                package.article_id = article_id
+                package.metadata = metadata_json
+                package.article_dir = dir
+                package.article_files = files
+                package.package_dir = self.__preservation_dir
+
+                package.create_article_bagit_structure()
+
+
+    def get_article(self, identifiers):
+        """
+        Checks if the identifier is doi or full text
+        Pulls article related to the identifier
+        :param identifiers:
+        :return: article dict
+        """
+        article = None
+        for identifier in identifiers:
+            if DOI_COMPILED.match(identifier):
+                article = Article.pull_by_key("bibjson.identifier.id", identifier)
+            elif HTTP_URL_COMPILED.match(identifier):
+                article = Article.pull_by_key("bibjson.link.url", identifier)
+            if article:
+                return article.data
+
+    def get_article_info(self, article_json):
+        """
+        Returns article info
+        :param article_json:
+        :return: issn, article id, metadata json
+        """
+
+        metadata_json = self.get_metadata_json(article_json)
+
+        issn = article_json["bibjson"]["journal"]["issns"][0]
+
+        article_id = article_json["id"]
+
+        return issn, article_id, metadata_json
+
+    def get_metadata_json(self, article_json):
+        """
+        Returns metadata of article which is required for preservation
+        :return: metadata
+        """
+        # Remove unnecessary data
+        metadata = deepcopy(article_json)
+        metadata.pop("index")
+        metadata.pop("admin")
+        metadata.pop("es_type")
+
+        return metadata
+
+class PreservationPackage:
+
+    def __init__(self):
+        self.issn = None
+        self.article_id = None
+        self.metadata = None
+        self.article_dir = None
+        self.article_files = None
+        self.package_dir = None
+
+    def create_article_bagit_structure(self):
+        """
+        Create directory structure for packaging
+        Create necessary files
+        Create bagit files
+        :return:
+        """
+
+        journal_dir = os.path.join(self.package_dir, self.issn)
+        if not os.path.exists(journal_dir):
+            os.mkdir(journal_dir)
+
+        dest_article_dir = os.path.join(journal_dir, self.article_id)
+        if not os.path.exists(dest_article_dir):
+            # Create article directory
+            os.mkdir(dest_article_dir)
+
+            # Create metadata directory
+            metada_dir = os.path.join(dest_article_dir, "metadata")
+            if not os.path.exists(metada_dir):
+                os.mkdir(metada_dir)
+
+            # Copy the files from user uploaded directory to the package
+            for file in self.article_files:
+                if not file == Preservation.IDENTIFIER_FILE:
+                    src = os.path.join(self.article_dir, file)
+                    dest = os.path.join(dest_article_dir,file)
+                    shutil.copy(src,dest)
+
+            # Create metadata file with article information
+            with open(os.path.join(metada_dir, "metadata.json"), 'w+') as metadata_file:
+                metadata_file.write(json.dumps(self.metadata, indent=4))
+
+            # Create a tag file with uuid of the article
+            with open(os.path.join(metada_dir, "tag.txt"), 'w+') as metadata_file:
+                metadata_file.write(json.dumps(self.article_id, indent=4))
+
+        # Bag the article
+        bagit.make_bag(dest_article_dir, checksums=["sha256"])
+
 
