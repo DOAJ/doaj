2a2cc31ae878f8e7765874f3dfda2bb3548c91f0
Aga Domanska <aga@cottagelabs.com>
2021-08-03 15:04:27 +0100

    fix the script

--- portality/scripts/find_oa_start_records.py
+++ portality/scripts/find_oa_start_records.py
@@ -1,6 +1,7 @@
 import csv
 import requests
 
+from portality.core import app
 
 if __name__ == "__main__":
 
@@ -16,11 +17,11 @@ if __name__ == "__main__":
         out = "oa_start_out.csv"
 
     headers = {
-        'Authorization': 'Basic ZG9hal9kZXY6ODgxODEqaHRz',
+        'Authorization': 'Basic ' + app.config.get("AUTHORIZATION_CODE"),
         'Content-Type': 'text/plain',
     }
 
-    data = '{ "_source": ["bibjson.oa_start"], "query": { "filtered": { "filter": { "exists" : { "field" : "bibjson.oa_start" } }, "query": { "match_all": {} } } }, "size": 20000 }'
+    data = '{ "_source": ["bibjson.oa_start", "admin.in_doaj"], "query": { "filtered": { "filter": { "exists" : { "field" : "bibjson.oa_start" } }, "query": { "match_all": {} } } }, "size": 200000 }'
 
     response = requests.post('https://doajes.cottagelabs.com/doaj/journal/_search', headers=headers, data=data)
 
@@ -29,9 +30,13 @@ if __name__ == "__main__":
 
     with open(out, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
-        writer.writerow(["ID","OA Start Date"])
+        writer.writerow(["ID", "OA Start Date", "In doaj?"])
 
         for h in hits:
-            writer.writerow([h["_id"], h["_source"]["bibjson"]["oa_start"]["year"]])
+            try:
+                in_doaj = h["_source"]["admin"]["in_doaj"]
+            except KeyError:
+                in_doaj = ""
+            writer.writerow([h["_id"], h["_source"]["bibjson"]["oa_start"]["year"], in_doaj])
 
 
