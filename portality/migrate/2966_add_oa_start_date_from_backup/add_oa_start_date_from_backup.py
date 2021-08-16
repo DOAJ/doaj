ad0d43246ab135facdfa865b4c335ca2bb2db491
Aga Domanska <aga@cottagelabs.com>
2021-08-16 19:38:53 +0100

    write data in bulk and remove the path to the data file - require the arg instead in the script

--- portality/migrate/2966_add_oa_start_date_from_backup/add_oa_start_date_from_backup.py
+++ portality/migrate/2966_add_oa_start_date_from_backup/add_oa_start_date_from_backup.py
@@ -1,6 +1,10 @@
 import csv
 from copy import deepcopy
+from datetime import datetime
 
+import esprit
+
+from portality.core import es_connection
 from portality.models import Journal
 from portality.settings import BASE_FILE_PATH
 
@@ -9,28 +13,42 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--out", help="output file path")
+    parser.add_argument("-d", "--data", help="csv with backup data")
     args = parser.parse_args()
 
-    if not args.out:
-        print("Please specify an output file path with the -o option")
-        parser.print_help()
-        exit()
+    if not args.data:
+        #print("Please specify a csv data file path with the -d option")
+        #parser.print_help()
+        #exit()
+        args.data = BASE_FILE_PATH + "/migrate/2966_add_oa_start_date_from_backup/oa_start_out.csv"
 
-    with open(args.out, "w", encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(['Journal ID', 'OA Start Date'])
+    try:
+        f = open(args.data)
+    except:
+        print("Could not open file: " + args.data + ". Try again.")
 
-        f = open(BASE_FILE_PATH + "/migrate/2966_add_oa_start_date_from_backup/oa_start_out.csv")
-        csv_reader = csv.reader(f)
-        header = next(csv_reader)
-        if header is not None:
-            for row in csv_reader:
-                print(row)
-                j = Journal.pull(row[0])
-                if j is not None:
-                    j.bibjson().oa_start = row[1]
-                    j.save()
-                    writer.writerow([j.id, j.bibjson().oa_start])
-        f.close()
+    batch = []
+    batch_size = 1000
+    csv_reader = csv.reader(f)
+    header = next(csv_reader)
+    if header is not None:
+        for row in csv_reader:
+            print(row[0])
+            j = Journal.pull(row[0])
+            if j is not None and "oa_start" not in j["bibjson"]:
+                data = j.data
+                data["bibjson"]["oa_start"] = row[1]
+
+                batch.append(data)
+
+                if len(batch) >= batch_size:
+                    print(datetime.now(), "writing ", len(batch), "to", "journal")
+                    esprit.raw.bulk(es_connection, batch, idkey="id", type_="journal", bulk_type="index")
+                    batch = []
+
+            if len(batch) > 0:
+                print(datetime.now(), "final result set / writing ", len(batch), "to", "journal")
+                esprit.raw.bulk(es_connection, batch, idkey="id", type_="journal", bulk_type="index")
+
+    f.close()
 
