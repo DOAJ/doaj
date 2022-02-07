1fe90d101d063946f08a0b76c4f4b44bfc9fb1f0
Richard Jones <richard@cottagelabs.com>
2022-02-07 21:21:05 +0000

    add list of editor groups for managing editor dashboard

--- portality/view/dashboard.py
+++ portality/view/dashboard.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
 from flask_login import current_user, login_required
+
+from portality import models
 from portality.decorators import ssl_required
 from portality.bll import DOAJ
 
@@ -15,5 +17,11 @@ def top_todo():
     # ~~-> Todo:Service~~
     svc = DOAJ.todoService()
     todos = svc.top_todo(current_user._get_current_object(), size=100)  # FIXME: 100 is probably too large, just using that to get a good view of the data during dev
+
+    # ~~-> EditorGroup:Model ~~
+    egs = []
+    if current_user.has_role("admin"):
+        egs = [e for e in models.EditorGroup.groups_by_maned(current_user.id)]
+
     # ~~-> Dashboard:Page~~
-    return render_template('dashboard/index.html', todos=todos)
+    return render_template('dashboard/index.html', todos=todos, maned_of=egs)
