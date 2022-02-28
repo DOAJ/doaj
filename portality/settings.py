7cafc310beb36078d588491f44e76a5e4625eea7
Steve Eardley <steve@eardley.xyz>
2022-02-28 15:41:45 +0000

    Version bump for applications / ur release

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "6.0.6"
+DOAJ_VERSION = "6.1.0"
 API_VERSION = "3.0.0"
 
 ######################################
