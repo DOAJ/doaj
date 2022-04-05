a2228dd0c29dc51fcd54d5ae14bba33023773457
Richard Jones <richard@cottagelabs.com>
2022-04-05 11:59:50 +0100

    fix typo in config value which prevents the limit on update requests shown being applied

--- portality/lib/query_filters.py
+++ portality/lib/query_filters.py
@@ -49,7 +49,7 @@ def owner(q):
 
 def update_request(q):
     q.clear_match_all()
-    q.add_must_filter({"range" : {"created_date" : {"gte" : app.config.get("UPDATE_REQUEST_SHOW_OLDEST")}}})
+    q.add_must_filter({"range" : {"created_date" : {"gte" : app.config.get("UPDATE_REQUESTS_SHOW_OLDEST")}}})
     q.add_must_filter({"term" : {"admin.application_type.exact" : constants.APPLICATION_TYPE_UPDATE_REQUEST}})
     return q
 
