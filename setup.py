42267a608c6dc475ec158e3339ee47bb0fec0e2b
Steve Eardley <steve@eardley.xyz>
2022-04-06 13:33:40 +0100

    pin jinja2 depencency

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         "Faker==2.0.3",
         "feedparser==6.0.8",
         "itsdangerous==2.0.1",     # fixme: unpinned dependency of flask, 2.1.0 is causing an import error 'json'
+        "jinja2<3.1.0",            # fixme: unpinned dependency of flask, import error on 'escape'
         "Flask==1.1.1",
         "Flask-Cors==3.0.8",
         "Flask-DebugToolbar==0.10.1",
