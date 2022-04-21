f26bdfa1ff0a388fb7bd2d473cf7b4b03fa61f6d
Aga Domanska <aga@cottagelabs.com>
2022-04-21 13:31:55 +0100

    add unit test

---
+++ doajtest/unit/event_consumers/test_application_publisher_revision_notify.py
@@ -0,0 +1,65 @@
+from portality import models
+from portality import constants
+from portality.bll import exceptions
+from doajtest.helpers import DoajTestCase
+from doajtest.fixtures import ApplicationFixtureFactory
+import time
+
+from portality.events.consumers.application_publisher_revision_notify import ApplicationPublisherRevisionNotify
+
+
+class TestApplicationPublisherRevisionNotify(DoajTestCase):
+    def setUp(self):
+        super(TestApplicationPublisherRevisionNotify, self).setUp()
+
+    def tearDown(self):
+        super(TestApplicationPublisherRevisionNotify, self).tearDown()
+
+    def test_consumes(self):
+        source = ApplicationFixtureFactory.make_application_source()
+
+        event = models.Event(constants.EVENT_APPLICATION_STATUS, context={"application": "abcd", "old_status": "in progress", "new_status": "revisions_required"})
+        assert ApplicationPublisherRevisionNotify.consumes(event)
+
+        event = models.Event(constants.EVENT_APPLICATION_STATUS,
+                             context={"application": "abcd", "old_status": "revisions_required", "new_status": "revisions_required"})
+        assert not ApplicationPublisherRevisionNotify.consumes(event)
+
+        event = models.Event("test:event", context={"application" : "abcd"})
+        assert not ApplicationPublisherRevisionNotify.consumes(event)
+
+        event = models.Event(constants.EVENT_APPLICATION_STATUS)
+        assert not ApplicationPublisherRevisionNotify.consumes(event)
+
+    def test_consume_success(self):
+        self._make_and_push_test_context("/")
+
+        source = ApplicationFixtureFactory.make_application_source()
+        app = models.Application(**source)
+        app.save()
+
+        acc = models.Account()
+        acc.set_id("publisher")
+        acc.set_email("test@example.com")
+        acc.save()
+
+        event = models.Event(constants.EVENT_APPLICATION_STATUS, context={"application": "abcdefghijk", "old_status": "in progress", "new_status": "revisions_required"})
+        ApplicationPublisherRevisionNotify.consume(event)
+
+        time.sleep(2)
+        ns = models.Notification.all()
+        assert len(ns) == 1
+
+        n = ns[0]
+        assert n.who == "publisher", "Expected: {}, Received: {}".format("publisher", n.who)
+        assert n.created_by == ApplicationPublisherRevisionNotify.ID, "Expected: {}, Received: {}".format(ApplicationPublisherRevisionNotify.ID, n.created_by)
+        assert n.classification == constants.NOTIFICATION_CLASSIFICATION_STATUS_CHANGE, "Expected: {}, Received: {}".format(constants.NOTIFICATION_CLASSIFICATION_STATUS_CHANGE, n.classification)
+        assert n.message is not None
+        assert n.action is None
+        assert not n.is_seen()
+
+    def test_consume_fail(self):
+        event = models.Event(constants.EVENT_APPLICATION_ASSED_ASSIGNED, context={"application": "abcd"})
+        with self.assertRaises(exceptions.NoSuchObjectException):
+            ApplicationPublisherRevisionNotify.consume(event)
+
