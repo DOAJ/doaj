7f9a7cd9f17a2ce578205a82a681a56772068297
Aga Domanska <aga@cottagelabs.com>
2022-04-27 11:45:30 +0100

    migration template

---
+++ portality/migrate/3245_ur_vs_app/README.md
@@ -0,0 +1,3 @@
+https://github.com/DOAJ/doajPM/issues/3245
+
+The script looks for applications which are rejected and if they have a `related_journal` then it tags them as update requests.
---
+++ portality/migrate/3245_ur_vs_app/operations.py
@@ -0,0 +1,6 @@
+def make_update_request(application):
+    print(application.bibjson().title)
+    print(application.application_status)
+    print(application.application_type)
+    print(" ")
+
