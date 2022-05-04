68d7bfacaa6a468228330ec55361b891afa09dc0
Aga Domanska <aga@cottagelabs.com>
2022-05-04 12:46:22 +0100

    fix import error

--- portality/bll/services/events.py
+++ portality/bll/services/events.py
@@ -5,14 +5,14 @@ from portality.events.consumers.account_created_email import AccountCreatedEmail
 from portality.events.consumers.application_assed_inprogress_notify import ApplicationAssedInprogressNotify
 from portality.events.consumers.application_assed_assigned_notify import ApplicationAssedAssignedNotify
 from portality.events.consumers.bg_job_finished_notify import BGJobFinishedNotify
-from portality.events.consumers.application_maned_ready_notify import ApplicationManedReadyNotify
+from portality.events.consumers.application_maned_ready_notify import ApplicationManedReadyNotifyg
 from portality.events.consumers.application_publisher_created_notify import ApplicationPublisherCreatedNotify
 from portality.events.consumers.application_publisher_revision_notify import ApplicationPublisherRevisionNotify
 from portality.events.consumers.application_editor_completed_notify import ApplicationEditorCompletedNotify
 from portality.events.consumers.application_editor_inprogress_notify import ApplicationEditorInProgressNotify
 from portality.events.consumers.account_passwordreset_email import AccountPasswordResetEmail
 from portality.events.consumers.application_editor_group_assigned_notify import ApplicationEditorGroupAssignedNotify
-from portality.events.consumers.application_application_accepted_notify import ApplicationPublisherAcceptedNotify
+from portality.events.consumers.application_publisher_accepted_notify import ApplicationPublisherAcceptedNotify
 from portality.events.consumers.update_request_publisher_accepted_notify import UpdateRequestPublisherAcceptedNotify
 from portality.events.consumers.application_publisher_assigned_notify import ApplicationPublisherAssignedNotify
 from portality.events.consumers.update_request_publisher_assigned_notify import UpdateRequestPublisherAssignedNotify
@@ -22,23 +22,23 @@ from portality.events.consumers.journal_editor_group_assigned_notify import Jour
 
 class EventsService(object):
     EVENT_CONSUMERS = [
-        AccountCreatedEmail,
-        AccountPasswordResetEmail,
-        ApplicationAssedInprogressNotify,
-        ApplicationAssedAssignedNotify,
-        ApplicationEditorCompletedNotify,
-        ApplicationEditorInProgressNotify,
-        ApplicationEditorGroupAssignedNotify,
-        ApplicationManedReadyNotify,
-        ApplicationPublisherAcceptedNotify,
-        ApplicationPublisherAssignedNotify,
-        BGJobFinishedNotify,
-        ApplicationPublisherCreatedNotify,
-        ApplicationPublisherRevisionNotify,
-        JournalAssedAssignedNotify,
-        JournalEditorGroupAssignedNotify,
-        UpdateRequestPublisherAcceptedNotify,
-        UpdateRequestPublisherAssignedNotify
+        # AccountCreatedEmail,
+        # AccountPasswordResetEmail,
+        # ApplicationAssedInprogressNotify,
+        # ApplicationAssedAssignedNotify,
+        # ApplicationEditorCompletedNotify,
+        # ApplicationEditorInProgressNotify,
+        # ApplicationEditorGroupAssignedNotify,
+        # ApplicationManedReadyNotify,
+        # ApplicationPublisherAcceptedNotify,
+        # ApplicationPublisherAssignedNotify,
+        # BGJobFinishedNotify,
+        # ApplicationPublisherCreatedNotify,
+        # ApplicationPublisherRevisionNotify,
+        # JournalAssedAssignedNotify,
+        # JournalEditorGroupAssignedNotify,
+        # UpdateRequestPublisherAcceptedNotify,
+        # UpdateRequestPublisherAssignedNotify
     ]
 
     def __init__(self):
