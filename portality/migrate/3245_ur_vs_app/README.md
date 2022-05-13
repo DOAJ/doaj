44765b4e0831a6e84ded9614749d9f7ed733e916
Steve Eardley <steve@eardley.xyz>
2022-05-13 15:58:10 +0100

    Diagnosing issues with upgrade script

--- portality/migrate/3245_ur_vs_app/README.md
+++ portality/migrate/3245_ur_vs_app/README.md
@@ -1,3 +1,5 @@
 https://github.com/DOAJ/doajPM/issues/3245
 
 The script looks for applications which are rejected and if they have a `related_journal` then it tags them as update requests.
+
+    python portality/upgrade.py -u portality/migrate/3245_ur_vs_app/migrate.json
--- portality/migrate/3245_ur_vs_app/operations.py
+++ portality/migrate/3245_ur_vs_app/operations.py
@@ -4,6 +4,5 @@ from portality import constants
 def make_update_request(application):
     if application.related_journal and not application.application_type == constants.APPLICATION_TYPE_UPDATE_REQUEST:
         application.application_type = constants.APPLICATION_TYPE_UPDATE_REQUEST
-        application.save()
     return application
 
