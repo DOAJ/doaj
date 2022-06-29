4e54a78930ee478e26e60ce2c3516d8cbb56fdd2
philip <philip.k@cottagelabs.com>
2022-06-29 16:51:23 +0100

    fixing and speed up unittest

--- doajtest/unit/test_bll_todo_top_todo.py
+++ doajtest/unit/test_bll_todo_top_todo.py
@@ -1,15 +1,18 @@
-from parameterized import parameterized
+import itertools
+from collections import Counter
+from datetime import datetime
+
 from combinatrix.testintegration import load_parameter_sets
+from parameterized import parameterized
 
 from doajtest.fixtures import ApplicationFixtureFactory, AccountFixtureFactory
 from doajtest.helpers import DoajTestCase
+from portality import constants
+from portality import models
 from portality.bll import DOAJ
 from portality.bll import exceptions
-from portality import models
-from portality import constants
-from portality.lib.paths import rel2abs
 from portality.lib import dates
-from datetime import datetime
+from portality.lib.paths import rel2abs
 
 
 def load_cases():
@@ -24,12 +27,43 @@ EXCEPTIONS = {
 
 class TestBLLTopTodo(DoajTestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestBLLTopTodo, cls).setUpClass()
+        apps = []
+        w = 7*24*60*60
+
+        # an application stalled for more than 8 weeks
+        cls.build_application("maned_stalled__maned_old", 10*w, 10*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+
+        # an application that is not stalled
+        cls.build_application("unstalled", 2*w, 2*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+
+        # an application that is old but rejected
+        cls.build_application("rejected", 11 * w, 11 * w, constants.APPLICATION_STATUS_REJECTED, apps)
+
+        # an application that was created over 10 weeks ago (but updated recently)
+        cls.build_application("maned_old", 2 * w, 11 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+
+        # an application that was created recently
+        cls.build_application("not_old", 2 * w, 2 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+
+        # an application that was created over 10 weeks ago and is accepted
+        cls.build_application("accepted", 12 * w, 12 * w, constants.APPLICATION_STATUS_ACCEPTED, apps)
+
+        models.Application.blockall([(ap.id, ap.last_updated) for ap in apps])
+
     def setUp(self):
         super(TestBLLTopTodo, self).setUp()
         self.svc = DOAJ.todoService()
 
     def tearDown(self):
-        super(TestBLLTopTodo, self).tearDown()
+        pass   # avoid reset db records
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        cls.reset_db_record()
 
     @parameterized.expand(load_cases)
     def test_top_todo(self, name, kwargs):
@@ -48,28 +82,6 @@ class TestBLLTopTodo(DoajTestCase):
         ###############################################
         ## set up
 
-        apps = []
-        w = 7*24*60*60
-
-        # an application stalled for more than 8 weeks
-        self.build_application("maned_stalled__maned_old", 10*w, 10*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
-
-        # an application that is not stalled
-        self.build_application("unstalled", 2*w, 2*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
-
-        # an application that is old but rejected
-        self.build_application("rejected", 11 * w, 11 * w, constants.APPLICATION_STATUS_REJECTED, apps)
-
-        # an application that was created over 10 weeks ago (but updated recently)
-        self.build_application("maned_old", 2 * w, 11 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
-
-        # an application that was created recently
-        self.build_application("not_old", 2 * w, 2 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
-
-        # an application that was created over 10 weeks ago and is accepted
-        self.build_application("accepted", 12 * w, 12 * w, constants.APPLICATION_STATUS_ACCEPTED, apps)
-
-        models.Application.blockall([(ap.id, ap.last_updated) for ap in apps])
 
         account = None
         if account_arg == "admin":
@@ -96,18 +108,11 @@ class TestBLLTopTodo(DoajTestCase):
 
         if raises is not None:
             with self.assertRaises(raises):
-                todos = self.svc.top_todo(account, size)
+                self.svc.top_todo(account, size)
         else:
             todos = self.svc.top_todo(account, size)
-
-            actions = {}
-            for todo in todos:
-                for aid in todo["action_id"]:
-                    if aid not in actions:
-                        actions[aid] = 0
-                    actions[aid] += 1
-
-            for k, v in category_args:
+            actions = Counter(itertools.chain.from_iterable(todo["action_id"] for todo in todos))
+            for k, v in category_args.items():
                 assert actions.get(k, 0) == v
 
             # todo_maned_stalled_expected = int(todo_maned_stalled_arg)
@@ -116,7 +121,8 @@ class TestBLLTopTodo(DoajTestCase):
             # todo_maned_follow_up_old_expected = int(todo_maned_follow_up_old_arg)
             # assert actions.get(constants.TODO_MANED_FOLLOW_UP_OLD, 0) == todo_maned_follow_up_old_expected
 
-    def build_application(self, id, lmu_diff, cd_diff, status, app_registry):
+    @staticmethod
+    def build_application(id, lmu_diff, cd_diff, status, app_registry):
         source = ApplicationFixtureFactory.make_application_source()
         ap = models.Application(**source)
         ap.set_id(id)
