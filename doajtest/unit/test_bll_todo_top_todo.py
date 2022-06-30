7c77f95573533d65982535515a7a8d0f82a7d784
philip <philip.k@cottagelabs.com>
2022-06-30 13:31:48 +0100

    add assed test cases

--- doajtest/unit/test_bll_todo_top_todo.py
+++ doajtest/unit/test_bll_todo_top_todo.py
@@ -11,6 +11,8 @@ from portality import constants
 from portality import models
 from portality.bll import DOAJ
 from portality.bll import exceptions
+from portality.constants import TODO_ASSOCIATE_PROGRESS_STALLED, TODO_ASSOCIATE_START_PENDING, \
+    TODO_ASSOCIATE_FOLLOW_UP_OLD
 from portality.lib import dates
 from portality.lib.paths import rel2abs
 
@@ -30,26 +32,33 @@ class TestBLLTopTodo(DoajTestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super(TestBLLTopTodo, cls).setUpClass()
-        apps = []
         w = 7*24*60*60
+        apps = [
+            # an application stalled for more than 8 weeks
+            cls.build_application("maned_stalled__maned_old", 10*w, 10*w, constants.APPLICATION_STATUS_IN_PROGRESS),
 
-        # an application stalled for more than 8 weeks
-        cls.build_application("maned_stalled__maned_old", 10*w, 10*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+            # an application that is not stalled
+            cls.build_application("unstalled", 2*w, 2*w, constants.APPLICATION_STATUS_IN_PROGRESS),
 
-        # an application that is not stalled
-        cls.build_application("unstalled", 2*w, 2*w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+            # an application that is old but rejected
+            cls.build_application("rejected", 11 * w, 11 * w, constants.APPLICATION_STATUS_REJECTED),
 
-        # an application that is old but rejected
-        cls.build_application("rejected", 11 * w, 11 * w, constants.APPLICATION_STATUS_REJECTED, apps)
+            # an application that was created over 10 weeks ago (but updated recently)
+            cls.build_application("maned_old", 2 * w, 11 * w, constants.APPLICATION_STATUS_IN_PROGRESS),
 
-        # an application that was created over 10 weeks ago (but updated recently)
-        cls.build_application("maned_old", 2 * w, 11 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+            # an application that was created recently
+            cls.build_application("not_old", 2 * w, 2 * w, constants.APPLICATION_STATUS_IN_PROGRESS),
 
-        # an application that was created recently
-        cls.build_application("not_old", 2 * w, 2 * w, constants.APPLICATION_STATUS_IN_PROGRESS, apps)
+            # an application that was created over 10 weeks ago and is accepted
+            cls.build_application("accepted", 12 * w, 12 * w, constants.APPLICATION_STATUS_ACCEPTED),
 
-        # an application that was created over 10 weeks ago and is accepted
-        cls.build_application("accepted", 12 * w, 12 * w, constants.APPLICATION_STATUS_ACCEPTED, apps)
+            # special editor, so it will not match TODO_ASSOCIATE_START_PENDING
+            cls.build_application("special_editor", 2 * w, 2 * w, constants.APPLICATION_STATUS_IN_PROGRESS,
+                                  editor='special_editorXXX'),
+
+            # status is pending
+            cls.build_application("pending_12", 12 * w, 12 * w, constants.APPLICATION_STATUS_PENDING),
+        ]
 
         models.Application.blockall([(ap.id, ap.last_updated) for ap in apps])
 
@@ -62,8 +71,8 @@ class TestBLLTopTodo(DoajTestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        super().tearDownClass()
         cls.reset_db_record()
+        super().tearDownClass()
 
     @parameterized.expand(load_cases)
     def test_top_todo(self, name, kwargs):
@@ -82,20 +91,7 @@ class TestBLLTopTodo(DoajTestCase):
         ###############################################
         ## set up
 
-
-        account = None
-        if account_arg == "admin":
-            asource = AccountFixtureFactory.make_managing_editor_source()
-            account = models.Account(**asource)
-        elif account_arg == "editor":
-            asource = AccountFixtureFactory.make_editor_source()
-            account = models.Account(**asource)
-        elif account_arg == "assed":
-            asource = AccountFixtureFactory.make_assed1_source()
-            account = models.Account(**asource)
-        elif account_arg == "no_role":
-            asource = AccountFixtureFactory.make_publisher_source()
-            account = models.Account(**asource)
+        account = self.make_account_by_type(account_arg)
 
         size = int(size_arg)
 
@@ -121,13 +117,62 @@ class TestBLLTopTodo(DoajTestCase):
             # todo_maned_follow_up_old_expected = int(todo_maned_follow_up_old_arg)
             # assert actions.get(constants.TODO_MANED_FOLLOW_UP_OLD, 0) == todo_maned_follow_up_old_expected
 
+
+    def test_todo__assed_normal(self):
+        account = self.make_account_by_type('assed')
+        size = 100
+
+
+        todos = self.svc.top_todo(account, size)
+
+        app_action_dict = {
+            t['object_id']: set(t['action_id']) for t in todos
+        }
+
+        # breakpoint()
+        excepted_app_action_dict = {
+            "maned_stalled__maned_old": {TODO_ASSOCIATE_PROGRESS_STALLED, TODO_ASSOCIATE_START_PENDING,
+                                         TODO_ASSOCIATE_FOLLOW_UP_OLD},
+            "unstalled": {TODO_ASSOCIATE_START_PENDING},
+            "rejected": {TODO_ASSOCIATE_START_PENDING, TODO_ASSOCIATE_PROGRESS_STALLED},
+            "maned_old": {TODO_ASSOCIATE_FOLLOW_UP_OLD, TODO_ASSOCIATE_START_PENDING},
+            "not_old": {TODO_ASSOCIATE_START_PENDING},
+            "accepted": {TODO_ASSOCIATE_START_PENDING, TODO_ASSOCIATE_PROGRESS_STALLED},
+            "pending_12": {TODO_ASSOCIATE_PROGRESS_STALLED, TODO_ASSOCIATE_FOLLOW_UP_OLD},
+        }
+
+        self.assertEqual(app_action_dict, excepted_app_action_dict)
+
+
     @staticmethod
-    def build_application(id, lmu_diff, cd_diff, status, app_registry):
+    def make_account_by_type(acc_type):
+        asource = None
+        if acc_type == "admin":
+            asource = AccountFixtureFactory.make_managing_editor_source()
+        elif acc_type == "editor":
+            asource = AccountFixtureFactory.make_editor_source()
+        elif acc_type == "assed":
+            asource = AccountFixtureFactory.make_assed1_source()
+        elif acc_type == "no_role":
+            asource = AccountFixtureFactory.make_publisher_source()
+
+        if asource is None:
+            return None
+        else:
+            return models.Account(**asource)
+
+
+    @staticmethod
+    def build_application(id, lmu_diff, cd_diff, status, editor=None):
         source = ApplicationFixtureFactory.make_application_source()
         ap = models.Application(**source)
         ap.set_id(id)
         ap.set_last_manual_update(dates.before(datetime.utcnow(), lmu_diff))  # 10 weeks ago
         ap.set_created(dates.before(datetime.utcnow(), cd_diff))  # 10 weeks ago
         ap.set_application_status(status)
+
+        if editor is not None:
+            ap.set_editor(editor)
+
         ap.save()
-        app_registry.append(ap)
+        return ap
