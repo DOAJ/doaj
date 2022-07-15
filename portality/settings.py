dde9dcf695c85873467d218f69ba1b9fa78e70df
Steve Eardley <steve@eardley.xyz>
2022-07-15 08:45:07 +0100

    Update conf for plausible

--- portality/settings.py
+++ portality/settings.py
@@ -1236,7 +1236,7 @@ TODO_LIST_SIZE = 48
 #######################################################
 # Plausible analytics
 # root url of plausible
-PLAUSIBLE_URL = "http://localhost:8000"
+PLAUSIBLE_URL = "https://plausible.io"
 PLAUSIBLE_JS_URL = PLAUSIBLE_URL + "/js/plausible.js"
 PLAUSIBLE_API_URL = PLAUSIBLE_URL + "/api/event/"
 # site name / domain name that used to register in plausible
