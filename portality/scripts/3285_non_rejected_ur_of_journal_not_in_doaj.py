7e2a0c4ce4be9c6076e15e24bcfcece5410f6d13
Steve Eardley <steve@eardley.xyz>
2022-08-04 10:57:39 +0100

    Extra info in UR script for 3285

--- portality/scripts/3285_non_rejected_ur_of_journal_not_in_doaj.py
+++ portality/scripts/3285_non_rejected_ur_of_journal_not_in_doaj.py
@@ -1,13 +1,11 @@
 import csv
-import esprit
-from portality.core import es_connection, app
-from portality.util import ipt_prefix
 from portality import models, constants
 
-def non_rejected_urs_of_journals_not_in_doaj(csvwriter=None):
-    """ Scroll through all applications, return unrejected update requests of journals that are no longer in doaj
-    """
+
+def non_rejected_urs_of_journals_not_in_doaj(include_missing: bool, csv_writer: csv.writer):
+    """ Scroll through all applications, return unrejected update requests of journals that are no longer in doaj """
     urs = []
+    csv_writer.writerow(["Application ID", "Reason"])
     for app in models.Application.iterate():
         if app.application_type != constants.APPLICATION_TYPE_UPDATE_REQUEST:
             continue
@@ -15,30 +13,26 @@ def non_rejected_urs_of_journals_not_in_doaj(csvwriter=None):
         if app.application_status != constants.APPLICATION_STATUS_REJECTED:
             if app.current_journal:
                 j = models.Journal.pull(app.current_journal)
-                if j.is_in_doaj():
-                    urs.append(app.id)
+
+                if j is None:
+                    writer.writerow([app.id, f"Journal {app.current_journal} could not be found"])
+                elif not j.is_in_doaj():
+                    writer.writerow([app.id, f"Journal {app.current_journal} not in DOAJ"])
+            else:
+                if include_missing:
+                    writer.writerow([app.id, "Application's current_journal is missing"])
 
     return urs
+
 
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--out", help="output file path")
+    parser.add_argument("-o", "--out", help="output file path", default=f'{__file__}_out.csv')
+    parser.add_argument("-f", "--full", help="Report on mising current_journal fields", action='store_true')
     args = parser.parse_args()
-
-    if not args.out:
-        print("Please specify an output file path with the -o option")
-        parser.print_help()
-        exit()
 
     with open(args.out, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
-        writer.writerow(["update request id"])
-
-        for ur in non_rejected_urs_of_journals_not_in_doaj():
-            print(ur)
-            writer.writerow([
-                ur
-            ])
-
+        non_rejected_urs_of_journals_not_in_doaj(args.full, writer)
