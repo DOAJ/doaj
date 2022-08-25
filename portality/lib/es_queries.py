4322c848fa5b34cf376ed5fa248da84d840bb63c
philip <philip.k@cottagelabs.com>
2022-08-25 11:24:04 +0100

    better version of query_all()

--- portality/lib/es_queries.py
+++ portality/lib/es_queries.py
@@ -3,11 +3,7 @@ ES_DATETIME_FMT = '%Y-%m-%dT%H:%M:%SZ'
 
 def query_all():
     return {
-        'query': {
-            'bool': {
-                'must': [
-                    {'match_all': {}}
-                ]
-            }
+        "query": {
+            "match_all": {}
         }
     }
