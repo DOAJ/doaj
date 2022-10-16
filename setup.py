19bf89ca5c28d14b200ba3e24c4784c32b8c3a19
Steve Eardley <steve@eardley.xyz>
2022-10-16 19:03:05 +0100

    change back to getting pycountry from pypi, even though it's outdated.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "openpyxl~=3.0.3",  # this package is needed for script only https://github.com/DOAJ/doajPM/issues/2433
         "parameterized==0.7.0",
         "psutil==5.6.3",
-        #"pycountry==22.3.5",  #todo: pycountry on pypi got outdated so we're checking out from github instead for now
+        "pycountry==22.3.5",  # FIXME: pycountry on pypi is quite outdated (2022-03-05, missing e.g. Türkiye)
         "python-dateutil==2.8.0",  # something else already installs this, so just note we need it without an explicit version freeze
         "pytz==2019.3",
         "redis==3.3.11",
