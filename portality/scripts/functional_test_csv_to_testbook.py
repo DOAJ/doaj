2fec58463f16d8f1691381a2ac5de022c75f3243
Richard Jones <richard@cottagelabs.com>
2022-10-26 16:55:53 +0100

    functional test migration to work in batches, and the first couple of migrated test sets

--- portality/scripts/functional_test_csv_to_testbook.py
+++ portality/scripts/functional_test_csv_to_testbook.py
@@ -12,28 +12,28 @@ def migrate(infile, outfile, suite, testset):
     current_test = {}
     current_step = {}
     for row in rows:
-        if _empty(row):
+        if _empty(row) or _note_row(row):
             last_row_empty = True
             continue
 
         if _test_title(row) and last_row_empty:
             if len(current_test.keys()) > 0:
                 tests.append(current_test)
-            current_test = {"title": row[2], "context" : {"role" : ""}, "steps": []}
+            current_test = {"title": row[2].strip(), "context" : {"role" : ""}, "steps": []}
         else:   # this is a regular test row
             if len(current_test.keys()) == 0:
                 raise Exception("Attempt to add step before test is initialised")
 
             if row[1]:
-                current_test["context"]["role"] = row[1]
+                current_test["context"]["role"] = row[1].strip()
             if row[2]:
                 if len(current_step.keys()) > 0:
                     current_test["steps"].append(current_step)
-                current_step = {"step": row[2]}
+                current_step = {"step": row[2].strip()}
             if row[3]:
                 if "results" not in current_step:
                     current_step["results"] = []
-                current_step["results"].append(row[3])
+                current_step["results"].append(row[3].strip())
 
         last_row_empty = False
 
@@ -62,7 +62,28 @@ def _empty(row):
     return row[0] == "" and row[1] == "" and row[2] == "" and row[3] == ""
 
 
-migrate("/home/richard/tmp/doaj/Dashboard - Editorial Group Status.csv",
-        "/home/richard/Code/External/doaj3/doajtest/testbook/dashboard/editorial_group_status.yml",
-        "Editor Search",
-        "Journals")
+def _note_row(row):
+    return row[0] != "" and row[1] == "" and row[2] == "" and row[3] == ""
+
+
+def migrate_all(indir, outdir):
+    for fn in os.listdir(indir):
+        print(fn)
+        rawname = fn[:-1 * len(".csv")]
+        suite, testset = rawname.split(" - ")
+        path = os.path.join(indir, fn)
+        subpath = os.path.join(_safe_name(suite), _safe_name(testset) + ".yml")
+        out = os.path.join(outdir, subpath)
+        migrate(path, out, suite, testset)
+
+
+def _safe_name(name):
+    return name.lower().replace(" ", "_")
+
+
+# migrate("/home/richard/tmp/doaj/functionaltests/Admin Article Metadata Form - Admin Article Metadata Form.csv",
+#         "/home/richard/Code/External/doaj3/doajtest/testbook/admin_article_metadata_form/admin_article_metadata_form.yml",
+#         "Admin Article Metadata Form",
+#         "Admin Article Metadata Form")
+
+migrate_all("/home/richard/tmp/doaj/functionaltests", "/home/richard/Code/External/doaj3/doajtest/testbook/migration/")
