013229fadc7d61b39bafe294376009459c17f0f0
Sophy <sophy.ouch@erudit.org>
2022-10-31 16:49:23 +0000

    Drop group info page (all moved to dashboard landing page Update icons in sidenav

--- portality/view/editor.py
+++ portality/view/editor.py
@@ -36,14 +36,6 @@ def index():
     return render_template('editor/dashboard.html', todos=todos, editor_of=editor_of, associate_of=associate_of, managing_editor=app.config.get("MANAGING_EDITOR_EMAIL"))
 
 # build an editor's page where things can be done
-@blueprint.route('/group-info')
-@login_required
-@ssl_required
-def groupinfo():
-    editor_of = models.EditorGroup.groups_by_editor(current_user.id)
-    associate_of = models.EditorGroup.groups_by_associate(current_user.id)
-    return render_template('editor/index.html', editor_of=editor_of, associate_of=associate_of, managing_editor=app.config.get("MANAGING_EDITOR_EMAIL"))
-
 @blueprint.route('/group_journals')
 @login_required
 @ssl_required
