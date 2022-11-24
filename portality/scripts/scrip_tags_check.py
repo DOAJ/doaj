47ebb0dd4de43ed44e8a7a51fe6e205c8bc9a48f
Aga Domanska <aga@cottagelabs.com>
2022-11-24 13:59:31 +0000

    args.out to script tags check

--- portality/scripts/scrip_tags_check.py
+++ portality/scripts/scrip_tags_check.py
@@ -25,18 +25,16 @@ if __name__ == "__main__":
 
     import argparse
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("-o", "--out", help="output file path")
-    # args = parser.parse_args()
-    #
-    # if not args.out:
-    #     print("Please specify an output file path with the -o option")
-    #     parser.print_help()
-    #     exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out", help="output file path")
+    args = parser.parse_args()
 
-    out = "out.txt"
+    if not args.out:
+        print("Please specify an output file path with the -o option")
+        parser.print_help()
+        exit()
 
-    with open(out, "w", encoding="utf-8") as f:
+    with open(args.out, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(["es_type", "ID", "Title", "field", "value"])
 
