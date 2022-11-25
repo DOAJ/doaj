c7a03919f43eb3817f4f6c6ec307415419a4d04a
Aga Domanska <aga@cottagelabs.com>
2022-11-25 12:58:36 +0000

    wrong if fixed

--- portality/api/current/data_objects/article.py
+++ portality/api/current/data_objects/article.py
@@ -173,8 +173,8 @@ class IncomingArticleDO(dataobj.DataObj, swagger.SwaggerSupport):
             if bibjson_element in bibjson:
                 for i in range(len(bibjson[bibjson_element]) - 1, -1, -1):
                     ide = bibjson[bibjson_element][i]
-                    if field is not None and field in ide:
-                        if ide[field] == "":
+                    if field is not None:
+                        if ide.get(field,"") == "":
                             bibjson[bibjson_element].remove(ide)
                     else:
                         if ide == "":
