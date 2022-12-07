04b2ad426c6f41150098b9a67f74ded5f26a1807
Steve Eardley <steve@eardley.xyz>
2022-12-07 14:57:45 +0000

    Fixes for upgrade to add admin records missing from articles

--- portality/migrate/20220928_1735_missing_admin/operations.py
+++ portality/migrate/20220928_1735_missing_admin/operations.py
@@ -1,19 +1,14 @@
-from portality.models import Journal
+from portality.models.article import NoJournalException
+
 def add_admin_field(article):
-    if not "admin" in article.data:
+
+    # Take this opportunity to delete any empty journal license field since it's not supported in the bibjson struct
+    if article.data.get('bibjson', {}).get('journal', {}).get('license') is not None:
+        del article.data['bibjson']['journal']['license']
+
+    if "admin" not in article.data:
         try:
-            j_data = Journal.find_by_issn(article.data["index"]["issn"])
-            if len(j_data):
-                print("Article matches more than one journal: ");
-                print(j_data);
-            else:
-                j = Journal.pull(j_data[0]["id"])
-        except:
-            print("\n Corrupted article, no issn: ");
-            print(article.data);
-        if j is not None:
-            article.data["admin"] = {"in_doaj": j.is_in_doaj}
-        else:
-            print("\n Article with no journal found: ");
-            log += article.data;
+            article.add_journal_metadata()
+        except NoJournalException:
+            print("\n Article with no journal found: " + article.id)
     return article
