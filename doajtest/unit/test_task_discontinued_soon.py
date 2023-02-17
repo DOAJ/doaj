f54af1c0f2e17efd8cdd7a8081a6d147ded17a59
Aga Domanska <aga@cottagelabs.com>
2023-02-17 12:40:48 +0000

    fix task test

--- doajtest/unit/test_task_discontinued_soon.py
+++ doajtest/unit/test_task_discontinued_soon.py
@@ -9,38 +9,40 @@ from portality.tasks import find_discontinued_soon
 from portality.ui.messages import Messages
 from doajtest.fixtures import JournalFixtureFactory
 
+DELTA = app.config.get('DISCONTINUED_DATE_DELTA',1)
+
 class TestDiscontinuedSoon(DoajTestCase):
 
-    def _date_tomorrow(self):
-        return (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    def _date_to_found(self):
+        return (datetime.datetime.today() + datetime.timedelta(days=DELTA)).strftime('%Y-%m-%d')
 
-    def _date_in_2_days(self):
-        return (datetime.datetime.today() + datetime.timedelta(days=2)).strftime('%Y-%m-%d')
+    def _date_too_late(self):
+        return (datetime.datetime.today() + datetime.timedelta(days=DELTA+1)).strftime('%Y-%m-%d')
 
     def test_discontinued_soon_found(self):
 
         # Both these should be found
-        journal_discontinued_tommorow_1 = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
-        journal_discontinued_tommorow_1.set_id("1")
-        jbib = journal_discontinued_tommorow_1.bibjson()
+        journal_discontinued_to_found_1 = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
+        journal_discontinued_to_found_1.set_id("1")
+        jbib = journal_discontinued_to_found_1.bibjson()
         jbib.title = "Discontinued Tomorrow 1"
-        jbib.discontinued_date = self._date_tomorrow()
-        journal_discontinued_tommorow_1.save(blocking=True)
+        jbib.discontinued_date = self._date_to_found()
+        journal_discontinued_to_found_1.save(blocking=True)
 
-        journal_discontinued_tommorow_2 = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
-        journal_discontinued_tommorow_2.set_id("2")
-        jbib = journal_discontinued_tommorow_2.bibjson()
+        journal_discontinued_to_found_2 = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
+        journal_discontinued_to_found_2.set_id("2")
+        jbib = journal_discontinued_to_found_2.bibjson()
         jbib.title = "Discontinued Tomorrow 2"
-        jbib.discontinued_date = self._date_tomorrow()
-        journal_discontinued_tommorow_2.save(blocking=True)
+        jbib.discontinued_date = self._date_to_found()
+        journal_discontinued_to_found_2.save(blocking=True)
 
         # that shouldn't be found
-        journal_discontinued_in_2_days = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
-        journal_discontinued_in_2_days.set_id("3")
-        jbib = journal_discontinued_in_2_days.bibjson()
+        journal_discontinued_too_late = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
+        journal_discontinued_too_late.set_id("3")
+        jbib = journal_discontinued_too_late.bibjson()
         jbib.title = "Discontinued In 2 days"
-        jbib.discontinued_date = self._date_in_2_days()
-        journal_discontinued_in_2_days.save(blocking=True)
+        jbib.discontinued_date = self._date_too_late()
+        journal_discontinued_too_late.save(blocking=True)
 
         job = find_discontinued_soon.FindDiscontinuedSoonBackgroundTask.prepare("system")
         task = find_discontinued_soon.FindDiscontinuedSoonBackgroundTask(job)
@@ -53,18 +55,18 @@ class TestDiscontinuedSoon(DoajTestCase):
     def test_discontinued_soon_not_found(self):
 
         # None of these should be found - this one discontinues in 2 days
-        journal_discontinued_in_2_days = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
-        journal_discontinued_in_2_days.set_id("1")
-        jbib = journal_discontinued_in_2_days.bibjson()
+        journal_discontinued_too_late = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=True))
+        journal_discontinued_too_late.set_id("1")
+        jbib = journal_discontinued_too_late.bibjson()
         jbib.title = "Discontinued In 2 days"
-        jbib.discontinued_date = self._date_in_2_days()
-        journal_discontinued_in_2_days.save(blocking=True)
+        jbib.discontinued_date = self._date_too_late()
+        journal_discontinued_too_late.save(blocking=True)
 
         # this one is not in doaj
         journal_not_in_doaj = models.Journal(**JournalFixtureFactory.make_journal_source(in_doaj=False))
         journal_not_in_doaj.set_id("2")
         jbib = journal_not_in_doaj.bibjson()
-        jbib.discontinued_date = self._date_tomorrow()
+        jbib.discontinued_date = self._date_to_found()
         journal_not_in_doaj.save(blocking=True)
 
         job = find_discontinued_soon.FindDiscontinuedSoonBackgroundTask.prepare("system")
