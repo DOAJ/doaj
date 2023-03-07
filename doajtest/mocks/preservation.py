6ccc9b21014dbb328c0882e2eca53828be7ba23d
Ramakrishna <mail2rk@gmail.com>
2023-03-07 20:29:55 +0530

    Updated unit tests and feature tests

--- doajtest/mocks/preservation.py
+++ doajtest/mocks/preservation.py
@@ -226,3 +226,227 @@ class PreservationMock:
         "es_type": "article"
     }
 
+    ARTICLE_DATA_JOURNAL2 = {
+        "index": {
+            "issn": [
+                "2673-611X"
+            ],
+            "date": "2016-10-01T00:00:00Z",
+            "date_toc_fv_month": "2016-10-01T00:00:00Z",
+            "subject": [
+                "Science: Natural history (General): General. Including nature conservation, geographical distribution"
+            ],
+            "schema_subject": [
+                "LCC:Neurology. Diseases of the nervous system"
+            ],
+            "classification": [
+                "Neurology. Diseases of the nervous system"
+            ],
+            "publisher": [
+                "Frontiers Media S.A."
+            ],
+            "license": [
+                "CC BY"
+            ],
+            "language": [
+                "English"
+            ],
+            "country": "United Kingdom",
+            "schema_code": [
+                "LCC:RC346-429"
+            ],
+            "classification_paths": [
+                "Medicine: Internal medicine: Neurosciences. Biological psychiatry. Neuropsychiatry: Neurology. Diseases of the nervous system"
+            ],
+            "unpunctitle": "Variation in TMEM106B in chronic traumatic encephalopathy",
+            "asciiunpunctitle": "Variation in TMEM106B in chronic traumatic encephalopathy",
+            "has_seal": "Yes",
+            "doi": "10.3389/fcosc.2022.1028295",
+            "fulltext": "https://frontiersin.org/articles/10.3389/fcosc.2022.1028295",
+            "schema_codes_tree": [
+                "LCC:RC31-1245",
+                "LCC:RC346-429",
+                "LCC:RC321-571",
+                "LCC:R"
+            ]
+        },
+        "last_updated": "2020-11-24T21:55:20Z",
+        "admin": {
+            "in_doaj": "true",
+            "seal": "true"
+        },
+        "created_date": "2018-11-04T12:37:46Z",
+        "id": "00005741594643f4996e2666a01e0310",
+        "bibjson": {
+            "title": "Variation in TMEM106B in chronic traumatic encephalopathy",
+            "year": "2018",
+            "month": "11",
+            "start_page": "1",
+            "end_page": "9",
+            "abstract": "Abstract The genetic basis of chronic traumatic encephalopathy (CTE) is poorly understood. Variation in transmembrane protein 106B (TMEM106B) has been associated with enhanced neuroinflammation during aging and with TDP-43-related neurodegenerative disease, and rs3173615, a missense coding SNP in TMEM106B, has been implicated as a functional variant in these processes. Neuroinflammation and TDP-43 pathology are prominent features in CTE. The purpose of this study was to determine whether genetic variation in TMEM106B is associated with CTE risk, pathological features, and ante-mortem dementia. Eighty-six deceased male athletes with a history of participation in American football, informant-reported Caucasian, and a positive postmortem diagnosis of CTE without comorbid neurodegenerative disease were genotyped for rs3173615. The minor allele frequency (MAF = 0.42) in participants with CTE did not differ from previously reported neurologically normal controls (MAF = 0.43). However, in a case-only analysis among CTE cases, the minor allele was associated with reduced phosphorylated tau (ptau) pathology in the dorsolateral frontal cortex (DLFC) (AT8 density, odds ratio [OR] of increasing one quartile = 0.42, 95% confidence interval [CI] 0.22–0.79, p = 0.008), reduced neuroinflammation in the DLFC (CD68 density, OR of increasing one quartile = 0.53, 95% CI 0.29–0.98, p = 0.043), and increased synaptic protein density (β = 0.306, 95% CI 0.065–0.546, p = 0.014). Among CTE cases, TMEM106B minor allele was also associated with reduced ante-mortem dementia (OR = 0.40, 95% CI 0.16–0.99, p = 0.048), but was not associated with TDP-43 pathology. All case-only models were adjusted for age at death and duration of football play. Taken together, variation in TMEM106B may have a protective effect on CTE-related outcomes.",
+            "journal": {
+                "volume": "6",
+                "number": "1",
+                "publisher": "BMC",
+                "title": "Acta Neuropathologica Communications",
+                "country": "GB",
+                "license": [
+                    {
+                        "title": "CC BY",
+                        "type": "CC BY",
+                        "url": "https://actaneurocomms.biomedcentral.com/submission-guidelines/copyright",
+                        "open_access": "true"
+                    }
+                ],
+                "language": [
+                    "EN"
+                ],
+                "issns": [
+                    "2673-611X"
+                ]
+            },
+            "identifier": [
+                {
+                    "type": "doi",
+                    "id": "10.1186/s40478-018-0619-9"
+                },
+                {
+                    "type": "eissn",
+                    "id": "2051-5960"
+                }
+            ],
+            "keywords": [
+                "Chronic traumatic encephalopathy",
+                "TMEM106B",
+                "Neuroinflammation",
+                "Football",
+                "Traumatic brain injury",
+                "Tau"
+            ],
+            "link": [
+                {
+                    "type": "fulltext",
+                    "url": "http://link.springer.com/article/10.1186/s40478-018-0619-9",
+                    "content_type": "HTML"
+                }
+            ],
+            "subject": [
+                {
+                    "scheme": "LCC",
+                    "term": "Neurology. Diseases of the nervous system",
+                    "code": "RC346-429"
+                }
+            ],
+            "author": [
+                {
+                    "name": "Jonathan D. Cherry",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Jesse Mez",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "John F. Crary",
+                    "affiliation": "Department of Pathology, Fishberg Department of Neuroscience, Friedman Brain Institute, Ronald M. Loeb Center for Alzheimer’s Disease, Icahn School of Medicine at Mount Sinai School"
+                },
+                {
+                    "name": "Yorghos Tripodis",
+                    "affiliation": "Department of Biostatistics, Boston University School of Public Health"
+                },
+                {
+                    "name": "Victor E. Alvarez",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Ian Mahar",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Bertrand R. Huber",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Michael L. Alosco",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Raymond Nicks",
+                    "affiliation": "Department of Veterans Affairs Medical Center"
+                },
+                {
+                    "name": "Bobak Abdolmohammadi",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Patrick T. Kiernan",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Laney Evers",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Sarah Svirsky",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Katharine Babcock",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Hannah M. Gardner",
+                    "affiliation": "VA Boston Healthcare System"
+                },
+                {
+                    "name": "Gaoyuan Meng",
+                    "affiliation": "VA Boston Healthcare System"
+                },
+                {
+                    "name": "Christopher J. Nowinski",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Brett M. Martin",
+                    "affiliation": "Department of Biostatistics, Boston University School of Public Health"
+                },
+                {
+                    "name": "Brigid Dwyer",
+                    "affiliation": "Department of Neurology, Boston University School of Medicine"
+                },
+                {
+                    "name": "Neil W. Kowall",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Robert C. Cantu",
+                    "affiliation": "Department of Anatomy and Neurobiology, Boston University School of Medicine"
+                },
+                {
+                    "name": "Lee E. Goldstein",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Douglas I. Katz",
+                    "affiliation": "Department of Neurology, Boston University School of Medicine"
+                },
+                {
+                    "name": "Robert A. Stern",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Lindsay A. Farrer",
+                    "affiliation": "Department of Neurology, Boston University School of Medicine"
+                },
+                {
+                    "name": "Ann C. McKee",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                },
+                {
+                    "name": "Thor D. Stein",
+                    "affiliation": "Boston University Alzheimer’s Disease and CTE Center, Boston University School of Medicine"
+                }
+            ]
+        },
+        "es_type": "article"
+    }
--- doajtest/unit/test_task_preservation.py
+++ doajtest/unit/test_task_preservation.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from io import BytesIO
-from datetime import datetime
 from unittest.mock import patch
 from doajtest.helpers import DoajTestCase
 from doajtest.mocks.preservation import PreservationMock
@@ -12,17 +11,54 @@ from werkzeug.datastructures import FileStorage
 from portality.models.article import Article
 
 
-class TestPreservation(DoajTestCase):
+def mock_pull_by_key(key, value):
+    if value == "http://link.springer.com/article/10.1186/s40478-018-0619-9":
+        article = Article()
+        article.data = PreservationMock.ARTICLE_DATA
+        return article
+    elif value == "https://www.frontiersin.org/articles/10.3389/fcosc.2022.1028295":
+        article = Article()
+        article.data = PreservationMock.ARTICLE_DATA_JOURNAL2
+        return article
 
-    def setUp(self):
-        super(TestPreservation, self).setUp()
+
+def mock_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if not args[0] == None and kwargs["data"]["org"] == "DOAJ":
+        return MockResponse({
+            "files": [
+                {
+                    "name": "name_of_tarball.tar.gz",
+                    "sha256": "decafbad"
+                }
+            ]
+        }, 200)
+
+    return MockResponse(None, 404)
+
+
+def mock_owner_of_article(*args, **kwargs):
+    return True
+
+
+class TestPreservationSetup(DoajTestCase):
+
+    def initial_setup(self, package_name):
+        super(TestPreservationSetup, self).setUp()
         resources = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "unit", "resources")
-        articles_zip_path = os.path.join(resources, "articles.zip")
+        articles_zip_path = os.path.join(resources, package_name)
         with open(articles_zip_path, 'rb') as zf:
-            self.zip_file = FileStorage(BytesIO(zf.read()), filename="articles.zip")
+            self.zip_file = FileStorage(BytesIO(zf.read()), filename=package_name)
 
         self.upload_dir = app.config.get("UPLOAD_DIR", ".")
-        created_time = dates.format(datetime.utcnow(), "%Y-%m-%d-%H-%M-%S")
+        created_time = dates.now()
         self.owner = "rama"
         self.journal_dir = "2051-5960"
         dir_name = self.owner + "-" + created_time
@@ -33,14 +69,23 @@ class TestPreservation(DoajTestCase):
         app.config["PRESERVATION_COLLECTION"] = {"rama": ["test", "2"]}
 
     def tearDown(self):
-        super(TestPreservation, self).tearDown()
+        super(TestPreservationSetup, self).tearDown()
         preservation.Preservation.delete_local_directory(self.local_dir)
         app.config["PRESERVATION_COLLECTION"] = self.preservation_collection
 
+
+class TestPreservation(TestPreservationSetup):
+
+    def setUp(self):
+        super(TestPreservation, self).initial_setup("articles.zip")
+
+    def tearDown(self):
+        super(TestPreservation, self).tearDown()
+
     def test_local_directory(self):
 
-        #Test creation of local directory
-        #TestPreservation.preserve.create_local_directories()
+        # Test creation of local directory
+        # TestPreservation.preserve.create_local_directories()
         job = preservation.PreservationBackgroundTask.prepare("rama", upload_file=self.zip_file)
         params = job.params
         local_dir = params["preserve__local_dir"]
@@ -49,41 +94,12 @@ class TestPreservation(DoajTestCase):
         assert os.path.isdir(os.path.join(self.upload_dir, dir_name))
         assert os.path.isdir(os.path.join(self.upload_dir, dir_name,dir_name))
 
-        #Test deletion of local directory
+        # Test deletion of local directory
         preservation.Preservation.delete_local_directory(local_dir)
         assert not os.path.exists(os.path.join(self.upload_dir, dir_name))
 
-    def mock_pull_by_key(key, value):
-        article = Article()
-        article.data = PreservationMock.ARTICLE_DATA
-        return article
-
-    def mock_requests_post(*args, **kwargs):
-        class MockResponse:
-            def __init__(self, json_data, status_code):
-                self.json_data = json_data
-                self.status_code = status_code
-
-            def json(self):
-                return self.json_data
-
-        if not args[0] == None and kwargs["data"]["org"] == "DOAJ":
-            return MockResponse({
-                      "files": [
-                        {
-                          "name": "name_of_tarball.tar.gz",
-                          "sha256": "decafbad"
-                        }
-                      ]
-                    }, 200)
-
-        return MockResponse(None, 404)
-
-    def mock_owner_of_article(*args, **kwargs):
-        return True
-
     @patch.object(Article, 'pull_by_key', mock_pull_by_key)
-    @patch.object(requests,"post", mock_requests_post)
+    @patch.object(requests, "post", mock_requests_post)
     @patch.object(preservation.Preservation, 'owner_of_article', mock_owner_of_article)
     def test_preservation(self):
         self.preserve.save_file(self.zip_file)
@@ -127,6 +143,88 @@ class TestPreservation(DoajTestCase):
         response = package.upload_package(sha256, tar_file)
         assert response.status_code == 200
 
+    def test_get_article_info(self):
+        issn, article_id, metadata_json = self.preserve.get_article_info(PreservationMock.ARTICLE_DATA)
+
+        assert issn == "2051-5960"
+        assert article_id == "00003741594643f4996e2555a01e03c7"
+        assert metadata_json["bibjson"]["identifier"][0]["id"] == "10.1186/s40478-018-0619-9"
+
+
+class TestPreservationMultipleJournals(TestPreservationSetup):
+
+    def setUp(self):
+        super(TestPreservationMultipleJournals, self).initial_setup("preservation_multiple_journals.zip")
+        self.another_journal_dir = "2673-611X"
+
+    def tearDown(self):
+        super(TestPreservationMultipleJournals, self).tearDown()
+
+    @patch.object(Article, 'pull_by_key', mock_pull_by_key)
+    @patch.object(requests,"post", mock_requests_post)
+    @patch.object(preservation.Preservation, 'owner_of_article', mock_owner_of_article)
+    def test_preservation_multiple_journals(self):
+        self.preserve.save_file(self.zip_file)
+
+        # Test extraction of zip file
+        self.preserve.extract_zip_file()
+
+        assert os.path.exists(os.path.join(self.tmp_dir, "articles"))
+        assert os.path.isdir(os.path.join(self.tmp_dir, "articles"))
+        assert os.path.isdir(os.path.join(self.tmp_dir, "articles", "article_1"))
+        assert os.path.exists(os.path.join(self.tmp_dir, "articles",
+                                           "article_1", "identifier.txt"))
+
+        reader = preservation.CSVReader(os.path.join(self.tmp_dir,
+                                                     "articles", "identifiers.csv"))
+        data = reader.articles_info()
+
+        assert "article_1" in data
+        assert "article/10.1186/s40478-018-0619-9" in data["article_1"][0]
+
+        assert "article_2" in data
+        assert "10.3389/fcosc.2022.1028295" in data["article_2"][0]
+
+        # Test package structure
+        self.preserve.create_package_structure()
+        package_dir = os.path.join(self.upload_dir,
+                                   self.preserve.dir_name, self.preserve.dir_name, self.journal_dir)
+        tag_manifest_file = os.path.join(package_dir, "00003741594643f4996e2555a01e03c7", "tagmanifest-sha256.txt")
+        manifest_file = os.path.join(package_dir, "00003741594643f4996e2555a01e03c7", "manifest-sha256.txt")
+        assert os.path.exists(package_dir)
+        assert os.path.exists(tag_manifest_file)
+        assert os.path.exists(manifest_file)
+
+        package = preservation.PreservationPackage(self.preserve.preservation_dir, self.journal_dir, self.owner)
+
+        # Test creation of tar file
+        package.create_package()
+        tar_file = package_dir + "_" + package.created_time + ".tar.gz"
+        assert os.path.exists(tar_file)
+
+        sha256 = package.sha256(tar_file)
+        response = package.upload_package(sha256, tar_file)
+        assert response.status_code == 200
+
+        # Test another journal package
+        package_dir = os.path.join(self.upload_dir,
+                                   self.preserve.dir_name, self.preserve.dir_name, self.another_journal_dir)
+        tag_manifest_file = os.path.join(package_dir, "00005741594643f4996e2666a01e0310", "tagmanifest-sha256.txt")
+        manifest_file = os.path.join(package_dir, "00005741594643f4996e2666a01e0310", "manifest-sha256.txt")
+        assert os.path.exists(package_dir)
+        assert os.path.exists(tag_manifest_file)
+        assert os.path.exists(manifest_file)
+
+        package = preservation.PreservationPackage(self.preserve.preservation_dir, self.another_journal_dir, self.owner)
+
+        # Test creation of tar file for another journal
+        package.create_package()
+        tar_file = package_dir + "_" + package.created_time + ".tar.gz"
+        assert os.path.exists(tar_file)
+
+        sha256 = package.sha256(tar_file)
+        response = package.upload_package(sha256, tar_file)
+        assert response.status_code == 200
 
     def test_get_article_info(self):
         issn, article_id, metadata_json = self.preserve.get_article_info(PreservationMock.ARTICLE_DATA)
