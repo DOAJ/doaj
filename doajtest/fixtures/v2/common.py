f65d9ad021fd8fa19d59655096361ed93dfdf739
philip <philip.k@cottagelabs.com>
2023-04-12 14:20:45 +0100

    other style of note_author_name

--- doajtest/fixtures/v2/common.py
+++ doajtest/fixtures/v2/common.py
@@ -1,11 +1,11 @@
 NOTES_FORM_EXPANDED = {
     'notes': [
         {"note": "Second Note", "note_date": "2014-05-22T00:00:00Z", "note_id": "1234",
-         'note_author': 'fake_account_id__b',
+         'note_author': '(fake_account_id__b)',
          "note_author_id": "fake_account_id__b",
          },
         {"note": "First Note", "note_date": "2014-05-21T14:02:45Z", "note_id": "abcd",
-         'note_author': 'fake_account_id__a',
+         'note_author': '(fake_account_id__a)',
          "note_author_id": "fake_account_id__a",
          }
     ]
