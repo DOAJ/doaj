93469d1d9411c4a481a9752c150c99baa4ef085f
Steve Eardley <steve@eardley.xyz>
2023-05-16 15:22:54 +0100

    Version bump for release

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "6.3.0"
+DOAJ_VERSION = "6.3.1"
 API_VERSION = "3.0.1"
 
 ######################################
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='doaj',
-    version='6.3.0',
+    version='6.3.1',
     packages=find_packages(),
     install_requires=[
         "awscli==1.20.50",
