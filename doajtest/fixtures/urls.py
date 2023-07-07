be55e81cf7711f766b254805f54840e4539eaf6b
Aga <aga@cottagelabs.com>
2023-07-07 12:14:35 +0100

    add ut for url regex

---
+++ doajtest/fixtures/urls.py
@@ -0,0 +1,12 @@
+VALID_URL_LISTS = [
+    "https://www.sunshine.com",
+    "http://www.moonlight.com",
+    "https://www.cosmos.com#galaxy",
+    "https://www.cosmos.com/galaxy",
+    "https://www.cosmos.com/galaxy#peanut"
+]
+
+INVALID_URL_LISTS = [
+    "ht:www",
+    "nonexistent.com"
+]
