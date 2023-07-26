3fdb3d57e3640a0fd3a821b4a6594eae7b9605a8
philip <philip.k@cottagelabs.com>
2023-07-26 10:07:30 +0100

    update

--- portality/scripts/replace_gdrive_excel.py
+++ portality/scripts/replace_gdrive_excel.py
@@ -6,19 +6,17 @@ from gspread_dataframe import set_with_dataframe
 
 def main():
     gdrive_key_json_path = '/home/kk/tmp/testdriveapi-393913-d2089ec4ef12.json'
+    excel_name = 'testexcel'
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name(gdrive_key_json_path, scope)
     client = gspread.authorize(creds)
 
     # Fetch the file
-    spreadsheet = client.open("testexcel")
-    worksheet = spreadsheet.get_worksheet(0)  # We'll only work with the first worksheet
+    worksheet = client.open(excel_name).get_worksheet(0)  # We'll only work with the first worksheet
 
-    # Get the data and convert it to dataframe
-    data = worksheet.get_all_values()
     # df = pd.DataFrame(data[1:], columns=data[0])
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(worksheet.get_all_values())
     print(df)
     # return
 
