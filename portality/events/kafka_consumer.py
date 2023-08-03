35a7367acccb7a2f1344dd2ad2a2bced9949a542
Ramakrishna <mail2rk@gmail.com>
2023-08-03 16:28:51 +0530

    added log to check the count of events

--- portality/events/kafka_consumer.py
+++ portality/events/kafka_consumer.py
@@ -11,13 +11,17 @@ topic_name = doajapp.config.get("KAFKA_EVENTS_TOPIC")
 app = faust.App('events', broker=broker, value_serializer='json')
 topic = app.topic(topic_name)
 
+event_counter = 0
+
 
 @app.agent(topic)
 async def handle_event(stream):
+    global event_counter
     with doajapp.test_request_context("/"):
         svc = DOAJ.eventsService()
         async for event in stream:
-            pass
+            event_counter += 1
+            doajapp.logger.info(f"Kafka event count {event_counter}")
             # TODO uncomment the following line once the Event model is fixed to Kafka
             # svc.consume(Event(raw=json.loads(event)))
 
