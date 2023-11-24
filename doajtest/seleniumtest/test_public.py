980f91e70665e4f5876d9eb129c9d27612fa5281
philip <philip.k@cottagelabs.com>
2023-11-24 10:12:50 +0000

    test circleci

--- doajtest/seleniumtest/test_public.py
+++ doajtest/seleniumtest/test_public.py
@@ -5,4 +5,5 @@ class PublicSeleniumSTC(SeleniumTestCase):
 
     def test_public(self):
         self.selenium.get('https://www.google.com/')
+        print(self.selenium.page_source)
         assert self.selenium.page_source
