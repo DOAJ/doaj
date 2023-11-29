c3f3a8f3eb0416687712943b1d6c125f88321d02
philip <philip.k@cottagelabs.com>
2023-11-29 13:14:36 +0000

    add manage-bgjobs to console_scripts

--- setup.py
+++ setup.py
@@ -74,4 +74,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
+    entry_points={
+        'console_scripts': [
+            'manage-bgjobs = portality.scripts.manage_background_jobs:main',
+        ],
+    },
+
 )
