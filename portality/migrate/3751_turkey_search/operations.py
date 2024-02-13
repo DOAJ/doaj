80490d23f47fa9a2d46d2f9247a824366fbeafd7
Steven Eardley <steve@eardley.xyz>
2024-02-13 15:00:15 +0000

    use JournalLikeObject when including applications

--- portality/migrate/3751_turkey_search/operations.py
+++ portality/migrate/3751_turkey_search/operations.py
@@ -1,8 +1,8 @@
-from portality.models.v2.journal import Journal
+from portality.models.v2.journal import JournalLikeObject
 
 
 def correct_turkey(obj):
-    obj: Journal
+    obj: JournalLikeObject
     if obj.data.get('index', {}).get('country') == 'Turkey':
         print(f'fix {obj.id}, {obj.bibjson().title} ')
         obj.data['index']['country'] = 'Türkiye'
