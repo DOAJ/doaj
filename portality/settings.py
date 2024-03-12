561f8e56d3143b4dff8efd92a2422e42d0abfd23
Steven Eardley <steve@eardley.xyz>
2024-03-12 10:30:11 +0000

    Version bump for autochecks release

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "6.6.4"
+DOAJ_VERSION = "6.6.5"
 API_VERSION = "3.0.1"
 
 ######################################
