de865971c4958257834739f572de684f9b8b0a13
Richard Jones <richard@cottagelabs.com>
2024-03-18 15:06:28 +0000

    generalise rapid_ur_resubmission script and add testdrive to produce testable data to work on

--- doajtest/functional/rapid_ur_resubmission.py
+++ doajtest/functional/rapid_ur_resubmission.py
@@ -9,19 +9,38 @@ them in the appropriate variables below
 import requests, json
 from doajtest.fixtures.v2.applications import ApplicationFixtureFactory
 
-API_KEY = "c7659b4bf01a456f80ec82fcb8295bfa"
-JOURNAL_ID = "abcdefghijk_journal"
 
-source = ApplicationFixtureFactory.incoming_application()
-source["admin"]["current_journal"] = JOURNAL_ID
-application = json.dumps(source)
+def rapid_resubmit(base_url, key, jid):
+    source = ApplicationFixtureFactory.incoming_application()
+    source["admin"]["current_journal"] = jid
+    application = json.dumps(source)
 
-# make rapid resubmission
-resp1 = requests.post(f"http://localhost:5004/api/applications?api_key={API_KEY}", data=application)
-resp2 = requests.post(f"http://localhost:5004/api/applications?api_key={API_KEY}", data=application)
+    # make rapid resubmission
+    resp1 = requests.post(f"{base_url}/api/applications?api_key={key}", data=application)
+    resp2 = requests.post(f"{base_url}/api/applications?api_key={key}", data=application)
 
-print(resp1.status_code)
-print(resp1.text)
+    return {"resp1": resp1, "resp2": resp2}
 
-print(resp2.status_code)
-print(resp2.text)
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="Base URL")
+    parser.add_argument("-k", "--key", help="API key")
+    parser.add_argument("-j", "--journal", help="Journal ID")
+    args = parser.parse_args()
+
+    print("Submitting duplicate responses in rapid succession...")
+    responses = rapid_resubmit(args.url, args.key, args.journal)
+
+    print("# Initial request:")
+    print("-- Response code: ", responses["resp1"].status_code, " (Expected: 201)")
+    print("-- Response body:\n", responses["resp1"].text)
+    print("\n")
+
+    print("# Second request:")
+    print("-- Response code: ", responses["resp2"].status_code, " (Expected: 400)")
+    print("-- Response body:\n", responses["resp2"].text)
+
+
+
