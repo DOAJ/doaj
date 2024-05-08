cab97438de35630e0119cbabadf80978e08b6269
philip <philip.k@cottagelabs.com>
2024-05-08 14:29:40 +0100

    Revert "remove if else no longer meaningful"
    This reverts commit e23f944cfc8cc102d17df0dbb4bf7c86ddfe6640.

--- portality/lib/formulaic.py
+++ portality/lib/formulaic.py
@@ -776,7 +776,11 @@ class FormulaicField(object):
             for k, v in custom_args.items():
                 kwargs[k] = v
 
-        wtf = self.wtfield
+        wtf = None
+        if wtfinst is not None:
+            wtf = wtfinst
+        else:
+            wtf = self.wtfield
 
         if add_data_as_choice and hasattr(wtf, "choices") and wtf.data not in [c[0] for c in wtf.choices]:
             wtf.choices += [(wtf.data, wtf.data)]
