62669555a47073c9decb13af7a1162b32b341eb7
philip <philip.k@cottagelabs.com>
2024-05-13 11:46:27 +0100

    fix test cases

--- doajtest/unit/test_view_doaj.py
+++ doajtest/unit/test_view_doaj.py
@@ -28,7 +28,9 @@ class TestViewDoaj(DoajTestCase):
             id_field = 'id'
             query = 'alksjdlaksjdalksdjl'
             response = client.get(f'/autocomplete/{doc_type}/{field_name}/{id_field}?q={query}', )
-            assert_not_found(response)
+            resp_json = response.json
+            assert response.status_code == 200
+            assert len(resp_json['suggestions']) == 0
 
     def test_autocomplete_pair__unknown_doc_type(self):
         with self.app_test.test_client() as client:
@@ -37,10 +39,9 @@ class TestViewDoaj(DoajTestCase):
             id_field = 'id'
             query = 'eng'
             response = client.get(f'/autocomplete/{doc_type}/{field_name}/{id_field}?q={query}', )
-            assert_not_found(response)
+            resp_json = response.json
+            assert response.status_code == 200
+            assert len(resp_json['suggestions']) == 1
+            assert resp_json['suggestions'][0]['id'] == ''
 
 
-def assert_not_found(response):
-    resp_json = response.json
-    assert response.status_code == 200
-    assert len(resp_json['suggestions']) == 0
