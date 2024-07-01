c8d0ddbc8ce843dc4d3a51e49e9cb61364024a21
Aga <aga@cottagelabs.com>
2024-07-01 16:59:47 +0100

    add helpers class

--- doajtest/testdrive/helpers/_statistics_group_structure.py
+++ doajtest/testdrive/helpers/_statistics_group_structure.py
@@ -1,5 +1,8 @@
 from portality import constants
 from portality import models
+from doajtest.fixtures import EditorGroupFixtureFactory
+import string
+import random
 
 
 class StatisticsGroupsStructure():
@@ -8,9 +11,33 @@ class StatisticsGroupsStructure():
     3 groups, each with the same Man Ed, 2 groups with the same editor, each with different number of AssEds
     """
 
-    NUMBER_OF_GROUPS = 3;
+    NUMBER_OF_GROUPS = 3
     NUMBER_OF_EDITORS = 2
     NUMBER_OF_ASSEDITORS = 8
+
+    FINISHED_APPLICATIONS = {
+        'ed0': 2,
+        'ed1': 3,
+        'assed1': 4,
+        'assed2': 1,
+        'assed3': 0,
+        'assed4': 5,
+        'assed5': 3,
+        'assed6': 7,
+        'assed7': 4,
+        'assed8': 2,
+    }
+
+    # Define editor groups and associate editors' associations
+    EDITOR_GROUPS = {
+        'eg0': {'editor_index': 0, 'start_assed_index': 0, 'end_assed_index': 2},
+        'eg1': {'editor_index': 0, 'start_assed_index': 2, 'end_assed_index': 5},
+        'eg2': {'editor_index': 1, 'start_assed_index': 5, 'end_assed_index': 8},
+    }
+
+    def create_random_str(self, n_char=10):
+        s = string.ascii_letters + string.digits
+        return ''.join(random.choices(s, k=n_char))
 
     def setup(self):
         self.createAccounts()
@@ -20,58 +47,92 @@ class StatisticsGroupsStructure():
     def createAccounts(self):
         un = self.create_random_str()
         pw1 = self.create_random_str()
-        self.admin = models.Account.make_account(un + "@example.com", un, "Editor " + un, [constants.ROLE_ADMIN])
+        admin = models.Account.make_account(un + "@example.com", un, "Editor " + un, [constants.ROLE_ADMIN])
         admin.set_password(pw1)
         admin.save()
+        self.admin = admin.id
 
-        self.editors = [];
-        for i in range(NUMBER_OF_EDITORS+1):
+        # Create editors accounts
+        self.editors = []
+        for i in range(self.NUMBER_OF_EDITORS):
             us = self.create_random_str()
             pw = self.create_random_str()
-            editors[i] = models.Account.make_account(un + "@example.com", un, "Editor" + str(i) + " " + un, [constants.ROLE_EDITOR])
-            editors[i].set_password(pw)
-            editors[i].save()
+            editor = models.Account.make_account(us + "@example.com", us, "Editor" + str(i) + " " + us,
+                                                 [constants.ROLE_EDITOR])
+            editor.set_password(pw)
+            editor.save()
+            self.editors.append(editor.id)
 
+        # Create associate editors accounts
         self.asseds = []
-        for i in range(NUMBER_OF_ASSEDITORS+1):
+        for i in range(1, self.NUMBER_OF_ASSEDITORS + 1):
             us = self.create_random_str()
             pw = self.create_random_str()
-            asseds[i] = models.Account.make_account(un + "@example.com", un, "AssEd" + str(i) + " " + un, [constants.ROLE_ASSOCIATE_EDITOR])
-            asseds[i].set_password(pw)
-            asseds[i].save()
+            assed = models.Account.make_account(us + "@example.com", us, "AssEd" + str(i) + " " + us,
+                                                [constants.ROLE_ASSOCIATE_EDITOR])
+            assed.set_password(pw)
+            assed.save()
+            self.asseds.append(assed.id)
 
     def createGroups(self):
-        eg_source = EditorGroupFixtureFactory.make_editor_group_source(maned=self.admin.id);
-        eg1 = models.EditorGroup(**eg_source)
-        eg1.editor = self.editors[0];
-        for i in range(2):
-            eg.add_associate(self.asseds[i]);
-        eg1.save(blocking=True)
 
-        eg_source = EditorGroupFixtureFactory.make_editor_group_source(maned=self.admin.id);
-        eg2 = models.EditorGroup(**eg_source)
-        eg2.editor = self.editors[0];
-        for i in range(2,5):
-            eg.add_associate(self.asseds[i]);
-        eg2.save(blocking=True)
-
-        eg_source = EditorGroupFixtureFactory.make_editor_group_source(maned=self.admin.id);
-        eg3 = models.EditorGroup(**eg_source)
-        eg3.editor = self.editors[0];
-        for i in range(5,9):
-            eg.add_associate(self.asseds[i]);
-        eg3.save(blocking=True)
+        self.groups = [];
+        for group_key, group_info in self.EDITOR_GROUPS.items():
+            eg_source = EditorGroupFixtureFactory.make_editor_group_source(group_name=group_key, maned=self.admin, editor=self.editors[group_info['editor_index']])
+            del eg_source["associates"] # these will be added in a moment
+            editor_group = models.EditorGroup(**eg_source)
+            editor_group.set_associates(self.asseds[group_info['start_assed_index']:group_info['end_assed_index']])
+            editor_group.save(blocking=True)
+            self.groups.append(editor_group.id)
 
     def createProvenanceData(self):
 
-        self.add_provenance_record("status:" + constants.APPLICATION_STATUS_READY, "editor", editors[0], eg1)
-        self.add_provenance_record("status:" + constants.APPLICATION_STATUS_READY, "editor", editors[0], eg1)
-        self.add_provenance_record("status:" + constants.APPLICATION_STATUS_COMPLETED, "associate_editor", assed1.id,
-                                   eg)
-        self.add_provenance_record("status:" + constants.APPLICATION_STATUS_COMPLETED, "associate_editor", assed2.id,
-                                   eg)
-        self.add_provenance_record("status:" + constants.APPLICATION_STATUS_COMPLETED, "associate_editor", assed3.id,
-                                   eg)
+        self.provenance_data = [];
+        for key, count in self.FINISHED_APPLICATIONS.items():
+            if key.startswith('ed'):
+                for _ in range(count):
+                    for group_key in self.EDITOR_GROUPS:
+                        if group_key in key:
+                            p = self.add_provenance_record("status:" + constants.APPLICATION_STATUS_READY, "editor",
+                                                       self.editors[int(key[-1])], group_key)
+                            self.provenance_data.append(p.id)
+            elif key.startswith('assed'):
+                for _ in range(count):
+                    group_key = key.replace('assed', 'eg')
+                    user_id = self.asseds[int(key[-1]) - 1]
+                    p = self.add_provenance_record("status:" + constants.APPLICATION_STATUS_COMPLETED,
+                                               "associate_editor", user_id, group_key)
+                    self.provenance_data.append(p.id)
 
+    def add_provenance_record(self, status, role, user, editor_group):
+        data = {
+            "user": user,
+            "roles": [role],
+            "type": "suggestion",
+            "action": status,
+            "editor_group": [editor_group]
+        }
+        p = models.Provenance(**data)
+        p.save(blocking=True)
+        return p;
 
+    def teardown(self):
+        for editor in self.editors:
+            models.Account.remove_by_id(editor)
+        for assed in self.asseds:
+            models.Account.remove_by_id(assed)
+        for group in self.groups:
+            models.EditorGroup.remove_by_id(group)
+        for p in self.provenance_data:
+            models.Provenance.remove_by_id(p)
+
+if __name__ == "__main__":
+    structure = StatisticsGroupsStructure()
+    structure.setup()
+    print(structure.admin)
+    print(structure.editors)
+    print(structure.asseds)
+    print(structure.groups)
+    print(structure.provenance_data)
+    structure.teardown()
 
