aff0c2fb3e694de44057cae2283c1914172a0d2b
Aga <aga@cottagelabs.com>
2024-08-30 10:13:17 +0100

    Add appropriate validator to WTForm Remove unnecessary validation override Add novalidate to form to prevent browser validation - it tried to set focus on a field hidden by select2

--- portality/view/forms.py
+++ portality/view/forms.py
@@ -94,9 +94,8 @@ class EditorGroupForm(Form):
     """
     group_id = HiddenField("Group ID", [validators.Optional()])
     name = StringField("Group Name", [validators.DataRequired(), UniqueGroupName()])
-    maned = StringField("Managing Editor", [validators.Optional(), MustHaveRole("admin")])
-
-    editor = StringField("Editor", [validators.Optional(), NotRole("publisher")])
+    maned = StringField("Managing Editor", [validators.DataRequired(), MustHaveRole("admin")])
+    editor = StringField("Editor", [validators.DataRequired(), NotRole("publisher")])
     associates = StringField("Associate Editors", [validators.Optional(), NotRole("publisher")])
 
     def __init__(self, formdata=None, obj=None, prefix='', data=None, meta=None, **kwargs):
@@ -104,15 +103,15 @@ class EditorGroupForm(Form):
         self.editor_error = False;
         self.maned_error = False;
 
-    def _validate_editors(self):
-        if not self.data["maned"]:
-            self.maned_error = True;
-        if not self.data["editor"]:
-            self.editor_error = True;
-        return not self.maned_error and not self.editor_error;
-
-    def validate(self, extra_validators = None):
-        return self._validate_editors() and super(EditorGroupForm, self).validate();
+    # def _validate_editors(self):
+    #     if not self.data["maned"]:
+    #         self.maned_error = True;
+    #     if not self.data["editor"]:
+    #         self.editor_error = True;
+    #     return not self.maned_error and not self.editor_error;
+    #
+    # def validate(self, extra_validators = None):
+    #     return self._validate_editors() and super(EditorGroupForm, self).validate();
 
 ##########################################################################
 ## Continuations Forms
