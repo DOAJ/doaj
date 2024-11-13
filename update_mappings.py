e0b60a3c490aedd72a80f444aae8ee7fb4362ccf
amdomanska <aga@cottagelabs.com>
2024-11-13 23:25:39 +0000

    remove accidental file

--- update_mappings.py
+++
@@ -1,4 +0,0 @@
-from portality.models import Journal, Application
-from portality.core import initialise_index, app, es_connection
-
-initialise_index(app, es_connection, only_mappings=[Journal.__type__, Application.__type__], force_mappings=True)
