404aa5406e0aa3c7c5c5f4066e163e9c1acee2e8
amdomanska <aga@cottagelabs.com>
2024-11-19 09:38:57 +0000

    PR cleanup: remove unnecessary space

--- portality/static/js/dates.js
+++ portality/static/js/dates.js
@@ -3,7 +3,7 @@ $.extend(true, doaj, {
         format: {
            FMT_DATETIME_STD: 'YYYY-MM-DDTH:mm:ssZ',
            FMT_DATETIME_NO_SECS: 'YYYY-MM-DD H:mm',
-            FMT_DATE_STD: 'YYYY-MM-DD'
+           FMT_DATE_STD: 'YYYY-MM-DD'
         },
         reformat: function(s, inFormat, outFormat) {
             return moment(s, inFormat).format(outFormat);
