e3e00b9f344702770018754af8efbce5cc49114a
Steven Eardley <steve@eardley.xyz>
2024-12-20 11:53:56 +0000

    version bump for py3.10 release

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "7.0.4"
+DOAJ_VERSION = "8.0.0"
 API_VERSION = "4.0.0"
 
 ######################################
