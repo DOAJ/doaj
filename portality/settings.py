7532719688882db2e70b201c54ea0df4cfda3f66
Steven Eardley <steve@eardley.xyz>
2025-03-11 15:10:32 +0000

    Version bump for collected release for: https://github.com/DOAJ/doajPM/issues/3980 https://github.com/DOAJ/doajPM/issues/4022 https://github.com/DOAJ/doajPM/issues/4075

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.1.3"
+DOAJ_VERSION = "8.1.4"
 API_VERSION = "4.0.0"
 
 ######################################
