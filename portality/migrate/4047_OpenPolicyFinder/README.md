51a84169e8e97511a05e7fe886d2df2c6cd1a88b
Steven Eardley <steve@eardley.xyz>
2025-05-08 11:30:50 +0100

    Update OPF script to also change URLs.

--- portality/migrate/4047_OpenPolicyFinder/README.md
+++ portality/migrate/4047_OpenPolicyFinder/README.md
@@ -4,4 +4,4 @@ Migration script to rename Sherpa/Romeo deposit policy to Open Policy Finder
 
 Run
 
-    python portality/migrate/4047_OpenPolicyFinder/migrate.py
+    python -u portality/upgrade.py -u portality/migrate/4047_OpenPolicyFinder/migrate.json
--- portality/migrate/4047_OpenPolicyFinder/functions.py
+++ portality/migrate/4047_OpenPolicyFinder/functions.py
@@ -1,6 +1,23 @@
-def rename_policy(journal_like):
+import re
+from portality.models import JournalLikeObject
+
+
+def rename_policy(journal_like: JournalLikeObject):
+    """ Update the name of Sherpa/Romeo to Open Policy Finder in metadata """
     jbib = journal_like.bibjson()
     if "Sherpa/Romeo" in jbib.deposit_policy:
         jbib.remove_deposit_policy("Sherpa/Romeo")
         jbib.add_deposit_policy("Open Policy Finder")
     return journal_like
+
+
+def rewrite_sherpa_url(journal_like: JournalLikeObject):
+    """ Substitute the updated URL for Open Policy Finder  """
+    jbib = journal_like.bibjson()
+    policy_url = jbib.deposit_policy_url
+    if policy_url is not None:
+        jbib.deposit_policy_url = re.sub(r'\s?https?://(v2|www)\.sherpa\.ac\.uk/',
+                                         "https://openpolicyfinder.jisc.ac.uk/",
+                                         policy_url)
+    return journal_like
+
