c84dd5726175307954aa3c1a06fcc75d3f6beb28
Steven Eardley <steve@eardley.xyz>
2025-05-22 14:14:37 +0100

    Also update the index to say our UR is finished

--- portality/migrate/20250521_cleanup_ingested_urs/cleanup_urs.py
+++ portality/migrate/20250521_cleanup_ingested_urs/cleanup_urs.py
@@ -2,8 +2,10 @@
 Identify and amend status of update requests created by CSV ingest to 'accepted'
 """
 
+from portality import constants
+
 
 def amend_status(ur_source: dict):
-    ur_source['admin']['application_status'] = 'accepted'
+    ur_source['admin']['application_status'] = constants.APPLICATION_STATUS_ACCEPTED
+    ur_source['index']['application_type'] = 'finished application/update'
     return ur_source
-
