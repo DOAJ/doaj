f18838299325344d5fb455ba062fdd80ac71eac3
Richard Jones <richard@cottagelabs.com>
2025-05-28 14:17:08 +0100

    write testbook and finish testdrive to match

--- doajtest/testdrive/autoassign.py
+++ doajtest/testdrive/autoassign.py
@@ -16,7 +16,7 @@ class Autoassign(TestDrive):
         acc.save()
         return acc, pw
 
-    def _draft(self, acc):
+    def _draft(self, acc, country):
         jsource = JournalFixtureFactory.make_journal_source(in_doaj=True, overlay={
             "id": models.Journal.makeid(),
             "created_date": dates.format(dates.before_now(86400)),
@@ -27,6 +27,9 @@ class Autoassign(TestDrive):
             },
             "bibjson": {
                 "title": acc.id + " Journal",
+                "publisher": {
+                    "country": country
+                }
             }
         })
         j = models.Journal(**jsource)
@@ -44,6 +47,9 @@ class Autoassign(TestDrive):
             },
             "bibjson": {
                 "title": acc.id + " Update Request",
+                "publisher": {
+                    "country": country
+                }
             }
         })
         ap = models.Application(**asource)
@@ -57,18 +63,23 @@ class Autoassign(TestDrive):
         pub3, pw3 = self._account("Publisher", [constants.ROLE_PUBLISHER])
         maned1, pw4 = self._account("Managing Editor", [constants.ROLE_ADMIN])
         maned2, pw5 = self._account("Managing Editor", [constants.ROLE_ADMIN])
+        maned3, pw6 = self._account("Managing Editor", [constants.ROLE_ADMIN])
 
-        eg1source = EditorGroupFixtureFactory.make_editor_group_source(group_name="Account Mapping", maned=maned1.id)
+        eg1source = EditorGroupFixtureFactory.make_editor_group_source(group_name="Account Mapping " + maned1.id, maned=maned1.id)
         eg1 = models.EditorGroup(**eg1source)
         eg1.save()
 
-        eg2source = EditorGroupFixtureFactory.make_editor_group_source(group_name="Country Mapping", maned=maned2.id)
+        eg2source = EditorGroupFixtureFactory.make_editor_group_source(group_name="Country Mapping " + maned2.id, maned=maned2.id)
         eg2 = models.EditorGroup(**eg2source)
         eg2.save()
 
-        j1, ap1 = self._draft(pub1)
-        j2, ap2 = self._draft(pub2)
-        j3, ap3 = self._draft(pub3)
+        eg3source = EditorGroupFixtureFactory.make_editor_group_source(group_name="Unmapped " + maned3.id, maned=maned3.id)
+        eg3 = models.EditorGroup(**eg3source)
+        eg3.save()
+
+        j1, ap1 = self._draft(pub1, "EH")
+        j2, ap2 = self._draft(pub2, "FR")
+        j3, ap3 = self._draft(pub3, "EH")
 
         ur1 = models.URReviewRoute()
         ur1.account_id = pub1.id
@@ -103,8 +114,12 @@ class Autoassign(TestDrive):
                 "publisher_username": pub3.id,
                 "publisher_password": pw3,
                 "draft": ap3.id,
-                "journal": j3.id
-            }
+                "journal": j3.id,
+                "editor_group": eg3.name,
+                "maned_username": maned3.id,
+                "maned_password": pw6
+            },
+            "mappings": [ur1.id, ur2.id],
         }
 
 
@@ -112,6 +127,9 @@ class Autoassign(TestDrive):
         models.Account.remove_by_id(params["map_by_account"]["publisher_username"])
         models.Account.remove_by_id(params["map_by_country"]["publisher_username"])
         models.Account.remove_by_id(params["unmapped"]["publisher_username"])
+        models.Account.remove_by_id(params["map_by_account"]["maned_username"])
+        models.Account.remove_by_id(params["map_by_country"]["maned_username"])
+        models.Account.remove_by_id(params["unmapped"]["maned_username"])
 
         models.Application.remove_by_id(params["map_by_account"]["update_request"])
         models.Application.remove_by_id(params["map_by_country"]["update_request"])
@@ -121,9 +139,14 @@ class Autoassign(TestDrive):
         models.Journal.remove_by_id(params["map_by_account"]["journal"])
         models.Journal.remove_by_id(params["unmapped"]["journal"])
 
+        for mapping in params["mappings"]:
+            models.URReviewRoute.remove_by_id(mapping)
+
         eg = models.EditorGroup.pull_by_key("name", params["map_by_account"]["editor_group"])
         eg.delete()
         eg = models.EditorGroup.pull_by_key("name", params["map_by_country"]["editor_group"])
+        eg.delete()
+        eg = models.EditorGroup.pull_by_key("name", params["unmapped"]["editor_group"])
         eg.delete()
 
         return {"status": "success"}
