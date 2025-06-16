dc4a6b8fa8a544334af6f59e10d2ba96bdd17a30
Steven Eardley <steve@eardley.xyz>
2025-06-16 17:03:03 +0100

    allow wait_until to return the condition function value

--- portality/lib/thread_utils.py
+++ portality/lib/thread_utils.py
@@ -1,16 +1,20 @@
 import time
-from typing import Callable
+from typing import Callable, Any
 
 
-def wait_until(cond_fn: Callable[[], bool], timeout=10, sleep_time=0.1,
+def wait_until(cond_fn: Callable[[], Any], timeout=10, sleep_time=0.1,
                timeout_msg='fail to meet the condition within the timeout period.', fallthrough: bool = False):
+    """ Wait until a function supplies a truthy reply. Or until timeout expires """
+
     start = time.time()
+    result = None
     while (time.time() - start) < timeout:
-        if cond_fn():
-            return
+        result = cond_fn()
+        if result:
+            return result
         time.sleep(sleep_time)
     if fallthrough:
         # Skip raising the error if we're willing to risk the condition not being met
-        return
+        return result
     else:
         raise TimeoutError(timeout_msg)
