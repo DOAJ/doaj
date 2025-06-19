99c6041d352af1e62093b63efb7d11bb4099dadc
Steven Eardley <steve@eardley.xyz>
2025-06-19 10:22:27 +0100

    version bump for prior release

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.3.1"
+DOAJ_VERSION = "8.3.2"
 API_VERSION = "4.0.0"
 
 ######################################
