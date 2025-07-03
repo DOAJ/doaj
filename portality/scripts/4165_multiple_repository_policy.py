6a6ba2c6fe41bf626b7ad61a9b1006dbbc06f48b
Aga <aga@cottagelabs.com>
2025-07-03 15:06:07 +0100

    go through both Journals and Applications

--- portality/scripts/4165_multiple_repository_policy.py
+++ portality/scripts/4165_multiple_repository_policy.py
@@ -1,27 +1,29 @@
-from portality.models import Journal
+from portality.models import Journal, Application
 import csv
 
 if __name__ == "__main__":
 
+    RECORDS = [Journal, Application]
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--out", help="output file path")
     args = parser.parse_args()
 
-    if not args.out:
-        print("Please specify an output file path with the -o option")
-        parser.print_help()
-        exit()
+    out = "out.csv"
 
     with open(out, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
-        writer.writerow(["IN DOAJ", "ID", "DEPOSIT POLICY SERVICES"])
+        writer.writerow(["IN DOAJ", "ID", "DEPOSIT POLICY SERVICES", "TYPE OF RECORD"])
         count = 0
-        for j in Journal.iterate():
-            dp_services = j.bibjson().deposit_policy
-            if len(dp_services) > 1:
-                print(dp_services)
-                try:
-                    writer.writerow([j.is_in_doaj(), j.id, dp_services])
-                except AttributeError:
-                    print("Error reading attributes for journal {0}".format(j['id']))
+        for record in RECORDS:
+            for r in record.iterate():
+                dp_services = r.bibjson().deposit_policy
+                if dp_services and len(dp_services) > 1:
+                    try:
+                        if record is Application:
+                            field = r.application_type
+                        else:
+                            field = r.is_in_doaj()
+                        writer.writerow([field, r.id, dp_services])
+                    except AttributeError:
+                        print("Error reading attributes for journal {0}".format(r.id))
