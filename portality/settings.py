767ac71872d3ada584f07b29b9badc30b4bf5e63
Steven Eardley <steve@eardley.xyz>
2025-09-24 11:53:26 +0100

    middle version bump to account for model & mapping upgrade

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.3.10"
+DOAJ_VERSION = "8.4.0"
 API_VERSION = "4.0.1"
 
 ######################################
