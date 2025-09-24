4b8085ed981333a1a152adbd93444069efe36d3b
Steven Eardley <steve@eardley.xyz>
2025-09-24 11:52:18 +0100

    release read-only mode after mappings update

--- portality/settings.py
+++ portality/settings.py
@@ -106,7 +106,7 @@ EVENT_SEND_FUNCTION = "portality.events.shortcircuit.send_event"
 
 # This puts the UI into READ_ONLY mode
 # ~~->ReadOnlyMode:Feature~~
-READ_ONLY_MODE = True
+READ_ONLY_MODE = False
 
 # This puts the cron jobs into READ_ONLY mode
 SCRIPTS_READ_ONLY_MODE = False
