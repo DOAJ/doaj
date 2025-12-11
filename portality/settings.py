fd27aa4af85b45c5d39744dcc203bc12646a3642
Steven Eardley <steve@eardley.xyz>
2025-12-11 12:51:36 +0000

    version increment for events handling as background jobs

--- portality/settings.py
+++ portality/settings.py
@@ -9,7 +9,7 @@ from portality.lib import paths
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.4.3"
+DOAJ_VERSION = "8.4.4"
 API_VERSION = "4.0.1"
 
 ######################################
