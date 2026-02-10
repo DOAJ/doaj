a69be5e8efaff9e0ba34a73f4bfbe2b8c3634b0d
Aga <aga@cottagelabs.com>
2026-02-10 18:35:57 +0000

    change wording in a flash

--- portality/ui/messages.py
+++ portality/ui/messages.py
@@ -161,7 +161,7 @@ class Messages(object):
 
     DEFAULT_500_DESCRIPTION = "An error has occurred. It looks like something has gone wrong."
 
-    YOUR_NOT_THE_OWNER = "You are not the owner of a journal you're trying to access. Here you can find your journals."
+    YOUR_NOT_THE_OWNER = "You are not the owner of a journal you're trying to access. Here are the journals associated with your account."
 
     @classmethod
     def flash(cls, tup):
