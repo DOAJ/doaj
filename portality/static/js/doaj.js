b0163b875b0aeef7a85639544ed1ed39b824ead6
kush.varade.19 <kush.varade.19@gmail.com>
2026-03-26 15:17:20 +0530

    The leak for formulaic wrt quick search handled

--- portality/static/js/doaj.js
+++ portality/static/js/doaj.js
@@ -122,11 +122,6 @@ var doaj = {
         const namespace = "doaj-minisearch";
         const container = "." + namespace + "--container";
 
-        // Initialize quick search keywords input with autocomplete suggestions
-        if (typeof autocomplete === 'function') {
-            autocomplete('#quick-search-keywords', 'bibjson.keywords', 'journal', 1, true, false);
-        }
-
         $(container + " [name=content-type]").on("change", function() {
             let that = $(this);
             if (!that.is(":checked")) {
