45ae40510a6014a05c1e460b649116f3fd2faa9e
Ramakrishna Sakhamuru <rama@cottagelabs.com>
2026-06-01 11:01:02 +0530

    version update

--- portality/settings.py
+++ portality/settings.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.6.4"
+DOAJ_VERSION = "8.6.5"
 API_VERSION = "4.0.1"
 
 ######################################
