bc73766205b6878d870521d442270f1f50308ef5
Steven Eardley <steve@eardley.xyz>
2026-08-27 16:13:59 +0100

    version bump for index change

--- portality/settings.py
+++ portality/settings.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # Application Version information
 # ~~->API:Feature~~
 
-DOAJ_VERSION = "8.6.13"
+DOAJ_VERSION = "8.7.0"
 API_VERSION = "4.0.1"
 
 ######################################
